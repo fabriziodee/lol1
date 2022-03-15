5623d7024d56b80afad513bcec30e763dd59fa0e
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-15 16:21:04 -0500

    Update nsfw_trapito.js

--- plugins/nsfw_trapito.js
+++ plugins/nsfw_trapito.js
@@ -2,6 +2,7 @@ let axios = require("axios")
 let handler = async (m, { conn, usedPrefix, command }) => {
 let group = global.DATABASE.data.chats[m.chat]
 if (group.isNsfw) return m.reply('En este grupo no se permite el contenido +18')
+conn.reply(m.chat, await, m)
 let res = await axios("https://api.waifu.pics/nsfw/trap")
 let json = res.data
 conn.sendFile(m.chat, json.url, 'trapito', '*UN TRAPITO <3*', m)
