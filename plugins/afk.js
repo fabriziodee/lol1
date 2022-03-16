dae315b27f3ba134bb6f0bb02f5773867f2625c1
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-16 18:35:46 -0500

    Update afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -1,12 +1,13 @@
-let handler = async (m, { text }) => {
+let handler = async (m, { text, usedPrefix, command }) => {
+  if (!text) throw `*Ingresé el motivo de afk*\n\n- Ejemplo: ${usedPrefix + command} A mimir`
   let user = global.DATABASE.data.users[m.sender]
   user.afk = + new Date
   user.afkReason = text
   m.reply(`*[ ⚠️ ] El usuario ${conn.getName(m.sender)} estará inactivo (AFK) no lo etiqueten*\n\n*🔰 Motivo de la inactividad${text ? ': ' + text : ''}*
 `)
 }
-handler.help = ['afk [alasan]']
-handler.tags = ['main']
-handler.command = /^afk$/i
+handler.help = ['afk']
+handler.tags = ['group']
+handler.command = /^(afk)$/i
 
 module.exports = handler
