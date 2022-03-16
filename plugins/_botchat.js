d452ed7bc964109fe1800af7e13b23795715a7bc
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-16 15:46:26 -0500

    Update _botchat.js

--- plugins/_botchat.js
+++ plugins/_botchat.js
@@ -3,10 +3,12 @@ let path = require('path')
 
 let handler = m => m
 
-handler.before = async function (m, { conn, command, text }) {
+handler.before = async function (m, { conn, command }) {
 
 if ((m.isBaileys && m.fromMe) || m.fromMe || !m.isGroup) return true
-if (!/Test/i.exec(m.text)) {
+let test = /Test/i
+let ttest = test.exec(m.text)
+if (ttest) {
 	let vn = './storage/audio/Yamete_onii_chan.mp3'
 	conn.sendFile(m.chat, vn, 'Yamete_onii_chan.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 	}
