372d792ec8544606e5ade98b7180b918e7f50ce4
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-16 15:00:48 -0500

    Update a.js

--- plugins/a.js
+++ plugins/a.js
@@ -3,11 +3,9 @@ let path = require('path')
 
 let handler = async (m, { conn }) => {
 let vn = './media/a.mp3'
-conn.sendFile(m.chat, vn, 'a.mp3', null, m, true, {
-type: 'audioMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
-})
+conn.sendFile(m.chat, vn, 'a.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 }
+
 handler.customPrefix = /ª|a|A/
 handler.command = /^(a|ª|A?$)/
 
