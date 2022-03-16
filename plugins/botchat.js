f2f14e8098c27a959fc564c690584af06aa1379c
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-16 15:30:59 -0500

    Update botchat.js

--- plugins/botchat.js
+++ plugins/botchat.js
@@ -2,17 +2,14 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn, command }) => {
-let type = Object.keys(m.message)[0]
-let budy = (type === 'conversation') ? m.message.conversation : (type === 'extendedTextMessage') ? m.message.extendedTextMessage.text : ''
-
-if (budy.includes('Test')) {
+if (command.includes('Test')) {
 	let vn = './storage/audio/Yamete_onii_chan.mp3'
 	conn.sendFile(m.chat, vn, 'Yamete_onii_chan.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 	}
 
 }
 
-//handler.customPrefix = /onichan|yamete|senpai/i
+handler.customPrefix = /Test/i
 handler.command = new RegExp
 
 handler.fail = null
