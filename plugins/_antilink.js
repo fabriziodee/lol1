fc30bb80677700bddb8fab5c41f2bc0ab4066262
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-16 07:07:19 -0500

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,3 +1,4 @@
+let { MessageType, mentionedJid } = require("@adiwajshing/baileys");
 let handler = m => m
 
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
@@ -7,20 +8,26 @@ handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
   let isGroupLink = linkRegex.exec(m.text)
 
   if (chat.antiLink && isGroupLink) {
-    await m.reply(`*「 ANTI LINKS 」*\n*Hasta la vista baby👋, ${await this.getName(m.sender)} rompiste las reglas serás exterminado....!!*`)
-    await m.reply(`*Tienes 3 segundos para eliminar el link y retractarte...!!!!*`)
-    await m.reply(`*3!!*`)
-    await m.reply(`*2!!*`)
-    await m.reply(`*1!!*`)
-    if (isAdmin) return m.reply('*Te salvaste cagon(a) eres admin, no puedo eliminarte :v*')
-    if (!isBotAdmin) return m.reply('*El bot no es admin, no puede exterminar a las personas*')
+    await this.sendMessage(m.chat, `     *‧ [ ⭕ Enlace Detectado ⭕ ] ‧*\n\n• Nombre: @${m.sender.split("@s.whatsapp.net")[0]}\n• Fecha: ${date}\n• Enlace: WhatsApp.com`, MessageType.text, { quoted: m, contextInfo: { mentionedJid: [m.sender] } })
+    if (isAdmin) return m.reply('Menos mal que eres un administrador')
+    if (!isBotAdmin) return m.reply('Menos mal que no soy administradora del grupo')
     let linkGC = ('https://chat.whatsapp.com/' + await this.groupInviteCode(m.chat))
     let isLinkThisGc = new RegExp(linkGC, 'i')
     let isgclink = isLinkThisGc.test(m.text)
-    if (isgclink) return m.reply('*Lol.. enviaste el enlace de este grupo :v*')
+    if (isgclink) return m.reply('Menos mal que este enlace es del grupo :v')
     await this.groupRemove(m.chat, [m.sender])
   }
   return true
 }
 
+
 module.exports = handler
+
+
+let d = new Date(new Date + 3600000)
+let locale = 'es'
+let date = d.toLocaleDateString(locale, {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    })
