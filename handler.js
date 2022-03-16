623f5f944b5f3f25ce95b6be479235d8e5036001
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-16 14:18:59 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,6 +1,7 @@
 let util = require('util')
 let simple = require('./lib/simple')
 let { MessageType } = require('@adiwajshing/baileys')
+let fetch = require('node-fetch')
 
 const isNumber = x => typeof x === 'number' && !isNaN(x)
 const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(resolve, ms))
