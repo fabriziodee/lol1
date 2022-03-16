7f4cd7af66c7509f0f2bed6b8a102559dcd848af
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-16 16:50:16 -0500

    Update _botchat.js

--- plugins/_botchat.js
+++ plugins/_botchat.js
@@ -21,7 +21,7 @@ let audio4B = audio4A.exec(m.text)
 let audio5A = /pasen hentai|pasen hentay|antojen/i
 let audio5B = audio5A.exec(m.text)
 
-let audio6A = /eres gay/i
+let audio6A = /eres gay|que gay/i
 let audio6B = audio6A.exec(m.text)
 
 let audio7A = /mirame/i
@@ -49,68 +49,68 @@ let audio13B = audio13A.exec(m.text)
 if (audio1B) {
 	let vn = './storage/audio/Yamete_onii_chan.mp3'
 	conn.sendFile(m.chat, vn, 'Yamete_onii_chan.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-	}
+	} return
 
 if (audio2B) {
 	let vn = './storage/audio/Yo_igual_te_amo_pero.mp3'
 	conn.sendFile(m.chat, vn, 'Yo_igual_te_amo_pero.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-	}
+	} return
 
 if (audio3B) {
 	let vn = './storage/audio/Tontito_soy_hombre.mp3'
 	conn.sendFile(m.chat, vn, 'Tontito_soy_hombre.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-	}
+	} return
 
 if (audio4B) {
 	let vn = './storage/audio/Tienes_que_mimir.mp3'
 	conn.sendFile(m.chat, vn, 'Tienes_que_mimir.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-	}
+	} return
 
 if (audio5B) {
-	let au = ["./storage/audio/Pensamientos_sexuales.mp3", "./storage/audio/Pensamientos_sexuales.mp3"]
+	let au = ["./storage/audio/Pensamientos_sexuales.mp3", "./storage/audio/No_te_da_verguenza.mp3"]
 	let vn = au[Math.floor(Math.random() * au.length)]
 	conn.sendFile(m.chat, vn, 'Pensamientos_verguensa.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-	}
+	} return
 
 if (audio6B) {
 	let vn = './storage/audio/Ok_maricon.mp3'
 	conn.sendFile(m.chat, vn, 'Ok_maricon.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-	}
+	} return
 
 if (audio7B) {
 	let vn = './storage/audio/No_miro_personas_lindas.mp3'
 	conn.sendFile(m.chat, vn, 'No_miro_personas_lindas.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-	}
+	} return
 
 if (audio8B) {
 	let vn = './storage/audio/No_llores_onii-chan.mp3'
 	conn.sendFile(m.chat, vn, 'No_llores_onii-chan.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-	}
+	} return
 
 if (audio9B) {
 	let vn = './storage/audio/Me_cago_en_la_concha.mp3'
 	conn.sendFile(m.chat, vn, 'Me_cago_en_la_concha.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-	}
+	} return
 
 if (audio10B) {
 	let vn = './storage/audio/Juguemos_al_ascensor.mp3'
 	conn.sendFile(m.chat, vn, 'Juguemos_al_ascensor.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-	}
+	} return
 
 if (audio11B) {
 	let vn = './storage/audio/Esto_no_es_leche.mp3'
 	conn.sendFile(m.chat, vn, 'Esto_no_es_leche.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-	}
+	} return
 
 if (audio12B) {
 	let vn = './storage/audio/Eres_muy_chistoso.mp3'
 	conn.sendFile(m.chat, vn, 'Eres_muy_chistoso.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-	}
+	} return
 
 if (audio13B) {
 	let vn = './storage/audio/El_suicidio_no_es_una_opcion.mp3'
 	conn.sendFile(m.chat, vn, 'El_suicidio_no_es_una_opcion.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-	}
+	} return
 
 
 
