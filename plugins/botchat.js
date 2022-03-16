c4b59c535dcfebec8bb2f3db445d6ce00d8df967
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-16 15:23:05 -0500

    Update botchat.js

--- plugins/botchat.js
+++ plugins/botchat.js
@@ -1,9 +1,11 @@
 let util = require('util')
 let path = require('path')
 
-let handler = async (m, { conn, command }) => {
+let handler = async (m, { conn }) => {
 
-if (command.includes('onichan', 'yamete', 'senpai')) {
+const budy = (type === 'conversation') ? m.message.conversation : (type === 'extendedTextMessage') ? m.message.extendedTextMessage.text : ''
+
+if (budy.includes('Test')) {
 	let vn = './storage/audio/Yamete_onii_chan.mp3'
 	conn.sendFile(m.chat, vn, 'Yamete_onii_chan.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 	}
