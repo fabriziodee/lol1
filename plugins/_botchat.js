049114509821363a23789e5bda41a9565bd32e8d
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-16 15:37:04 -0500

    Update and rename botchat.js to _botchat.js

--- plugins/_botchat.js
+++ plugins/_botchat.js
@@ -1,17 +1,13 @@
 let util = require('util')
 let path = require('path')
 
-let handler = async (m, { conn, command }) => {
-if (command.includes('Test')) {
+let handler = async (m, { conn, command, text }) => {
+
+if (!/Test/i.test(m.text)) {
 	let vn = './storage/audio/Yamete_onii_chan.mp3'
 	conn.sendFile(m.chat, vn, 'Yamete_onii_chan.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 	}
 
 }
-
-handler.customPrefix = /Test/i
-handler.command = new RegExp
-
-handler.fail = null
 
 module.exports = handler
