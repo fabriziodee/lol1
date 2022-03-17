0a15e4919f337ac06fc68c5ac970230be1252c83
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-17 17:59:07 -0500

    Update traducir.js

--- plugins/traducir.js
+++ plugins/traducir.js
@@ -2,7 +2,7 @@ const translate = require('translate-google-api')
 const defaultLang = 'es'
 const tld = 'cn'
 
-let handler = async (m, { args, usedPrefix, command }) => {
+let handler = async (m, { args, text, usedPrefix, command }) => {
 let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
 if (!teks) return m.reply(`*Ingrese un texto o etiqueta uno*\n\n- Ejemplo: ${usedPrefix + command} es Hello`)
 let lang = (args[0] || defaultLang)
