c3f03e157b38be55ab2db8e99894ea6eec641c4d
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-17 17:24:53 -0500

    Update and rename boobs.js to nsfw-pechos.js

--- plugins/nsfw-pechos.js
+++ plugins/nsfw-pechos.js
@@ -6,7 +6,6 @@ if (chat.isNsfw) {
 conn.reply(m.chat, wait, m)
 let res = await axios("https://meme-api.herokuapp.com/gimme/boobs")
 let json = res.data
-let ShadowBot = json.url
 conn.sendFile(m.chat, json.url, 'pechos', '*PECHOS*', m)
 } else m.reply('En este grupo no se permite el contenido +18')
 }
