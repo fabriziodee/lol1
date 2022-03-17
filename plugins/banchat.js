628a0349e55c302d7c1a6738b77744bf8c4653fa
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-17 08:10:20 -0500

    Update banchat.js

--- plugins/banchat.js
+++ plugins/banchat.js
@@ -1,13 +1,25 @@
 let handler = async (m, { conn, participants }) => {
 // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
-if (!(m.chat in global.DATABASE._data.chats)) return m.reply('*Este chat no está registrado en la base de datos!*')
+if (!(m.chat in global.DATABASE._data.chats)) return m.reply('Este chat no está registrado en la base de datos!')
 let chat = global.DATABASE._data.chats[m.chat]
-if (chat.isBanned) return m.reply('*Este chat ya estaba baneado previamente*\n*Si desea desbanearlo use /unbanchat*')
+if (chat.isBanned) return m.reply('Este grupo ya está baneado!')
 chat.isBanned = true
-m.reply('*Este chat ha sido baneado a desicion de un administrador del grupo*')
+m.reply('\t\t*‧ 📫 Chat baneado 📫 ‧*\n\nAhora nadie podrá utilizar los comandos')
 // } else m.reply('Aquí hay un número de un host...')
 }
-handler.command = /^banchat$/i
+
+handler.help = ['banchat']
+handler.tags = ['owner']
+handler.command = /^(banchat)$/i
+handler.owner = true
+handler.mods = true
+handler.premium = false
 handler.group = true
-handler.admin = true
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = true
+
+handler.fail = null
+
 module.exports = handler
