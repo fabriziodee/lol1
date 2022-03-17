2aca69379301ae6839210cf4d7d18a162b2dfa4a
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-16 19:08:09 -0500

    Update afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -1,9 +1,11 @@
+let { MessageType, mentionedJid } = require("@adiwajshing/baileys");
+
 let handler = async (m, { text, usedPrefix, command }) => {
   if (!text) throw `*Ingresé el motivo de afk*\n\n- Ejemplo: ${usedPrefix + command} A mimir`
   let user = global.DATABASE.data.users[m.sender]
   user.afk = + new Date
   user.afkReason = text
-  conn.reply(m.chat, `\t\t*‧ ⏰ Ahora estas en AFK ⏰ ‧*\n\n• Nombre: @${m.sender.split("@s.whatsapp.net")[0]}\n• Razón: ${text ? `${text}` : 'No hay'}\n\nSi alguien te etiqueta será notificado`, MessageType.text, { quoted: m, contextInfo: { mentionedJid: [m.sender] } }) 
+  conn.sendMessage(m.chat, `\t\t*‧ ⏰ Ahora estas en AFK ⏰ ‧*\n\n• Nombre: @${m.sender.split("@s.whatsapp.net")[0]}\n• Razón: ${text ? `${text}` : 'No hay'}\n\nSi alguien te etiqueta será notificado`, MessageType.text, { quoted: m, contextInfo: { mentionedJid: [m.sender] } }) 
 }
 
 handler.help = ['afk']
