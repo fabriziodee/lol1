5daccde81ba81765ec68be12a2c3df81269b7c2f
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-17 18:28:24 -0500

    Update traducir.js

--- plugins/traducir.js
+++ plugins/traducir.js
@@ -1,45 +1,13 @@
 const translate = require('translate-google-api')
-const defaultLang = 'es'
-const tld = 'cn'
 
-let handler = async (m, { args, usedPrefix, command }) => {
-    let err = `
-Contoh:
-${usedPrefix + command} <lang> [text]
-${usedPrefix + command} id your messages
-
-Daftar bahasa yang didukung: https://cloud.google.com/translate/docs/languages
-`.trim()
-
-    let lang = args[0]
-    let text = args.slice(1).join(' ')
-    if ((args[0] || '').length !== 2) {
-        lang = defaultLang
-        text = args.join(' ')
-    }
-    if (!text && m.quoted && m.quoted.text) text = m.quoted.text
-
-    let result
-    try {
-        result = await translate(`${text}`, {
-            tld,
-            to: lang,
-        })
-    } catch (e) {
-        result = await translate(`${text}`, {
-            tld,
-            to: defaultLang,
-        })
-        throw err
-    } finally {
-        m.reply(result[0])
-    }
-
+let handler = async (m, { args, text, usedPrefix, command }) => {
+result = await translate(`Hello`, { 'cn', to: 'es' })
+m.reply(result[0])
 }
-handler.help = ['translate'].map(v => v + ' <lang> <teks>')
+
+handler.help = ['traducir']
 handler.tags = ['tools']
-handler.command = /^(tr(anslate)?)$/i
-handler.limit = false
+handler.command = /^(traducir|trad|translate)$/i
 handler.fail = null
-handler.exp = 0
+
 module.exports = handler
