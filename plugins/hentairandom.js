86ccd66fc82c5dba6d1a4c183f9e3416352a7aa8
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-18 09:02:02 -0500

    Update hentairandom.js

--- plugins/hentairandom.js
+++ plugins/hentairandom.js
@@ -1,9 +1,19 @@
+let axios = require("axios")
 let fetch = require('node-fetch')
-     let handler  = async (m, { conn, usedPrefix, command }) => {
-    heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/hentai?apikey=apirey`)
-    json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '*Hentai By ShadowBot*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+let chat = global.DATABASE.data.chats[m.chat]
+if (chat.isNsfw) {
+conn.reply(m.chat, wait, m)
+heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/hentai?apikey=apirey`)
+json = await heum.buffer()
+conn.sendFile(m.chat, json, 'hentai', '*HENTAI*', m)
+} else m.reply('En este grupo no se permite el contenido +18')
 }
+
+handler.help = ['hentai']
+handler.tags = ['nsfw']
 handler.command = /^(hentai|hentay)$/i
+handler.limit = true
 
 module.exports = handler
