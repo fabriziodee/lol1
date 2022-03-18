224fae3832384926a83893d763c5489380258f08
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-18 16:32:14 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -6,7 +6,7 @@ let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 let handler = async (m, { conn, text }) => {
 if (!text) throw `*Ingrese un número*\n\n- Ejemplo: ${usedPrefix + command} 100 GB`
 if (Number(text.split(' MB')[0]) >= 70.00) return m.reply(`✳️ El archivo es demasiado grande, la descarga se canceló`)
-if (Number(text.split(' GB')[0]) >= 70.00) return m.reply(`✳️ El archivo es demasiado grande, la descarga se canceló`)
+if (Number(text.split(' GB')[0]) >= 0) return m.reply(`✳️ El archivo es demasiado grande, la descarga se canceló`)
 }
 
 handler.help = ['test']
