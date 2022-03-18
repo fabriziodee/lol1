9625d0b08742cb898f96c253aad54ae729c4e120
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-17 19:40:26 -0500

    Update traducir.js

--- plugins/traducir.js
+++ plugins/traducir.js
@@ -1,21 +1,21 @@
 let fetch = require('node-fetch')
 let handler = async (m, { args, text, usedPrefix, command }) => {
-    let text = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
+    let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
     let msg = `*Ingrese un texto para traducir*\n\n- Ejemplo: ${usedPrefix + command} es Hello`
     if (!teks) return m.reply(msg)
     let lang = (args[0] || 'es')
     let texxt = args.join(' ')
     if (args[0].length == 2 && args.length > 0) {
         lang = args[0]
-        text = args.slice(1).join(' ')
+        teks = args.slice(1).join(' ')
     }
     let res = await fetch(global.API('bg', '/translate', {
-        q: text,
+        q: teks,
         lang
     }))
     let json = await res.json()
     if (json.status !== true) throw json
-    conn.reply(m.chat, `\t\t*‧ 🌐 Google Traductor 🌐 ‧*\n\n*${json.result.from.language.iso}:* ${text}\n*${lang}:* ${json.result.text}`, m)
+    conn.reply(m.chat, `\t\t*‧ 🌐 Google Traductor 🌐 ‧*\n\n*${json.result.from.language.iso}:* ${teks}\n*${lang}:* ${json.result.text}`, m)
 }
 
 handler.help = ['traducir']
