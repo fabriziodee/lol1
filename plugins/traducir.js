f097c0d39ceb11e5343e0daa10c3fba4455b61b2
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-17 19:53:55 -0500

    Update traducir.js

--- plugins/traducir.js
+++ plugins/traducir.js
@@ -3,10 +3,10 @@ let handler = async (m, { args, text, usedPrefix, command }) => {
     let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
     let msg = `*Ingrese un texto para traducir*\n\n- Ejemplo: ${usedPrefix + command} es Hello`
     if (!teks) return m.reply(msg)
-    let lang = (args[0] || 'es')
+    let lang = args[0] ? args[0] : 'es'
     let texxt = args.join(' ')
     if (args[0].length == 2 && args.length > 0) {
-        lang = args[0]
+        lang = args[0] ? args[0] : 'es'
         teks = args.slice(1).join(' ')
     }
     let res = await fetch(global.API('bg', '/translate', {
