a8adb6333a4c5f6d96ab0a4513f4c13bf2bc6c02
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-18 12:29:37 -0500

    Create activar.js

---
+++ plugins/activar.js
@@ -0,0 +1,34 @@
+let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner, isPrems }) => {
+
+let chat = global.DATABASE._data.chats[m.chat]
+let user = global.DATABASE._data.users[m.sender]
+let type = (args[0] || ' ').toLowerCase()
+
+switch (type) {
+case 'bienvenida':
+case 'welcome':
+      if (chat.welcome) return reply('La *bienvenida* ya está *activado* en este chat!')
+      if (!m.isGroup) return global.dfail('group', m, conn)
+      if (!isOwner) return global.dfail('owner', m, conn)
+      chat.welcome = true
+      m.reply('Se activó la *bienvenida* en este grupo!')
+      break
+
+    default:
+let opc = `
+*Ingrese una opción para activar*
+
+- Ejemplo: ${usedPrefix + command} bienvenida
+
+*Lista de opciones:*
+- Bienvenida
+`.trim()
+return m.reply(opc)
+  }
+}
+
+handler.help = ['activar < opcion >']
+handler.tags = ['group']
+handler.command = /^(activar)$/i
+
+module.exports = handler
