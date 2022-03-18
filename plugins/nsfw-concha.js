4a86b902b72ea7cd37003285e3d49d126614ba7e
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-18 08:45:16 -0500

    Update and rename pussy.js to nsfw-concha.js

--- plugins/nsfw-concha.js
+++ plugins/nsfw-concha.js
@@ -1,23 +1,26 @@
 const axios = require('axios')
+
 let handler = async(m, { conn }) => {
+let chat = global.DATABASE.data.chats[m.chat]
+if (chat.isNsfw) {
+conn.reply(m.chat, wait, m)
 let porn = await axios.get('https://meme-api.herokuapp.com/gimme/pussy')
-           conn.sendFile(m.chat, `${porn.data.url}`, '', `${porn.data.title}`, m)
-  }
-handler.help = ['pussy']
-handler.tags = ['images']
-handler.command = /^(pussy)$/i
+conn.sendFile(m.chat, `${porn.data.url}`, 'cuca', `*${porn.data.title}*`, m)
+} else m.reply('En este grupo no se permite el contenido +18')
+}
+
+handler.help = ['cuca']
+handler.tags = ['nsfw']
+handler.command = /^(concha|vajina|cuca|pussy)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
 handler.group = false
 handler.private = false
 handler.register = false
-
 handler.admin = false
 handler.botAdmin = false
-
 handler.fail = null
-handler.exp = 0
 handler.limit = false
 
 module.exports = handler
