79e1744a7cd327714f0c76b66c9b17e5040f513d
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-19 12:11:47 -0500

    Update and rename gay2.js to gay.js

--- plugins/gay.js
+++ plugins/gay.js
@@ -1,4 +1,5 @@
 let handler = async (m, { conn }) => {
+conn.reply(m.chat, wait, m)
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/gay', {
 avatar: await conn.getProfilePicture(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
