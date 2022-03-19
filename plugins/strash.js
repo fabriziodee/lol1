b36b29ac22b6d38aeee9fd71e67aabbc1063aada
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-19 00:23:48 -0500

    Update strash.js

--- plugins/strash.js
+++ plugins/strash.js
@@ -6,21 +6,22 @@ let handler = async (m, { conn, text }) => {
  try {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw '*Responda a una foto*'
-  if (!/image\/(jpe?g|png)/.test(mime)) throw `Tamaño/peso no soportado`
+  if (!mime) throw 'Etiqueta una imagen!'
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Tamaño/peso no soportado!`
   let img = await q.download()
   let url = await uploadImage(img)
   let wanted = global.API('dzx', '/api/canvas/trash', { url }) //`https://api.dhamzxploit.my.id/api/canvas/trash?url=${url}`
-  let stiker = await sticker(null, wanted, 'trash', '©The Shadow Brokers - Bot')
+  let stiker = await sticker(null, wanted, global.packname, global.author)
   conn.sendMessage(m.chat, stiker, MessageType.sticker, {
     quoted: m
   })
  } catch (e) {
-   m.reply('Conversion Failed')
+   m.reply(error)
   }
 }
+
 handler.help = ['trash']
-handler.tags = ['General']
-handler.command = /^trash$/i
+handler.tags = ['stiker']
+handler.command = /^(trash)$/i
 
 module.exports = handler
