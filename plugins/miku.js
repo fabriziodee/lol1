b33fde0d4582ae164e892f0a603022a12b309202
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-18 23:57:24 -0500

    Update miku.js

--- plugins/miku.js
+++ plugins/miku.js
@@ -1,9 +1,13 @@
 let fetch = require('node-fetch')
-     let handler  = async (m, { conn, usedPrefix, command }) => {
-    heum = await fetch(`https://api-reysekha.herokuapp.com/api/wallpaper/miku?apikey=APIKEY`)
-    json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '*Hatsune Miku*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
+
+let handler  = async (m, { conn, usedPrefix, command }) => {
+heum = await fetch(`https://api-reysekha.herokuapp.com/api/wallpaper/miku?apikey=APIKEY`)
+json = await heum.buffer()
+conn.sendFile(m.chat, json, 'miku', `*MIKU*`, m)
 }
+
+handler.help = ['miku']
+handler.tags = ['random']
 handler.command = /^(micu|miku)$/i
 
 module.exports = handler
