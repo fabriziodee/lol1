00ac8704786909f4218df2ed9b7ca87068ca18f1
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-19 14:37:15 -0500

    Create menutype.js

---
+++ plugins/menutype.js
@@ -0,0 +1,51 @@
+let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner, isPrems }) => {
+
+let chat = global.DATABASE._data.chats[m.chat]
+let user = global.DATABASE._data.users[m.sender]
+let type = (args[0] || ' ').toLowerCase()
+
+switch (type) {
+case 'imagen':
+      if (chat.menu == 1) return m.reply('El menu *simple* ya está activado!')
+      if (!m.isGroup) return global.dfail('group', m, conn)
+      if (!isOwner) return global.dfail('owner', m, conn)
+      chat.menu = 1
+      m.reply('Se activó el menu *simple*!')
+      break
+
+case 'documento':
+      if (chat.menu == 2) return m.reply('El menu *documento* ya está activado!')
+      if (!m.isGroup) return global.dfail('group', m, conn)
+      if (!isOwner) return global.dfail('owner', m, conn)
+      chat.menu = 2
+      m.reply('Se activó el menu *documento*!')
+      break
+      
+case 'simple':
+      if (chat.menu == 3) return m.reply('El menu *imagen* ya está activado!')
+      if (!m.isGroup) return global.dfail('group', m, conn)
+      if (!isOwner) return global.dfail('owner', m, conn)
+      chat.menu = 3
+      m.reply('Se activó el menu *imagen*!')
+      break
+
+    default:
+let opc = `
+*Ingrese un tipo de menú*
+
+- Ejemplo: ${usedPrefix + command} simple
+
+*Tipos de menús:*
+- Imagen
+- Documento
+- Simple
+`.trim()
+return m.reply(opc)
+  }
+}
+
+handler.help = ['menutype']
+handler.tags = ['owner']
+handler.command = /^(menutype)$/i
+
+module.exports = handler
