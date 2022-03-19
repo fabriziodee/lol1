fb8bb38c7b94d3a46851fb00f1ec939e0447f8ba
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-19 00:53:46 -0500

    Update google.js

--- plugins/google.js
+++ plugins/google.js
@@ -1,25 +1,25 @@
 let fetch = require('node-fetch')
 let googleIt = require('google-it')
-let handler = async (m, { conn, command, args }) => {
+let handler = async (m, { conn, usedPrefix, command, args }) => {
   let full = /f$/i.test(command)
   let text = args.join` `
-  if (!text) return conn.reply(m.chat, '*[❗] Ingrese el texto o tema que desee buscar*', m)
+  if (!text) return conn.reply(m.chat, `*Ingrese un texto que desea buscar*\n\n- Ejemplo: ${usedPrefix + command} Minecraft`, m)
   let url = 'https://google.com/search?q=' + encodeURIComponent(text)
   let search = await googleIt({ query: text })
   let msg = search.map(({ title, link, snippet}) => {
     return `*${title}*\n_${link}_\n_${snippet}_`
-  }).join`\n\n`
+  }).join`\n\n─────────────────\n\n`
   try {
     let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).buffer()
     if (ss.includes('html')) throw ''
-    await conn.sendFile(m.chat, ss, 'screenshot.png', url + '\n\n' + msg, m)
+    await conn.sendFile(m.chat, ss, 'screenshot.png', '\t\t*‧ [ 🔎 Google Busqueda 🔎 ] ‧*\n\n─────────────────\n\n' + msg, m)
   } catch (e) {
     m.reply(msg)
   }
 }
-handler.help = ['google', 'googlef'].map(v => v + ' <pencarian>')
+handler.help = ['google', 'googlef'].map(v => v + '')
 handler.tags = ['internet']
-handler.command = /^googlef?$/i
+handler.command = /^(google|googlef)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
