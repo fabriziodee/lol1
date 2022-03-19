9957551fbdb15e71ce4425aed39132b668ef5ff1
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-19 13:15:41 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -7,7 +7,7 @@ let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 let handler = async (m, { conn, text }) => {
 let lolitest = fs.readFileSync('./storage/image/menu.jpg')
 const btn = [ { urlButton: { displayText: `𝙃𝙤𝙨𝙩𝙞𝙣𝙜 `, url: `https://jsrepos.com/lib/Ilhamskhyi-catzy` } }, { urlButton: { displayText: `𝙂𝙞𝙩𝙝𝙪𝙗 `, url: `https://github.com/Ilhamskhyi` } }, { urlButton: { displayText: `𝙙𝙚𝙫𝙚𝙡𝙤𝙥𝙚𝙧 `, url: `developer atau pengembang bot https://wa.me/+6287773710155` } }, { quickReplyButton: {displayText: '𝙛𝙖𝙨𝙩 𝙧𝙚𝙨𝙥𝙤𝙣𝙚 𝙗𝙤𝙩', id: '.ping '}}, { quickReplyButton: {displayText: '𝙘𝙤𝙢𝙢𝙖𝙣𝙙𝙚𝙧 ', id: '.allmenu'}} ]
-conn.sendMessage(m.chat, { caption: 'Test', footer, templateButtons: btn, image: lolitest }, { quoted: m })
+conn.sendMessage(m.chat, { caption: 'Test', footer: 'tesr', templateButtons: btn, image: lolitest }, { quoted: m })
 }
 
 handler.help = ['test']
