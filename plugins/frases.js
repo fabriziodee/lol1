5af2fde4b1435784c5bbd15646813e6f72d346ef
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-19 00:05:52 -0500

    Update frases.js

--- plugins/frases.js
+++ plugins/frases.js
@@ -3,6 +3,10 @@ let fetch = require('node-fetch')
 let res = await fetch("https://api-alc.herokuapp.com/api/other/frases?apikey=ConfuMods")
 let json = await res.json()
 let { frase } = json
-conn.reply(m.chat, `*┏━━━━━━━━━━━━━━━━┓*\n*┠❧  ${frase}*\n*┗━━━━━━━━━━━━━━━━┚*`, m)}
+conn.reply(m.chat, `“${frase}”`, m)}
+
+handler.help = ['frase']
+handler.tags = ['fun']
 handler.command = /^(frase|frases|frace|fraces)$/i
+
 module.exports = handler
