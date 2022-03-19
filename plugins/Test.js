9dbb46191113d4d9c277173124668d915efdca2f
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-19 13:27:54 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -5,22 +5,8 @@ let fs = require ('fs')
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 
 let handler = async (m, { conn, text }) => {
-let caption = 'test4'
-let footer = 'test5'
-const btnCover = [
-                    { quickReplyButton: { displayText: `Original Sound`, id: `test` } },
-                    { quickReplyButton: { displayText: `Extract Audio`, id: `test2` } },
-                    { urlButton: { displayText: `Download in Browser`, url: `test3` } }
-                ]
-                let buttonMessage = {
-                    caption,
-                    footer,
-                    templateButtons: btnCover,
-                    height: 'test6',
-                    width: 'test7',
-                    video: { url: 'test8' }
-                }
-                await conn.sendMessage(m.chat, buttonMessage, { quoted: m })
+const buttonsDefault = [ { urlButton: { displayText: `group whatsapp`, url: `https://chat.whatsapp.com/JtreUWX9m9dJaAff9Te1ae` } }, ]
+await conn.sendMessage(m.chat, { text: 'test', footer: 'test2', templateButtons: buttonsDefault }, { quoted: m })
 }
 
 handler.help = ['test']
