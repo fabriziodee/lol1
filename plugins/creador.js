ed93aa3bf7c48b5a59453851aa21084d171d415c
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-18 22:44:40 -0500

    Update and rename creator.js to creador.js

--- plugins/creador.js
+++ plugins/creador.js
@@ -2,6 +2,8 @@ function handler(m) {
 this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
 }
 
+handler.help = ['creador']
+handler.tags = ['info']
 handler.command = /^(contacto|owner|creator|creador|propietario|dueño)$/i
 
 module.exports = handler
