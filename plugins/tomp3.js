7e5f5396101e64e6cdae20835b52b3ad688c0b7c
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-19 12:19:19 -0500

    Update tomp3.js

--- plugins/tomp3.js
+++ plugins/tomp3.js
@@ -4,16 +4,16 @@ const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, usedPrefix, command }) => {
   let q = m.quoted ? m.quoted : m
   let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-  if (!/video|audio/.test(mime)) throw `*🔰 Responda a un video o nota de voz con el comando* *${usedPrefix + command}*`
+  if (!/video|audio/.test(mime)) throw `Etiqueta un audio o video para convertirlo en mp3!`
   let media = await q.download()
   let audio = await toAudio(media, 'mp4')
   conn.sendMessage(m.chat, audio, MessageType.audio, {
     quoted: m, mimetype: 'audio/mp4'
   })
 }
-handler.help = ['tomp3']
-handler.tags = ['General']
 
-handler.command = /^(tomp3|mp3)$/i
+handler.help = ['tomp3']
+handler.tags = ['tools']
+handler.command = /^(tomp3|mp3|amp3)$/i
 
 module.exports = handler
