cd397eaada61dcbdfddc3c2cc5d03128d4cc26fb
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-20 12:20:48 -0500

    Update and rename random_dog.js to random-perro.js

--- plugins/random-perro.js
+++ plugins/random-perro.js
@@ -1,24 +1,21 @@
-//created by Scooppt
 let fetch = require('node-fetch')
 
 let handler  = async (m, { conn, text }) => {
+conn.reply(m.chat, wait, m)
   try {
     let res = await fetch('https://random.dog/woof.json')
     let json = await res.json()
     if (json.status) throw json
-    let caption = `
-©The Shadow Brokers - Bot
-`.trim()
-    conn.sendFile(m.chat, json.url, 'dog.jpg', caption, m)
+    conn.sendFile(m.chat, json.url, 'perro.jpg', '*PERRO*', m)
    } catch (e) {
         console.log(e)
-        throw '_*Erro!*_'
+        throw error
     }
 }
 
-handler.help = ['dog']
+handler.help = ['perro']
 handler.tags = ['random']
-handler.command = /^dog$/i
+handler.command = /^(perro|dog)$/i
 
 handler.fail = null
 
