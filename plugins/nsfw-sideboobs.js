d36732275d9186594e81deac9eef3b55bafe0242
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-20 11:59:11 -0500

    Update and rename sideboobs.js to nsfw-sideboobs.js

--- plugins/nsfw-sideboobs.js
+++ plugins/nsfw-sideboobs.js
@@ -2,13 +2,15 @@ let axios = require('axios');
 let fetch = require('node-fetch')
 let neko = require('nekos.life')
 let Neko = new neko()
-     let handler  = async (m, { conn, args }) => {
-     json = (await axios.get('https://meme-api.herokuapp.com/gimme/sideboobs')).data
-   conn.sendFile(m.chat, json.url, 'boobs.jpg', json.title, m, false)
+
+let handler  = async (m, { conn, args }) => {
+conn.reply(m.chat, wait, m)
+json = (await axios.get('https://meme-api.herokuapp.com/gimme/sideboobs')).data
+conn.sendFile(m.chat, json.url, 'boobs.jpg', `*${json.title}*`, m, false)
 }
 handler.help = ['sideboobs']
-handler.tags = ['sange']
-handler.command = /^sideboobs$/i
+handler.tags = ['nsfw']
+handler.command = /^(sideboobs)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
