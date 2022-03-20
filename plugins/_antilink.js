46e92d8ec9bff10900381fe18560d91b1e5d6268
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-19 21:03:28 -0500

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -7,7 +7,7 @@ handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
   let chat = global.DATABASE.data.chats[m.chat]
   let isGroupLink = linkRegex.exec(m.text)
 
-  if (chat.antiLink && isGroupLink) {
+  if (chat.antilink && isGroupLink) {
     await this.sendMessage(m.chat, `\t\t\t\t*‧ [ ⭕ Enlace Detectado ⭕ ] ‧*\n\n• Nombre: @${m.sender.split("@s.whatsapp.net")[0]}\n• Fecha: ${date}\n• Enlace: WhatsApp.com`, MessageType.text, { quoted: m, contextInfo: { mentionedJid: [m.sender] } })
     if (isAdmin) return m.reply('Menos mal que eres un administrador')
     if (!isBotAdmin) return m.reply('Menos mal que no soy administradora del grupo')
