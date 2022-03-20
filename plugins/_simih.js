573d57d2ebee19552554cefa68ae1c1d1eaa1e76
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-20 15:36:08 -0500

    Update _simih.js

--- plugins/_simih.js
+++ plugins/_simih.js
@@ -1,1 +1,19 @@
-//Test
+let fetch = require('node-fetch')
+
+let handler = m => m
+
+handler.before = async (m) => {
+    if (m.isBaileys && m.fromMe) {
+        if (/^.*false|disable|(turn)?off|0/i.test(m.text)) return
+        if (!m.text) return
+        let res = await fetch(API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(m.text), lc: 'es' }))
+        if (!res.ok) return 
+        let json = await res.json()
+        if (json.success == "Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.") return m.reply('simi nya gk tau bang')
+        m.reply(json.success)
+        return !0
+    }
+    return !0
+}
+
+module.exports = handler
