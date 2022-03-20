27986b8db16777aaa3d4efbe37b33e16716b7a0c
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-20 15:17:14 -0500

    Update _simih.js

--- plugins/_simih.js
+++ plugins/_simih.js
@@ -1,1 +1,15 @@
-Test
+let handler = m => m
+
+handler.before = function (m, { user, text }) {
+  if (m.isBaileys && m.fromMe) {
+  let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "es" }, ''))
+  let json = await res.json()
+  if (json.success) m.reply(json.success)
+  else throw json
+}
+}
+
+module.exports = handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
