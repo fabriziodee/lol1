7d1969f1fcefec916adeb529c2e9ac770a1cf1e6
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-20 10:24:33 -0500

    Update togif.js

--- plugins/togif.js
+++ plugins/togif.js
@@ -1,17 +1,19 @@
 let { webp2mp4 } = require('../lib/webp2mp4')
 let handler = async (m, { conn, usedPrefix, command }) => {
-    if (!m.quoted) throw `*Responde a un video o sticker con el comando: ${usedPrefix + command}*`
+    if (!m.quoted) throw `Etiqueta un video o stickergif para convertirlo en GIF!`
     let mime = m.quoted.mimetype || ''
-    if (!/webp/.test(mime)) throw `*Responde a un video o sticker con el comando: ${usedPrefix + command}*`
+    if (!/webp/.test(mime)) throw `Etiqueta un video o stickergif para convertirlo en GIF!`
+    conn.reply(m.chat, wait, m)
     let media = await m.quoted.download()
     let out = Buffer.alloc(0)
     if (/webp/.test(mime)) {
         out = await webp2mp4(media)
     }
-    await conn.sendFile(m.chat, out, 'out.gif', '©The Shadow Brokers - Bot', m, 0, { mimetype: 'video/gif', thumbnail: Buffer.alloc(0) })
+    await conn.sendFile(m.chat, out, 'out.gif', '*Se convirtió a GIF*', m, 0, { mimetype: 'video/gif', thumbnail: Buffer.alloc(0) })
 }
+
 handler.help = ['togif']
 handler.tags = ['sticker']
-handler.command = ['togif','gif']
+handler.command = /^(togif|agif|gif)$/i
 
 module.exports = handler
