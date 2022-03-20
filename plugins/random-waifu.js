eba4fc4c08c78522c1c0c01ad64971eb2c08bad1
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-20 12:15:46 -0500

    Update random-waifu.js

--- plugins/random-waifu.js
+++ plugins/random-waifu.js
@@ -1,6 +1,7 @@
 let fetch = require('node-fetch')
 
 let handler = async(m, { conn }) => {
+  conn.reply(m.chat, wait, m)
   let res = await fetch('https://api.waifu.pics/sfw/waifu')
   if (!res.ok) throw await res.text()
   let json = await res.json()
