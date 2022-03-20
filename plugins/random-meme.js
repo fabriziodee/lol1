c4d72d96118917c84b13c091d8b27b771fff8b15
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-20 12:28:13 -0500

    Update and rename random_meme.js to random-meme.js

---
+++ plugins/random-meme.js
@@ -0,0 +1,15 @@
+const axios = require('axios')
+
+let handler = async(m, { conn, usedPrefix, command }) => {
+let res = await axios("https://meme-api.herokuapp.com/gimme/memesmexico")
+let json = res.data
+let ShadowBot = 
+conn.sendFile(m.chat, json.url, 'meme', `*${json.title}*`, m)
+}
+conn.sendButtonImg(m.chat, json.url, "*😂😂😂😂😂*", '©The Shadow Borkers - Bot', '😂 SIGUIENTE 😂', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+
+handler.help = ['meme']
+handler.tags = ['random']
+handler.command = /^(meme)$/i
+
+module.exports = handler
