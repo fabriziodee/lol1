e2fe2def78696db6969a9bffba2597bab3a4f060
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-20 12:14:56 -0500

    Update random-miku.js

--- plugins/random-miku.js
+++ plugins/random-miku.js
@@ -1,6 +1,7 @@
 let fetch = require('node-fetch')
 
 let handler  = async (m, { conn, usedPrefix, command }) => {
+conn.reply(m.chat, wait, m)
 heum = await fetch(`https://api-reysekha.herokuapp.com/api/wallpaper/miku?apikey=APIKEY`)
 json = await heum.buffer()
 conn.sendFile(m.chat, json, 'miku', `*MIKU*`, m)
