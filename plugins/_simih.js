3bc9113d8e71916af141154aeb5c767746177525
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-20 15:23:25 -0500

    Update _simih.js

--- plugins/_simih.js
+++ plugins/_simih.js
@@ -3,7 +3,7 @@ let handler = m => m
 
 handler.before = function (m, { user, text }) {
   if (m.isBaileys && m.fromMe) {
-  let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "es" }, ''))
+  let res = await fetch(`https://api.simsimi.net/v2/?text=${text}&lc=es`)
   let json = await res.json()
   if (json.success) m.reply(json.success)
   else throw json
