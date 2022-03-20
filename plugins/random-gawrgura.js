01b901e80ff38cac2eac000841f4f1db02c4c40e
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-20 12:09:46 -0500

    Update random-gawrgura.js

--- plugins/random-gawrgura.js
+++ plugins/random-gawrgura.js
@@ -1,9 +1,11 @@
 const axios = require('axios')
+
 let handler = async(m, { conn }) => {
 conn.reply(m.chat, wait, m)
 let les = await axios.get('https://meme-api.herokuapp.com/gimme/GawrGura')
-  conn.sendFile(m.chat, `${les.data.url}`, '', `*${les.data.title}*`, m)
-  }
+conn.sendFile(m.chat, `${les.data.url}`, '', `*${les.data.title}*`, m)
+}
+
 handler.help = ['gawrgura']
 handler.tags = ['random']
 handler.command = /^(gawrgura)$/i
