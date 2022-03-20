bf4b1b27686fd361f63f0a00192890d336ddc0e7
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-20 12:16:44 -0500

    Update random-wallpaper.js

--- plugins/random-wallpaper.js
+++ plugins/random-wallpaper.js
@@ -6,9 +6,9 @@ let les = await axios.get('https://meme-api.herokuapp.com/gimme/wallpaper')
 conn.sendFile(m.chat, les.data.url, 'wallpaper', `*${les.data.title}*`, m)
 }
 
-handler.help = ['rawallpaper']
+handler.help = ['wallpaper']
 handler.tags = ['random']
-handler.command = /^(randomwallpaper|rawallpaper|randomwall)$/i
+handler.command = /^(randomwallpaper|wallpaper|randomwall)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
