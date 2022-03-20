80e7e74b1624ec96b55c07795cf66b3eb032d40e
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-20 15:29:46 -0500

    Update _simih.js

--- plugins/_simih.js
+++ plugins/_simih.js
@@ -1,17 +1,1 @@
-let fetch = require('node-fetch')
-let handler = m => m
-
-handler.before = function (m, { user, text }) {
-  if (m.isBaileys && m.fromMe) {
-  	
-let res = await fetch(`https://api.simsimi.net/v2/?text=${text}&lc=es`)
-let json = await res.json()
-let { success } = json
-m.reply(`${success}`)
-}
-}
-
-module.exports = handler
-
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
+//Test
