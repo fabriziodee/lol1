05f176801706eb44c1cb1384a3382bdca1b3df84
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-20 22:44:03 -0500

    Update nsfw-pechos.js

--- plugins/nsfw-pechos.js
+++ plugins/nsfw-pechos.js
@@ -1,8 +1,8 @@
 const axios = require('axios')
 
 let handler = async(m, { conn, usedPrefix, command }) => {
-let chat = global.DATABASE.data.chats[m.chat]
-if (chat.isNsfw) {
+let chat = global.DATABASE._data.chats[m.chat]
+if (chat.nsfw) {
 conn.reply(m.chat, wait, m)
 let res = await axios("https://meme-api.herokuapp.com/gimme/boobs")
 let json = res.data
