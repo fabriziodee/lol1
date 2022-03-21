0a8da7bc99e6d8fb748ffaf30f04b29bd5aad5df
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-20 22:44:25 -0500

    Update nsfw-pussy.js

--- plugins/nsfw-pussy.js
+++ plugins/nsfw-pussy.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 
 let handler = async(m, { conn }) => {
 let chat = global.DATABASE.data.chats[m.chat]
-if (chat.isNsfw) {
+if (chat.nsfw) {
 conn.reply(m.chat, wait, m)
 let porn = await axios.get('https://meme-api.herokuapp.com/gimme/pussy')
 conn.sendFile(m.chat, `${porn.data.url}`, 'cuca', `*${porn.data.title}*`, m)
