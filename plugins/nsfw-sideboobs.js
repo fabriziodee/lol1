d54cffe26a7c1e83645982731d6cef67a4978740
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-20 22:49:31 -0500

    Update nsfw-sideboobs.js

--- plugins/nsfw-sideboobs.js
+++ plugins/nsfw-sideboobs.js
@@ -4,10 +4,14 @@ let neko = require('nekos.life')
 let Neko = new neko()
 
 let handler  = async (m, { conn, args }) => {
+let chat = global.DATABASE._data.chats[m.chat]
+if (chat.nsfw) {
 conn.reply(m.chat, wait, m)
 json = (await axios.get('https://meme-api.herokuapp.com/gimme/sideboobs')).data
 conn.sendFile(m.chat, json.url, 'boobs.jpg', `*${json.title}*`, m, false)
+} else m.reply('En este grupo no se permite el contenido +18')
 }
+
 handler.help = ['sideboobs']
 handler.tags = ['nsfw']
 handler.command = /^(sideboobs)$/i
