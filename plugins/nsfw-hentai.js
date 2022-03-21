1110ac71ec314ba31f866174d0c1fa825a8d932e
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-20 22:42:55 -0500

    Update nsfw-hentai.js

--- plugins/nsfw-hentai.js
+++ plugins/nsfw-hentai.js
@@ -2,8 +2,8 @@ let axios = require("axios")
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-let chat = global.DATABASE.data.chats[m.chat]
-if (chat.isNsfw) {
+let chat = global.DATABASE._data.chats[m.chat]
+if (chat.nsfw) {
 conn.reply(m.chat, wait, m)
 heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/hentai?apikey=apirey`)
 json = await heum.buffer()
