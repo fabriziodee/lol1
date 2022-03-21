5aaaadcb75b16151ab9e2a9050c5626a46502491
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-20 22:52:52 -0500

    Update and rename nsfw_yuri.js to nsfw-yuri.js

--- plugins/nsfw-yuri.js
+++ plugins/nsfw-yuri.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 
 let handler  = async (m, { conn, usedPrefix, command }) => {
 let chat = global.DATABASE.data.chats[m.chat]
-if (chat.isNsfw) {
+if (chat.nsfw) {
 conn.reply(m.chat, wait, m)
 heum = await fetch(`https://api-alc.herokuapp.com/api/nsfw/yuri2?apikey=ConfuMods`)
 json = await heum.buffer()
