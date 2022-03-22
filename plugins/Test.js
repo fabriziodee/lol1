8d936431ae0cbd55116cb415b458601a92707b3f
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-22 12:14:46 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -2,7 +2,6 @@ let { MessageType } = require("@adiwajshing/baileys")
 let fs = require ('fs')
 
 let handler = async (m, { conn, text }) => {
-let tumb = fs.readFileSync('./storage/image/menuvid.mp4')
 let tumbb = fs.readFileSync('./storage/image/menu2.jpg')
 let [[jid, { invite_code, invite_code_exp }]] = Object.entries(m.sender)
 conn.sendGroupV4Invite(m.sender, jid, invite_code, invite_code_exp, false, 'Test')
