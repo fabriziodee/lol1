0c5f2679048c34bbba6ab590e1a44aca1b4a9705
g4tito <84881966+g4tito@users.noreply.github.com>
2022-03-22 20:09:23 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -3,33 +3,7 @@ let fs = require ('fs')
 
 let handler = async (m, { conn, text, command, usedPrefix }) => {
 let tumbb = fs.readFileSync('./storage/image/menu2.jpg')
-let duelo = `
-*Slime*
-- Vida: 40
-
-*Jugador*
-- Vida: 100
-`.trim()
-      let _duelo = conn.prepareMessageFromContent(m.chat, {
-"listMessage":  {
-"title": "\t\t\t*‧ ⚔️ BATALLA RPG ⚔️ ‧*",
-"description": duelo,
-"buttonText": "Opciones",
-"listType": "SINGLE_SELECT",
-"sections": [
-{
-"rows": [
-{
-"title": "Atacar 🔪",
-"rowId": `${usedPrefix}selec atacar`
-},
-{
-"title": "Escapar 💨",
-"rowId": `${usedPrefix}selec escapar`
-}
-]}]}}, { quoted: m })
-conn.relayWAMessage(_duelo)
-
+//Test
 }
 
 handler.help = ['test']
