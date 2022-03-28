71b15115d694f15352d4ce79d6a9e1ca94d6e314
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-03-28 00:11:32 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -4,6 +4,27 @@ let fs = require ('fs')
 let handler = async (m, { conn, text, command, usedPrefix }) => {
 let tumbb = fs.readFileSync('./storage/image/menu2.jpg')
 //Test
+res = await conn.prepareMessageFromContent(m.chat, {
+    "productMessage": {
+      "product": {
+        "productImage": tumbb,
+        "productId": "4938174216214248",
+        "title": '✧───────···[ Menu ]···────────✧',
+        "description": `Test`,
+        "retailerId": `Test2`,
+        "url": '\n',
+        "descriptionCount": "999999999",
+        "productImageCount": "1",
+      },
+      "businessOwnerJid": "0@s.whatsapp.net",
+      "contextInfo": {
+        "forwardingScore": 9999,
+        "isForwarded": true
+      }
+    }
+  },
+    { quoted: m })
+  conn.relayWAMessage(res)
 }
 
 handler.help = ['test']
