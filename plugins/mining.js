d3562663ff307eb4fa02d4d51d2cb226ae0ccc78
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-03-28 14:34:14 -0500

    Update mining.js

--- plugins/mining.js
+++ plugins/mining.js
@@ -6,33 +6,37 @@ let handler = async (m, { conn, usedPrefix }) => {
     let __waktur = (new Date - global.DATABASE._data.users[m.sender].lastmining)
     let _waktur = (180000 - __waktur)
     let waktur = clockString(_waktur)
-    let hasil = (pickaxe == 1 ? Math.floor(Math.random() * 5) : '' || pickaxe == 2 ? Math.floor(Math.random() * 7) : '' || pickaxe == 3 ? Math.floor(Math.random() * 10) : '' || pickaxe == 4 ? Math.floor(Math.random() * 20) : '' || pickaxe == 5 ? Math.floor(Math.random() * 30) : '' )
-    let hasiiil = (pickaxe == 1 ? Math.floor(Math.random() * 20) : '' || pickaxe == 2 ? Math.floor(Math.random() * 30) : '' || pickaxe == 3 ? Math.floor(Math.random() * 40) : '' || pickaxe == 4 ? Math.floor(Math.random() * 50) : '' || pickaxe == 5 ? Math.floor(Math.random() * 60) : '' )
-    let hasiil = (pickaxe == 1 ? Math.ceil(Math.random() * 200) : '' || pickaxe == 2 ? Math.ceil(Math.random() * 250) : '' || pickaxe == 3 ? Math.ceil(Math.random() * 300) : '' || pickaxe == 4 ? Math.ceil(Math.random() * 350) : '' || pickaxe == 5 ? Math.ceil(Math.random() * 500) : '' )
-    let hasiiiil = (pickaxe == 1 ? Math.ceil(Math.random() * 200) : '' || pickaxe == 2 ? Math.ceil(Math.random() * 400) : '' || pickaxe == 3 ? Math.ceil(Math.random() * 600) : '' || pickaxe == 4 ? Math.ceil(Math.random() * 800) : '' || pickaxe == 5 ? Math.ceil(Math.random() * 1000) : '' )
-    let konz = Math.floor(Math.random() * 100)
+    let diamond = (pickaxe == 1 ? Math.floor(Math.random() * 5) : '' || pickaxe == 2 ? Math.floor(Math.random() * 7) : '' || pickaxe == 3 ? Math.floor(Math.random() * 10) : '' || pickaxe == 4 ? Math.floor(Math.random() * 20) : '' || pickaxe == 5 ? Math.floor(Math.random() * 30) : '' )
+    let gold = (pickaxe == 1 ? Math.floor(Math.random() * 10) : '' || pickaxe == 2 ? Math.floor(Math.random() * 17) : '' || pickaxe == 3 ? Math.floor(Math.random() * 25) : '' || pickaxe == 4 ? Math.floor(Math.random() * 40) : '' || pickaxe == 5 ? Math.floor(Math.random() * 55) : '' )
+    let iron = (pickaxe == 1 ? Math.floor(Math.random() * 20) : '' || pickaxe == 2 ? Math.floor(Math.random() * 30) : '' || pickaxe == 3 ? Math.floor(Math.random() * 40) : '' || pickaxe == 4 ? Math.floor(Math.random() * 50) : '' || pickaxe == 5 ? Math.floor(Math.random() * 60) : '' )
+    let stone = (pickaxe == 1 ? Math.ceil(Math.random() * 200) : '' || pickaxe == 2 ? Math.ceil(Math.random() * 250) : '' || pickaxe == 3 ? Math.ceil(Math.random() * 300) : '' || pickaxe == 4 ? Math.ceil(Math.random() * 350) : '' || pickaxe == 5 ? Math.ceil(Math.random() * 500) : '' )
+    let exp = (pickaxe == 1 ? Math.ceil(Math.random() * 200) : '' || pickaxe == 2 ? Math.ceil(Math.random() * 400) : '' || pickaxe == 3 ? Math.ceil(Math.random() * 600) : '' || pickaxe == 4 ? Math.ceil(Math.random() * 800) : '' || pickaxe == 5 ? Math.ceil(Math.random() * 1000) : '' )
+    let durability = Math.floor(Math.random() * 100)
     let exp = (Math.floor(Math.random() * 200) + (pickaxe * 70))
-    let goa = (pickRandom(['una cueva', 'un volcan', 'jupiter', 'saturno']))
-    let selesai = (pickRandom(['huuh', 'Selesai Juga', 'Kayaknya Sampah', 'Kayaknya Bagus', 'Perlu Upgrade pickaxe nih biar hasilnya bagus', 'Trash!', 'GG', 'Banyak Batu doang', 'Iron nya dikit', 'Diamond nya dikit', 'Bjir banyak Diamond', 'Bjir banyak Iron']))
-     
+    let goa = (pickRandom(['una cueva 🏔', 'un volcan 🌋', 'jupiter 🪨', 'saturno 🪐']))
+    
     if (pickaxe > 0) {
     if (global.DATABASE._data.users[m.sender].pickaxedurability > 99) {
     if (new Date - global.DATABASE._data.users[m.sender].lastmining > 180000) {
        
 global.DATABASE._data.users[m.sender].lastmining = new Date * 1
-global.DATABASE._data.users[m.sender].diamond += hasil * 1 
-global.DATABASE._data.users[m.sender].iron += hasiiil * 1 
-global.DATABASE._data.users[m.sender].batu += hasiil * 1 
-global.DATABASE._data.users[m.sender].pickaxedurability -= konz * 1
-global.DATABASE._data.users[m.sender].exp += hasiiiil * 1
+global.DATABASE._data.users[m.sender].diamond += diamond * 1 
+global.DATABASE._data.users[m.sender].gold += gold * 1 
+global.DATABASE._data.users[m.sender].iron += iron * 1 
+global.DATABASE._data.users[m.sender].stone += stone * 1 
+global.DATABASE._data.users[m.sender].pickaxedurability -= durability * 1
+global.DATABASE._data.users[m.sender].exp += exp * 1
 
           setTimeout(() => {
-          	m.reply(`Minaste en *${goa}* y obtienes:
+          	m.reply(`*Minaste en ${goa} y obtienes*
           
-- Diamante: ${hasil}
-- Hiero: ${hasiiil}
-- Piedra: ${hasiil}
-- Exp: ${hasiiiil}`)
+- Diamante: ${diamond}
+- Oro: ${gold}
+- Hiero: ${iron}
+- Piedra: ${stone}
+- Exp: ${exp}
+
+*⛏Durabilidad:* -${durability}`)
           }, 0)
           
             } else m.reply(`Te quedaste sin energía vuelve dentro de *${waktur}*`)
