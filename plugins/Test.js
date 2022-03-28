3cfd9b3cb70f19a7b7736a8cd6435e6ebef8263a
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-03-28 00:18:29 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -4,27 +4,25 @@ let fs = require ('fs')
 let handler = async (m, { conn, text, command, usedPrefix }) => {
 let tumbb = fs.readFileSync('./storage/image/menu2.jpg')
 //Test
-let res = await conn.prepareMessageFromContent(m.chat, {
-    "productMessage": {
-      "product": {
-        "productImage": tumbb,
-        "productId": "4938174216214248",
-        "title": '✧───────···[ Menu ]···────────✧',
-        "description": `Test`,
-        "retailerId": `Test2`,
-        "url": '\n',
-        "descriptionCount": "999999999",
-        "productImageCount": "1",
-      },
-      "businessOwnerJid": "0@s.whatsapp.net",
-      "contextInfo": {
-        "forwardingScore": 9999,
-        "isForwarded": true
-      }
-    }
-  },
-    { quoted: m })
-  conn.relayWAMessage(res)
+imeg = await conn.prepareMessage(m.chat, tumbb, 'imageMessage')
+imeu = imeg.message.imageMessage
+conn.relayWAMessage(await conn.prepareMessageFromContent(m.chat, {productMessage: {
+businessOwnerJid: '51940617554@s.whatsapp.net',
+product: {
+productId: '1011',
+productImage:imeu,
+title: 'HALO SAYA SELFBOT',
+description:  `Silahkan Ketik #menu`,
+currencyCode: 'IDR',
+priceAmount1000: '10000000',
+retailerId: '0507',
+url: 'https://wa.me/51940617554',
+productImageCount: 1,
+salePriceAmount1000: '7.8000',
+}
+}
+}, { quoted: m }))
+
 }
 
 handler.help = ['test']
