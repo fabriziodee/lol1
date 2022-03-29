9ae64e093eda2147f01dd304e99043a34644e9a3
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-03-28 22:45:12 -0500

    Create rpg-vender.js

---
+++ plugins/rpg-vender.js
@@ -0,0 +1,36 @@
+let { MessageType, MentionedJid } = require("@adiwajshing/baileys")
+let fs = require('fs')
+
+const Sdiamond = 750
+
+let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner, isPrems }) => {
+
+let chat = global.DATABASE._data.chats[m.chat]
+let user = global.DATABASE._data.users[m.sender]
+
+let type = (args[0] || ' ').toLowerCase()
+
+const count = args[2] && args[2].length > 0 ? Math.min(99999999, Math.max(parseInt(args[2]), 1)) : !args[2] || args.length < 4 ? 1 :Math.min(1, count)
+
+switch (type) {
+case 'diamante':
+      if (global.DATABASE._data.users[m.sender].diamond >= count * 1) {
+      	global.DATABASE._data.users[m.sender].diamond -= count * 1
+      	global.DATABASE._data.users[m.sender].money += Sdiamond * count
+          conn.reply(m.chat, `Vendiste ${count} de diamante por ${Sdiamond * count} de dinero`, m)
+          } else conn.reply(m.chat, 'No tienes suficientes diamantes para vender', m)
+      break
+
+    default:
+let sell = `
+//Test
+`.trim()
+return conn.reply(m.chat, sell, m)
+  }
+}
+
+handler.help = ['vender']
+handler.tags = ['rpg']
+handler.command = /^(vender|sell)$/i
+
+module.exports = handler
