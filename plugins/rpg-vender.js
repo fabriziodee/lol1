f898a7e25b126839c8b85b7df9e9a0d1d851deb7
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-03-30 14:15:24 -0500

    Update rpg-vender.js

--- plugins/rpg-vender.js
+++ plugins/rpg-vender.js
@@ -6,8 +6,10 @@ const Sgold = 350
 const Siron = 50
 const Sstone = 4
 const Soak = 5
+
 const Stropicalfish = 57
-const Sxx = 750
+const Sblowfish = 300
+const Scommonfish = 3
 
 let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner, isPrems }) => {
 
@@ -15,8 +17,10 @@ let chat = global.DATABASE._data.chats[m.chat]
 let user = global.DATABASE._data.users[m.sender]
 
 let type = (args[0] || ' ').toLowerCase()
+let _type = (args[1] || ' ').toLowerCase()
 
 const count = args[1] && args[1].length > 0 ? Math.min(99999999, Math.max(parseInt(args[1]), 1)) : !args[1] || args.length < 3 ? 1 : Math.min(1, count)
+const _count = args[2] && args[2].length > 0 ? Math.min(99999999, Math.max(parseInt(args[2]), 1)) : !args[2] || args.length < 4 ? 1 :Math.min(1, count)
 
 switch (type) {
 case 'diamante':
@@ -60,13 +64,33 @@ case 'madera':
           } else conn.reply(m.chat, 'No tienes suficiente madera, para vender', m)
       break
 
-case 'pes tropical':
+case 'pez':
+switch (_type) {
 case 'tropical':
-      if (global.DATABASE._data.users[m.sender].tropicalfish >= count * 1) {
-      	global.DATABASE._data.users[m.sender].tropicalfish -= count * 1
-      	global.DATABASE._data.users[m.sender].money += Stropicalfish * count
-          conn.reply(m.chat, `Vendiste ${count} peces tropical por ${Stropicalfish * count} de dinero`, m)
+      if (global.DATABASE._data.users[m.sender].tropicalfish >= _count * 1) {
+      	global.DATABASE._data.users[m.sender].tropicalfish -= _count * 1
+      	global.DATABASE._data.users[m.sender].money += Stropicalfish * _count
+          conn.reply(m.chat, `Vendiste ${_count} peces tropical por ${Stropicalfish * _count} de dinero`, m)
           } else conn.reply(m.chat, 'No tienes suficientes peces tropicales, para vender', m)
+      break
+      
+case 'globo':
+      if (global.DATABASE._data.users[m.sender].blowfish >= _count * 1) {
+      	global.DATABASE._data.users[m.sender].blowfish -= _count * 1
+      	global.DATABASE._data.users[m.sender].money += Sblowfish * _count
+          conn.reply(m.chat, `Vendiste ${_count} peces tropical por ${Sblowfish * _count} de dinero`, m)
+          } else conn.reply(m.chat, 'No tienes suficientes peces tropicales, para vender', m)
+      break
+      
+case 'comun':
+      if (global.DATABASE._data.users[m.sender].commonfish >= _count * 1) {
+      	global.DATABASE._data.users[m.sender].commonfish -= _count * 1
+      	global.DATABASE._data.users[m.sender].money += Scommonfish * _count
+          conn.reply(m.chat, `Vendiste ${_count} peces tropical por ${Scommonfish * _count} de dinero`, m)
+          } else conn.reply(m.chat, 'No tienes suficientes peces tropicales, para vender', m)
+      break
+      
+      }
       break
 
 
@@ -82,6 +106,8 @@ let sell = `
 - Hierro
 - Piedra
 - Pez tropical
+- Pez comun
+- Pez globo
 `.trim()
 return conn.reply(m.chat, sell, m)
   }
