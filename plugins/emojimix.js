e22dca36c0ea549278ef99968909084d66353026
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-03-31 09:19:22 -0500

    Update emojimix.js

--- plugins/emojimix.js
+++ plugins/emojimix.js
@@ -1,9 +1,9 @@
 const { MessageType } = require('@adiwajshing/baileys')
-let fetch = require('node-fetch')
 const { sticker } = require('../lib/sticker')
+let fetch = require('node-fetch')
 let fs = require("fs")
 
-let handler = async (m, { conn, text, args }) => {
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 if (!args[0]) throw `*Ingrese dos emojis*\m\n- Ejemplo: ${usedPrefix + command} 🐱+👻`
 let [emoji1, emoji2] = text.split`+`
 let anu = await (await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)).json()
