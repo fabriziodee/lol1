68c55a286b7fcdf540c3b146f63de3cdc39bea50
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-03-30 23:02:45 -0500

    Update _antiviewonce.js

--- plugins/_antiviewonce.js
+++ plugins/_antiviewonce.js
@@ -3,7 +3,7 @@ let handler = m => m
 handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
         let q = m.quoted ? m.quoted : m
         if (q.mtype == 'viewOnceMessage') {
-            await this.copyNForward(m.chat, await this.loadMessage(m.chat, q.id), false, { quoted: m, readViewOnce: true })
+            await this.copyNForward(m.chat, await this.loadMessage(m.chat, q.id), false, { readViewOnce: true })
         }
 }
 
