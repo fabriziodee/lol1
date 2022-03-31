d54bfdbaa43aa78e49dd27375c890063eeb8c6c4
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-03-31 08:51:27 -0500

    Create emojimix.js

---
+++ plugins/emojimix.js
@@ -0,0 +1,17 @@
+let fetch = require('node-fetch')
+let fs = require("fs")
+
+let handler = async (m, { conn, text, args }) => {
+if (!args[0]) throw `*Ingrese dos emojis*\m\n- Ejemplo: ${usedPrefix + command} 🐱+👻`
+   	let [emoji1, emoji2] = text.split`+`
+		let anu = await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
+		let emix = anu.results[0].media_formats.png_transparent.url
+		    let encmedia = await conn.sendImageAsSticker(m.chat, emix, m, { packname: global.packname, author: global.author, categories: res.tags })
+		    await fs.unlinkSync(encmedia)
+		
+}
+handler.help = ['emojimix']
+handler.tags = ['sticker']
+handler.command = /^(emojimix|emix)$/i
+
+module.exports = handler
