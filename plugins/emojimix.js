7aae566ccdd06bc3d39b847ae275c14f7040ab96
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-03-31 09:15:44 -0500

    Update emojimix.js

--- plugins/emojimix.js
+++ plugins/emojimix.js
@@ -3,13 +3,15 @@ let fs = require("fs")
 
 let handler = async (m, { conn, text, args }) => {
 if (!args[0]) throw `*Ingrese dos emojis*\m\n- Ejemplo: ${usedPrefix + command} 🐱+👻`
-   	let [emoji1, emoji2] = text.split`+`
-                let anu = await (await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)).json()
-		let emix = anu.results[0].media_formats.png_transparent.url
-                m.reply(`${emix}`)
-                conn.sendFile(m.chat, emix, 'emojimix', '', m)
+let [emoji1, emoji2] = text.split`+`
+let anu = await (await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)).json()
+let emix = anu.results[0].media_formats.png_transparent.url
+conn.sendFile(m.chat, emix, 'emojimix', '', m)
+let stiker = await sticker(null, emix, global.packname, global.author)
+conn.sendMessage(m.chat, stiker, MessageType.sticker, { quoted: m })
 		
 }
+
 handler.help = ['emojimix']
 handler.tags = ['sticker']
 handler.command = /^(emojimix|emix)$/i
