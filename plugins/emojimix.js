a391e24e932bad642f169d3fe0c2d27aad421df0
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-03-31 09:01:58 -0500

    Update emojimix.js

--- plugins/emojimix.js
+++ plugins/emojimix.js
@@ -4,8 +4,9 @@ let fs = require("fs")
 let handler = async (m, { conn, text, args }) => {
 if (!args[0]) throw `*Ingrese dos emojis*\m\n- Ejemplo: ${usedPrefix + command} 🐱+👻`
    	let [emoji1, emoji2] = text.split`+`
-		let anu = await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
-		let emix = anu.results[0].media_formats.png_transparent.url
+                let anu = await (await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)).json()
+		let emix = anu.locale
+                m.reply(`${emix}`)
 		    let encmedia = await conn.sendImageAsSticker(m.chat, emix, m, { packname: global.packname, author: global.author, categories: res.tags })
 		    await fs.unlinkSync(encmedia)
 		
