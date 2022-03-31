ce338bbdc3800b51aa47828586999b31879afc16
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-03-31 01:02:19 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -4,7 +4,7 @@ let fs = require ('fs')
 let handler = async (m, { conn, text, command, usedPrefix }) => {
 let tumbb = fs.readFileSync('./storage/image/menu2.jpg')
 //Test
-conn.sendMessage(from, 'Test', MessageType.text, { quoted: { id: "F5D54EDF35F5DFA26A3E67FF8C96F578" }, thumbnail : tumbb })
+conn.sendMessage(m.chat, 'Test', MessageType.text, { quoted: { id: "F5D54EDF35F5DFA26A3E67FF8C96F578" }, thumbnail : tumbb })
 }
 
 handler.help = ['test']
