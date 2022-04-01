7de9f49fd74655b63906294aeb64482732aba3d3
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-01 14:27:11 -0500

    Update getmsg.js

--- plugins/getmsg.js
+++ plugins/getmsg.js
@@ -6,9 +6,9 @@ let handler = async (m, { conn, command, usedPrefix, text }) => {
     let _m = await conn.serializeM(msgs[text])
     await _m.copyNForward(m.chat, true)
 }
-handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'sticker'].map(v => 'get' + v + ' <text>')
+handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'sticker'].map(v => 'get' + v + '')
 handler.tags = ['database']
-handler.command = /^ver(vn|msg|video|audio|img|sticker)$/
+handler.command = /^get(vn|msg|video|audio|img|sticker)$/
 handler.rowner = true
 
 module.exports = handler
