2fd5412e710cdbd0e6799aafa41484e68fa13795
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-01 09:04:30 -0500

    Update reportar-bug.js

--- plugins/reportar-bug.js
+++ plugins/reportar-bug.js
@@ -1,11 +1,11 @@
 const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async(m, { conn, text }) => {
-	let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
+    let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
     if (!teks) return conn.reply(m.chat, 'Ingrese un error/bug o etiqueta uno!', m)
     if (text > 300) return conn.reply(m.chat, 'El texto es demasiado largo, máximo 300 caracteres!', m)
     var nomor = m.sender
-    const teks1 = `*_❒═════[ REPORTE ]═════╾❒_*\n*_┬_*\n*_├Numero: wa.me/${nomor.split("@s.whatsapp.net")[0]}_*\n*_┴_*\n*_┬_*\n*_├Mensaje: ${text}_*\n*_┴_*`
+    const teks1 = `*_❒═════[ REPORTE ]═════╾❒_*\n*_┬_*\n*_├Numero: wa.me/${nomor.split("@s.whatsapp.net")[0]}_*\n*_┴_*\n*_┬_*\n*_├Mensaje: ${teks}_*\n*_┴_*`
     conn.sendMessage('51940617554@s.whatsapp.net', teks1, MessageType.text)
     conn.reply(m.chat, 'El problema se envió al creador de la bot', m)
 }
