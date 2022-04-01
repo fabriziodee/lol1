72949ade0f5c827375408b6d315475e0a6cd8872
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-03-31 21:50:02 -0500

    Update mediafire.js

--- plugins/mediafire.js
+++ plugins/mediafire.js
@@ -1,4 +1,20 @@
-const { mdfireDl } = require('./lib/mediafire.js')
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const mdfireDl = async (url) => {
+const res = await axios.get(url) 
+const $ = cheerio.load(res.data)
+const hasil = []
+const link = $('a#downloadButton').attr('href')
+const size = $('a#downloadButton').text().replace('Download', '').replace('(', '').replace(')', '').replace('\n', '').replace('\n', '').replace('                         ', '')
+const seplit = link.split('/')
+const nombre = seplit[5]
+mime = nombre.split('.')
+mime = mime[1]
+hasil.push({ nombre, mime, size, link })
+return hasil
+}
+
 
 let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
 if (!text) return m.reply(`Kirim perintah ${usedPrefix + command} *link mediafire*`)
