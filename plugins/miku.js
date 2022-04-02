4e86a3d90e3e81c6136a0d2d8e487d3ab8a255c5
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-02 18:41:26 -0500

    Create miku.js

---
+++ plugins/miku.js
@@ -0,0 +1,13 @@
+let fetch = require('node-fetch')
+
+let handler  = async (m, { conn, usedPrefix, command }) => {
+let heum = await fetch(`https://server-api-rey.herokuapp.com/api/wallpaper/miku?apikey=apirey`)
+let json = await heum.buffer()
+conn.sendFile(m.chat, json, 'miku', '*MIKU*', m, false)
+}
+
+handler.help = ['miku']
+handler.tags = ['random']
+handler.command = /^(miku)$/i
+
+module.exports = handler
