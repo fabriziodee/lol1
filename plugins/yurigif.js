354515aabdd8693026667725536b3c20457dc98b
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-02 11:53:03 -0500

    Update yurigif.js

--- plugins/yurigif.js
+++ plugins/yurigif.js
@@ -1,10 +1,11 @@
 const axios = require('axios')
- let handler = async(m, { conn }) => {
+
+let handler = async(m, { conn }) => {
 let les = await axios.get('https://meme-api.herokuapp.com/gimme/yurigif')
-            conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
+conn.sendFile(m.chat, `${les.data.url}`, 'yuri.gif', `*${les.data.title}*`, m)
   }
 handler.help = ['yurigif']
-handler.tags = ['General']
+handler.tags = ['nsfw']
 handler.command = /^(yurigif)$/i
 handler.owner = false
 handler.mods = false
