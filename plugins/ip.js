7875b9a80e7937a7f178f72fa706b66fbf0ad542
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-01 20:39:30 -0500

    Create ip.js

---
+++ plugins/ip.js
@@ -0,0 +1,48 @@
+let { MessageType } = require("@adiwajshing/baileys")
+let axios = require("axios");
+let handler = async(m, { conn, text }) => {
+
+if (!text) return conn.reply(m.chat, 'Masukan Alamat IP yang akan dicek', m)
+let ip = await getJson(`http://ip-api.com/json/${text}`)
+if(ip.status == 'fail') return reply('*ip incorrecta*')
+let iptext = `*🔍Ip:* _${text}_
+
+      *Latitud de ip*: ${ip.lat}
+      *Longitud de ip*: ${ip.lon}
+
+🌆 *País*: _${ip.country}_
+      *Código de país*: ${ip.countryCode}
+
+🏡 *Región*: _${ip.region}_
+      *Nombre de región*: ${ip.regionName}
+
+🏙️  *Ciudad*: _${ip.city}_
+
+📚 *Código postal*: _${ip.zip}_
+
+🕐 *Zona horaria*: _${ip.timezone}_
+
+🌐 *Proveedor de servicios de Internet*:
+_${ip.isp}_
+
+🕋 *Organización*: _${ip.org}_ 
+conn.reply(m.chat, iptext, m)
+    
+}
+handler.help = ['ip']
+handler.tags = ['tools']
+handler.command = /^(ip)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+handler.limit = false
+
+module.exports = handler
