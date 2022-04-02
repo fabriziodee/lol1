eceb311e809d4bf1655d40c2b854497280faf93c
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-02 17:47:29 -0500

    Update pinterest.js

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -1,14 +1,20 @@
 let fetch = require('node-fetch')
-let handler  = async (m, { conn, args, text }) => {
-if (!text) throw '*[❗] Que desea buscar?*\n*Inserte un texto*'
-if (text) m.reply('*[ ✔️ ] Buscando, aguarde un momento....*\n\n*[❗] Si llega a dar algun problema intente de nuevo con otro texto*')
+
+let handler  = async (m, { conn, args, text, usedPrefix, command }) => {
+if (!text) throw `*Que imagen desea buscar*\n\n- Ejemplo: ${usedPrefix + command} minecraft`
+conn.reply(m.chat, wait, m)
 heum = await require('../lib/scraper').pinterest(text)
 json = await heum
 random = json.result[Math.floor(Math.random() * json.result.length)]
-if (json.result.length == 0) return conn.sendFile(m.chat, './Menu2.jpg', 'error not found', 'ERROR 404 NOT FOUND', m)
+if (json.result.length == 0) return conn.reply(m.chat, error, m)
 data = await fetch('https://api.imgbb.com/1/upload?key=c93b7d1d3f7a145263d4651c46ba55e4&image='+random).then(v => v.json())
 get = await conn.getFile(data.data.url)
 conn.sendMessage(m.chat, get.data, 'imageMessage', { quoted: m, mimetype: get.mime, caption: data.data.url })}
-handler.command = /^pinterest$/i
+
+handler.help = ['pinterest']
+handler.tags = ['internet']
+handler.command = /^(pinterest)$/i
+
 handler.fail = null
+
 module.exports = handler
