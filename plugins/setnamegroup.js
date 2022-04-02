d3dded2d748402b222974cfaea1c735402e012b0
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-02 14:35:45 -0500

    Update setnamegroup.js

--- plugins/setnamegroup.js
+++ plugins/setnamegroup.js
@@ -1,17 +1,16 @@
 let { GroupSettingChange, mentionedJid } = require('@adiwajshing/baileys')
 
-let handler = async (m, { conn, text, usedPrefix }) => {
-
- if (!text) return conn.reply(m.chat, 'Ingrese el nombre del grupo!', m)
-
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+ if (!text) return conn.reply(m.chat, `*Ingrese el nombre del grupo*\n\n- Ejemplo: ${usedPrefix + command} lolibot`, m)
     let id = m.key.remoteJid
     let idgrup = `${id.split("@s.whatsapp.net")[0]}`
     conn.groupUpdateSubject(idgrup, text)
-conn.reply(m.chat, 'Se cambió el nombre del grupo!', m)
+    conn.reply(m.chat, 'Se cambió el nombre del grupo!', m)
 }
+
 handler.help = ['setnamegroup']
 handler.tags = ['group']
-handler.command = /^(setnamegroup)$/i
+handler.command = /^(setnamegroup|setnamegp|setnamegc)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
