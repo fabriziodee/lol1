744b5185e3b4b5d430ac3fec68bbe81e500af55d
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-03 11:29:55 -0500

    Update imgweb.js

--- plugins/imgweb.js
+++ plugins/imgweb.js
@@ -1,10 +1,13 @@
 let fetch = require('node-fetch')
+
 let handler = async (m, { conn, text, usedPrefix, command, args }) => {
-  if (!args[0]) return conn.reply(m.chat, '*Porfavor ingresa un url de una imagen*', m)
+  conn.reply(m.chat, wait, m)
+  if (!args[0]) return conn.reply(m.chat, `*Ingrese el link de una imagen para descargar*\n\n- Ejemplo: ${usedPrefix + command} https://cdn.nekos.life/ero/ero02.png`, m)
   let url = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
   let ss = await (await fetch(text)).buffer()
   conn.sendFile(m.chat, ss, 'error.png', url, m)
 }
+
 handler.help = ['imgweb']
 handler.tags = ['internet']
 handler.command = /^(imgweb)$/i
