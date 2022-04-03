12152be0be8b2fe21597075dd8eecee527522750
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-03 14:13:01 -0500

    Update banchat.js

--- plugins/banchat.js
+++ plugins/banchat.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, participants }) => {
 // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
-if (!(m.chat in global.DATABASE._data.chats)) return m.reply('Este chat no está registrado en la base de datos!')
+if (!(m.chat in global.DATABASE._data.chats)) return m.reply('Este grupo no está registrado en la base de datos!')
 let chat = global.DATABASE._data.chats[m.chat]
-if (chat.isBanned) return m.reply('Este grupo ya está baneado!')
+if (chat.isBanned) return m.reply('Este grupo ya está muteado!')
 chat.isBanned = true
-m.reply('\t\t*‧ 📫 Chat baneado 📫 ‧*\n\nAhora nadie podrá utilizar los comandos')
+m.reply('*Grupo muteado 🔇*\n\nAhora nadie podrá utilizar ningún comando')
 // } else m.reply('Aquí hay un número de un host...')
 }
 
