86035f3b312caa9f0a9eb4474d990f2b4208a59b
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-03 11:31:50 -0500

    Update and rename imgweb.js to image-web.js

--- plugins/image-web.js
+++ plugins/image-web.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, text, usedPrefix, command, args }) => {
-  conn.reply(m.chat, wait, m)
   if (!args[0]) return conn.reply(m.chat, `*Ingrese el link de una imagen para descargar*\n\n- Ejemplo: ${usedPrefix + command} https://cdn.nekos.life/ero/ero02.png`, m)
+  conn.reply(m.chat, wait, m)
   let url = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
   let ss = await (await fetch(text)).buffer()
   conn.sendFile(m.chat, ss, 'error.png', url, m)
