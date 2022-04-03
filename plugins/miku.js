63632f35d438a81be05c9f546641efc9caec4554
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-02 19:09:24 -0500

    Update miku.js

--- plugins/miku.js
+++ plugins/miku.js
@@ -1,6 +1,7 @@
 let fetch = require('node-fetch')
 
 let handler  = async (m, { conn, usedPrefix, command }) => {
+conn.reply(m.chat, wait, m)
 let heum = await fetch(`https://server-api-rey.herokuapp.com/api/wallpaper/miku?apikey=apirey`)
 let json = await heum.buffer()
 conn.sendFile(m.chat, json, 'miku', '*MIKU*', m, false)
