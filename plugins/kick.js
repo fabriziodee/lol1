42fc7cbe7d2bd54de9d9dca4c771e4f6b3e9c940
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-03 23:06:12 -0500

    Update kick.js

--- plugins/kick.js
+++ plugins/kick.js
@@ -1,10 +1,11 @@
 let handler = async (m, { command, text }) => {
   let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
+  if (user) return m.reply('Etiqueta a alguien del grupo para eliminar!')
   conn.groupRemove(m.chat, [user])
   }
 
 handler.help = ['kick']
-handler.tags = ['test']
+handler.tags = ['adm']
 handler.command = /^(kick)$/i
 
 module.exports = handler
