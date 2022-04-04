b9ded0d7306e9c665550c4d2d3cc78666f46df1a
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-03 19:53:20 -0500

    Update ephoto.3.js

--- plugins/ephoto.3.js
+++ plugins/ephoto.3.js
@@ -1,10 +1,17 @@
 const { ephoto3 } = require('../lib/ephoto3.js')
-let handler = async(m, { conn, args, usedPrefix, text}) => {
-if (!text) throw '*Y el texto..?*\n*Ingrese un texto*\n\n*Ejemplo:*\n*#logocorazon T y S*\n\n*Nota: El texto no puede ser mayor a 15 letras*'
-if (text.length > 15) throw '*Uhm.. texto demasiado largo, ingrese un texto menor a 15 caracteres*'
+
+let handler = async(m, { conn, args, usedPrefix, command, text}) => {
+if (!text) throw `*Ingrese un texto para hacer el logo*\n\n- Ejemplo: ${usedPrefix + command} M y G`
+if (text.length > 15) throw 'El texto es demasiado largo, mínimo 15 caracteres!'
 let result = await ephoto3('https://en.ephoto360.com/text-heart-flashlight-188.html', `${text}`)
 let uh = `https://s1.ephoto360.com${result.image}`
-conn.sendFile(m.chat, uh,'p.jpg', '*_Dicen que le brillan los ojos cuando habla de ti_*', m,false, { thumbnail: Buffer.alloc(0) })}
-handler.command = /^logocorazon|logocorazón|logoheart$/i
+conn.sendFile(m.chat, uh,'p.jpg', `*LOGO CORAZÓN*\n*Texto:* ${text}`, m)
+}
+
+handler.help = ['logocorazón']
+handler.tags = ['maker']
+handler.command = /^(logocorazon|logocorazón)$/i
+
 handler.fail = null
+
 module.exports = handler
