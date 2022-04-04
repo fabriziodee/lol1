2a6c31972e41315b0b14c6af39e277555ac56c81
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-03 23:19:10 -0500

    Update add.js

--- plugins/add.js
+++ plugins/add.js
@@ -31,19 +31,15 @@ let handler = async (m, { conn, text, participants, usedPrefix, command }) => {
     } : {})
   }
 }
-handler.help = ['add', '+'].map(v => v + ' número')
-handler.tags = ['admin']
-handler.command = /^(add|agregar|añadir|\+)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
+
+handler.help = ['add']
+handler.tags = ['adm']
+handler.command = /^(add|agregar|añadir|)$/i
 handler.group = true
 handler.private = false
-
 handler.admin = true
 handler.botAdmin = true
 
 handler.fail = null
-handler.limit = false
 
 module.exports = handler
