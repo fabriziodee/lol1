c62e2d5f53272b0fcc1af412ced8cb4015fc2a72
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-05 06:48:32 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -6,13 +6,13 @@ let tumbb = fs.readFileSync('./storage/image/menu2.jpg')
 //Test
   let teks = m.quoted ? m.quoted : m.quoted
   if (!teks) throw 'Etiqueta un mensaje para enviar a todos los chats!'
-  let chats = conn.chats.all().filter(v => !v.read_only && v.message).map(v => v.jid)
+  //let chats = conn.chats.all().filter(v => !v.read_only && v.message).map(v => v.jid)
   let faketumb = fs.readFileSync('./storage/image/menu2.jpg')
   let fakereply = { key : { participant : '0@s.whatsapp.net' }, message: { orderMessage: { itemCount : 2022, status: 1, surface : 1, message: '↷✦╎lolibot╎💌˖ ⸙', orderTitle: "↷✦╎lolibot╎💌˖ ⸙", thumbnail: faketumb, sellerJid: '0@s.whatsapp.net' } } }
   //let content = (/bcgc|broadcastgroup|bcgrup|bcgrup|broadcastgc/i.test(text) ? text : text + '\n' + readMore + '「 ' + conn.getName(conn.user.jid) + ' Mensaje de difusion en grupos 」')
   await conn.copyNForward("573218938795@s.whatsapp.net", await conn.loadMessage(m.chat, m.quoted.id), false, { quoted: fakereply })
   //conn.sendMessage(id, content, m.mtype, m.msg.contextInfo ? { contextInfo: m.msg.contextInfo } : {})
-  conn.reply(m.chat, `El mensaje se envío a *${chats.length} chats*!`, m)
+  m.reply('El mensaje se envío @573218938795!')
 }
 
 //handler.help = ['test']
