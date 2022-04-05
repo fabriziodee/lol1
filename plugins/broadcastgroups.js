9c9391d784ee2bf30ad1db7b676aa341c514edc9
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-04 22:30:24 -0500

    Update broadcastgroups.js

--- plugins/broadcastgroups.js
+++ plugins/broadcastgroups.js
@@ -1,7 +1,8 @@
 let handler  = async (m, { conn, text }) => {
   let groups = conn.chats.all().filter(v => v.jid.endsWith('g.us') && !v.read_only && v.message).map(v => v.jid)
+  let member = (await conn.groupMetadata(target)).participants.map(v => v.jid)
   //let content = (/bcgc|broadcastgroup|bcgrup|bcgrup|broadcastgc/i.test(text) ? text : text + '\n' + readMore + '「 ' + conn.getName(conn.user.jid) + ' Mensaje de difusion en grupos 」')
-  for (let id of groups) await conn.copyNForward(id, await conn.loadMessage(m.chat, m.quoted.id), true)
+  for (let id of groups) await conn.copyNForward(id, await conn.loadMessage(m.chat, m.quoted.id), true, { contextInfo: { mentionedJid: member }})
   //conn.sendMessage(id, content, m.mtype, m.msg.contextInfo ? { contextInfo: m.msg.contextInfo } : {})
   conn.reply(m.chat, `El mensaje se envío a *${groups.length} grupos*!`, m)
 }
