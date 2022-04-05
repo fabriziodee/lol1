99e19d5bdcdd9056e0ac377125494a905e09a7de
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-05 00:37:21 -0500

    Create runtime.js

---
+++ plugins/runtime.js
@@ -0,0 +1,35 @@
+let handler = async (m, { usedPrefix, command }) => {
+let uptime = process.uptime()
+//let time = require('moment-timezone').tz('Asia/Kolkata').format('HH:mm:ss')
+let runnya = `\t\t\t∙ ☀️ *Runtime* ☀️ ∙
+
+• *Hora:* ${time}
+• *Tiempo activa:* ${runtime(uptime)}`
+await m.reply(runnya)
+}
+
+handler.help = ['runtime']
+handler.tags = ['info']
+handler.command = /^(uptime|runtime)$/i
+
+module.exports = handler
+
+const runtime = function (seconds) {
+  seconds = Number(seconds);
+  var d = Math.floor(seconds / (3600 * 24));
+  var h = Math.floor((seconds % (3600 * 24)) / 3600);
+  var m = Math.floor((seconds % 3600) / 60);
+  var s = Math.floor(seconds % 60);
+  var dDisplay = d > 0 ? d + (d == 1 ? " dia, " : " Dias, ") : "";
+  var hDisplay = h > 0 ? h + (h == 1 ? " hora, " : " Horas, ") : "";
+  var mDisplay = m > 0 ? m + (m == 1 ? " minuto, " : " Minutos, ") : "";
+  var sDisplay = s > 0 ? s + (s == 1 ? " segundo" : " Segundos") : "";
+  return dDisplay + hDisplay + mDisplay + sDisplay;
+};
+
+let time = d.toLocaleString('en-US', { 
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      hour12: true 
+    })
