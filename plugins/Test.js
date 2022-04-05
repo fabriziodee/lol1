97bc2cdfdd62852b7de5866aad45c85df5286556
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-05 07:09:19 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -8,7 +8,7 @@ let tumbb = fs.readFileSync('./storage/image/menu2.jpg')
   if (!teks) throw 'Etiqueta un mensaje para enviar a todos los chats!'
   //let chats = conn.chats.all().filter(v => !v.read_only && v.message).map(v => v.jid)
   let faketumb = fs.readFileSync('./storage/image/menu2.jpg')
-  let fakereply = { key : { participant : '0@s.whatsapp.net' }, message: { orderMessage: { itemCount : 2022, status: 1, surface : 1, message: '↷✦╎lolibot╎💌˖ ⸙', orderTitle: "↷✦╎lolibot╎💌˖ ⸙", thumbnail: faketumb, sellerJid: '0@s.whatsapp.net' } } }
+  let fakereply = { key : { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ?  { remoteJid: "51940617554-1621199330@g.us" } : {}) }, message: { orderMessage: { itemCount : 2022, status: 1, surface : 1, message: '↷✦╎lolibot╎💌˖ ⸙', orderTitle: "↷✦╎lolibot╎💌˖ ⸙", thumbnail: faketumb, sellerJid: '0@s.whatsapp.net' } } }
   //let content = (/bcgc|broadcastgroup|bcgrup|bcgrup|broadcastgc/i.test(text) ? text : text + '\n' + readMore + '「 ' + conn.getName(conn.user.jid) + ' Mensaje de difusion en grupos 」')
   await conn.copyNForward("51940617554-1600359399@g.us", await conn.loadMessage(m.chat, m.quoted.id), false, { quoted: fakereply })
   //conn.sendMessage(id, content, m.mtype, m.msg.contextInfo ? { contextInfo: m.msg.contextInfo } : {})
