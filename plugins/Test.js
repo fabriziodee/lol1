995449c1e506c0f15ad820bd96cdd95ed08b4697
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-05 13:47:44 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -1,18 +1,15 @@
-let { MessageType } = require("@adiwajshing/baileys")
 let fs = require ('fs')
 
-let handler = async (m, { conn, text, command, usedPrefix }) => {
-let tumbb = fs.readFileSync('./storage/image/menu2.jpg')
-//Test
+let handler  = async (m, { conn, text, participants }) => {
   let teks = m.quoted ? m.quoted : m.quoted
-  if (!teks) throw 'Etiqueta un mensaje para enviar a todos los chats!'
-  //let chats = conn.chats.all().filter(v => !v.read_only && v.message).map(v => v.jid)
+  if (!teks) throw 'Etiqueta un mensaje para enviar a grupos!'
+  //let groups = conn.chats.all().filter(v => v.jid.endsWith('g.us') && !v.read_only && v.message).map(v => v.jid)
   let faketumb = fs.readFileSync('./storage/image/menu2.jpg')
-  let fakereply = { key : { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ?  { remoteJid: "51940617554-1621199330@g.us" } : {}) }, message: { orderMessage: { itemCount : 2022, status: 1, surface : 1, message: '↷✦╎lolibot╎💌˖ ⸙', orderTitle: "↷✦╎lolibot╎💌˖ ⸙", thumbnail: faketumb, sellerJid: '0@s.whatsapp.net' } } }
+  let fakereply = { key : { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ?  { remoteJid: "51940617554-1621199330@g.us" }, message: { orderMessage: { itemCount : 2022, status: 1, surface : 1, message: '↷✦╎Anuncio a Grupos╎💌˖ ⸙', orderTitle: "↷✦╎Anuncio a Grupos╎💌˖ ⸙", thumbnail: faketumb, sellerJid: '0@s.whatsapp.net' } } }
   //let content = (/bcgc|broadcastgroup|bcgrup|bcgrup|broadcastgc/i.test(text) ? text : text + '\n' + readMore + '「 ' + conn.getName(conn.user.jid) + ' Mensaje de difusion en grupos 」')
-  await conn.copyNForward("573218938795@s.whatsapp.net", await conn.loadMessage(m.chat, m.quoted.id), false, { quoted: fakereply })
-  //conn.sendMessage(id, content, m.mtype, m.msg.contextInfo ? { contextInfo: m.msg.contextInfo } : {})
-  m.reply('El mensaje se envío!')
+  conn.copyNForward(id, await conn.loadMessage(m.chat, m.quoted.id), false, { quoted: fakereply, contextInfo: { mentionedJid: (await conn.groupMetadata("120363020974888410@g.us")).participants.map(v => v.jid) }})
+  //conn.sendMessage(m.chat, content, m.mtype, m.msg.contextInfo ? { contextInfo: m.msg.contextInfo } : {})
+  conn.reply(m.chat, `El mensaje se envío a *${groups.length} grupos*!`, m)
 }
 
 //handler.help = ['test']
@@ -20,4 +17,10 @@ let tumbb = fs.readFileSync('./storage/image/menu2.jpg')
 handler.command = /^(test)$/i
 handler.rowner = true
 
+handler.fail = null
+
 module.exports = handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
