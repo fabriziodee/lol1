785b4af29ec62c53d405b29236bd77a2adca6c9b
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-04 22:24:36 -0500

    Update broadcastgroups.js

--- plugins/broadcastgroups.js
+++ plugins/broadcastgroups.js
@@ -3,11 +3,12 @@ let handler  = async (m, { conn, text }) => {
   //let content = (/bcgc|broadcastgroup|bcgrup|bcgrup|broadcastgc/i.test(text) ? text : text + '\n' + readMore + '「 ' + conn.getName(conn.user.jid) + ' Mensaje de difusion en grupos 」')
   for (let id of groups) await conn.copyNForward(id, await conn.loadMessage(m.chat, m.quoted.id), true)
   //conn.sendMessage(id, content, m.mtype, m.msg.contextInfo ? { contextInfo: m.msg.contextInfo } : {})
-  conn.reply(m.chat, `_Envíando el mensaje de difusión a ${groups.length} grupos_`, m)
+  conn.reply(m.chat, `El mensaje se envío a *${groups.length} grupos*!`, m)
 }
-handler.help = ['broadcastgroup','bcgc'].map(v => v + ' <teks>')
+
+handler.help = ['bcgc']
 handler.tags = ['owner']
-handler.command = /^(broadcast|bc)(group|grup|gc)$/i
+handler.command = /^(broadcastgroup|bcgc)$/i
 handler.rowner = true
 
 handler.fail = null
