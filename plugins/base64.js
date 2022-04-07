4d33381759608b2ad730b69598db0b880c3f1283
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-07 16:25:35 -0500

    Update base64.js

--- plugins/base64.js
+++ plugins/base64.js
@@ -1,9 +1,11 @@
-let handler = async (m, { command, text }) => {
-  let txt = m.quoted ? m.quoted.text ? m.quoted.text : text ? text : m.text : m.text
+let handler = async (m, { usedPrefix, command, text }) => {
+  let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text 
+  if (!teks) throw `*Ingrese un texto o etiqueta uno*\n\n- Ejemplo: ${usedPrefix + command} gato`
   m.reply(Buffer.from(txt, 'utf-8').toString('base64'))
 }
+
 handler.help = ['base64']
 handler.tags = ['tools']
-handler.command = /^base64$/i
+handler.command = /^(base64)$/i
 
 module.exports = handler
