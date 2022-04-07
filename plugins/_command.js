08e79a30d1ef1deae2b0091f2716eecc00e5e09e
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-07 08:41:54 -0500

    Create _command.js

---
+++ plugins/_command.js
@@ -0,0 +1,10 @@
+let handler = m => m
+
+handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
+        //let q = m.quoted ? m.quoted : m
+        if (command == command) {
+            m.reply('Test comando no registrado')
+        }
+}
+
+module.exports = handler
