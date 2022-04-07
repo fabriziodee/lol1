0ad38493529d043843bdb37cfda84d41c6037c4c
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-07 16:31:46 -0500

    Update tovideo.js

--- plugins/tovideo.js
+++ plugins/tovideo.js
@@ -1,18 +1,20 @@
 const { MessageType } = require('@adiwajshing/baileys')
 const { toVideo } = require('../lib/converter')
+
 let handler  = async (m, { conn, args }) => {
   try {
-    let q = m.quoted ? m.quoted : m
+      let q = m.quoted ? m.quoted : m
+      if (!q) throw 'Etiqueta un sticker con movimiento para convertirlo a Gif!'
       let img = await q.download()
       convert = await toVideo(img, 'webp')
       conn.sendMessage(m.chat, convert, MessageType.video, { quoted: m})
     } catch {
-    m.reply('Conversation failed')
+    m.reply('La conversación falló!')
    }
 }
-handler.help = ['tovideo (reply stiker)']
-handler.tags = ['General']
-handler.command = /^tovideo|tomp4$/i
+handler.help = ['tovideo']
+handler.tags = ['sticker']
+handler.command = /^(tovideo|tovid|tomp4|amp4)$/i
 
 module.exports = handler
 
