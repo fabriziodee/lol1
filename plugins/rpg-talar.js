a8a5f5fe2bc4997ed2d27b35ed7d866cfbe5469e
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-08 00:14:17 -0500

    Update rpg-talar.js

--- plugins/rpg-talar.js
+++ plugins/rpg-talar.js
@@ -2,10 +2,12 @@ let handler = async (m, { conn, usedPrefix }) => {
 	
 	let user = global.DATABASE._data.users[m.sender]
 	let axe = global.DATABASE._data.users[m.sender].axe
-	let pdurability = global.DATABASE._data.users[m.sender].axedurability
+	let adurability = global.DATABASE._data.users[m.sender].axedurability
     let __waktur = (new Date - global.DATABASE._data.users[m.sender].lastcutting)
     let _waktur = (180000 - __waktur)
     let waktur = clockString(_waktur)
+    //let jungle = (axe == 1 ? Math.floor(Math.random() * 5) : '' || axe == 2 ? Math.floor(Math.random() * 7) : '' || axe == 3 ? Math.floor(Math.random() * 10) : '' || axe == 4 ? Math.floor(Math.random() * 20) : '' || axe == 5 ? Math.floor(Math.random() * 30) : '' )
+    //let birch = (axe == 1 ? Math.floor(Math.random() * 10) : '' || axe == 2 ? Math.floor(Math.random() * 17) : '' || axe == 3 ? Math.floor(Math.random() * 25) : '' || axe == 4 ? Math.floor(Math.random() * 40) : '' || axe == 5 ? Math.floor(Math.random() * 55) : '' )
     let oak = (axe == 1 ? Math.ceil(Math.random() * 100) : '' || axe == 2 ? Math.ceil(Math.random() * 150) : '' || axe == 3 ? Math.ceil(Math.random() * 200) : '' || axe == 4 ? Math.ceil(Math.random() * 250) : '' || axe == 5 ? Math.ceil(Math.random() * 350) : '' )
     let exp = (axe == 1 ? Math.ceil(Math.random() * 200) : '' || axe == 2 ? Math.ceil(Math.random() * 400) : '' || axe == 3 ? Math.ceil(Math.random() * 600) : '' || axe == 4 ? Math.ceil(Math.random() * 800) : '' || axe == 5 ? Math.ceil(Math.random() * 1000) : '' )
     let durability = Math.floor(Math.random() * 100)
@@ -14,8 +16,10 @@ let handler = async (m, { conn, usedPrefix }) => {
     if (axe > 0) {
     if (global.DATABASE._data.users[m.sender].axedurability > 99) {
     if (new Date - global.DATABASE._data.users[m.sender].lastcutting > 180000) {
-       
-global.DATABASE._data.users[m.sender].lastcutting = new Date * 1
+     
+global.DATABASE._data.users[m.sender].lastcutting = new Date * 1 
+//global.DATABASE._data.users[m.sender].jungle += jungle * 1 
+//global.DATABASE._data.users[m.sender].birch += birch * 1 
 global.DATABASE._data.users[m.sender].oak += oak * 1 
 global.DATABASE._data.users[m.sender].axedurability -= durability * 1
 global.DATABASE._data.users[m.sender].exp += exp * 1
@@ -34,11 +38,16 @@ global.DATABASE._data.users[m.sender].exp += exp * 1
      } else m.reply(`Todavía no tienes una hacha, compralo escribiendo ${usedPrefix}comprar hacha`)
  }
 
+//Update
+//- Roble: ${oak}
+//- Abedul: ${birch}
+//- Jungla: ${jungle}
+//- Exp: ${exp}
 
 handler.help = ['talar']
 handler.tags = ['rpg']
-handler.command = /^(cut|talar)$/i
 
+handler.command = /^(cut|talar)$/i
 handler.disabled = false
 
 module.exports = handler
