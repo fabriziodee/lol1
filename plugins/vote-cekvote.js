4d156ea8ad1ee25aaa824137f8c830a092069419
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-07 19:02:12 -0500

    Update vote-cekvote.js

--- plugins/vote-cekvote.js
+++ plugins/vote-cekvote.js
@@ -15,7 +15,7 @@ ${upvote.map(u => '- @' + u.split('@')[0]).join('\n')}
 
 *Votos en contra (❌)*
 Total: ${devote.length} votos
-${devote.map(u => '- @' + u.split('@')[0]).join('\n')}}`
+${devote.map(u => '- @' + u.split('@')[0]).join('\n')}`
     conn.send2Button(m.chat, vt, `Si quiere eliminar la votación\nescriba *${usedPrefix}delvote*`, '✅ Si', `${usedPrefix}upvote`, '❌ No', `${usedPrefix}devote`, m, { contextInfo: { mentionedJid } })
 }
 
