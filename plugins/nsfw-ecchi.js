0d2c94deb20a3a8386e636bb9c27f228882552bc
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-09 14:20:20 -0500

    Update nsfw-ecchi.js

--- plugins/nsfw-ecchi.js
+++ plugins/nsfw-ecchi.js
@@ -2,12 +2,11 @@ const axios = require('axios')
 
 let handler = async(m, { conn, usedPrefix, command }) => {
 let chat = global.DATABASE.data.chats[m.chat]
-if (chat.nsfw) {
+if (chat.nsfw) return nsfw
 conn.reply(m.chat, wait, m)
 let res = await axios("https://meme-api.herokuapp.com/gimme/ecchi")
 let json = res.data
 conn.sendFile(m.chat, json.url, 'ecchi', '*ECCHI*', m)
-} else m.reply('En este grupo no se permite el contenido +18')
 }
 
 handler.help = ['ecchi']
