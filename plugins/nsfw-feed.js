3b71ddf400c6914d921085535bdb21c76a3632ff
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-09 14:37:24 -0500

    Update nsfw-feed.js

--- plugins/nsfw-feed.js
+++ plugins/nsfw-feed.js
@@ -1,7 +1,7 @@
 let axios = require("axios")
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-let chat = global.DATABASE.data.chats[m.chat]
+let chat = global.DATABASE._data.chats[m.chat]
 if (!chat.nsfw) return m.reply(global.nsfw)
 conn.reply(m.chat, wait, m)
 let res = await axios("https://nekos.life/api/v2/img/feet")
