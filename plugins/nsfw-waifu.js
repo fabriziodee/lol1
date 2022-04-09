9679d5e44ff74a516f0e7338a9300a9b22c02cb6
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-09 14:44:48 -0500

    Update nsfw-waifu.js

--- plugins/nsfw-waifu.js
+++ plugins/nsfw-waifu.js
@@ -1,18 +1,16 @@
 let axios = require("axios")
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-let chat = global.DATABASE.data.chats[m.chat]
-if (chat.nsfw) {
+let chat = global.DATABASE._data.chats[m.chat]
+if (!chat.nsfw) return m.reply(global.nsfw)
 conn.reply(m.chat, wait, m)
 let res = await axios("https://api.waifu.pics/nsfw/waifu")
 let json = res.data
 conn.sendFile(m.chat, json.url, 'hwaifu', '*WAIFU*', m)
-} else m.reply('En este grupo no se permite el contenido +18')
 }
 
 handler.help = ['hwaifu']
 handler.tags = ['nsfw']
 handler.command = /^(hwaifu)$/i
-handler.limit = true
 
 module.exports = handler
