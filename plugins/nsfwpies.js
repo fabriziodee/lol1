4061099f6b1a13b4a3993b9cb6d2f3cb427c9096
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-09 15:05:52 -0500

    Update nsfwpies.js

--- plugins/nsfwpies.js
+++ plugins/nsfwpies.js
@@ -1,7 +1,16 @@
 let fetch = require('node-fetch')
+
 let handler  = async (m, { conn, usedPrefix, command }) => {
+let chat = global.DATABASE._data.chats[m.chat]
+if (!chat.nsfw) return m.reply(global.nsfw)
+conn.reply(m.chat, wait, m)
 heum = await fetch(`https://api-alc.herokuapp.com/api/nsfw/foot?apikey=ConfuMods`)
 json = await heum.buffer()
-conn.sendButtonImg(m.chat, json, '*Disfrutalo!!*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)}
-handler.command = /^(pies)$/i
+conn.sendFile(m.chat, json, 'pies', '*PIES*', m)
+}
+
+handler.help = ['pies']
+handler.tags = ['nsfw']
+handler.command = /^(pies|patas)$/i
+
 module.exports = handler
