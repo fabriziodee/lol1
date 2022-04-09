ad2e33fbb77a3ecd073796769cc23cc074c910fd
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-09 14:47:43 -0500

    Update nsfwbdsm.js

--- plugins/nsfwbdsm.js
+++ plugins/nsfwbdsm.js
@@ -1,9 +1,14 @@
 let fetch = require('node-fetch')
-     let handler  = async (m, { conn, usedPrefix, command }) => {
-    heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/bdsm?apikey=apirey`)
-    json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '*Disfrutalo!!*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
+
+let handler  = async (m, { conn, usedPrefix, command }) => {
+let chat = global.DATABASE._data.chats[m.chat]
+if (!chat.nsfw) return m.reply(global.nsfw)
+conn.reply(m.chat, wait, m)
+heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/bdsm?apikey=apirey`)
+json = await heum.buffer()
+conn.sendButtonImg(m.chat, json, '*Disfrutalo!!*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
 }
+
 handler.command = /^(bdsm|nsfwbdsm)$/i
 
 module.exports = handler
