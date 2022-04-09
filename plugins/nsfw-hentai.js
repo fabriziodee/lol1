d6afbfeeaeab30a3bd217b811976ad970107b531
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-09 14:29:42 -0500

    Update nsfw-hentai.js

--- plugins/nsfw-hentai.js
+++ plugins/nsfw-hentai.js
@@ -2,18 +2,16 @@ let axios = require("axios")
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-let chat = global.DATABASE._data.chats[m.chat]
-if (chat.nsfw) {
+let chat = global.DATABASE.data.chats[m.chat]
+if (!chat.nsfw) return m.reply(global.nsfw)
 conn.reply(m.chat, wait, m)
 heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/hentai?apikey=apirey`)
 json = await heum.buffer()
 conn.sendFile(m.chat, json, 'hentai', '*HENTAI*', m)
-} else m.reply('En este grupo no se permite el contenido +18')
 }
 
 handler.help = ['hentai']
 handler.tags = ['nsfw']
 handler.command = /^(hentai|hentay)$/i
-handler.limit = true
 
 module.exports = handler
