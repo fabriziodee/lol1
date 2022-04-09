75a51137edb24af1ce95d18a660e5ec76ea8acf0
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-09 13:06:15 -0500

    Update getplugin.js

--- plugins/getplugin.js
+++ plugins/getplugin.js
@@ -2,7 +2,7 @@ let handler = async (m, { usedPrefix, command, text }) => {
     let ar = Object.keys(plugins)
     let ar1 = ar.map(v => v.replace('.js', ''))
     if (!text) throw `*Ingrese en nombre de un archivo plugin*\n\n- Ejemplo: ${usedPrefix + command} menu`
-    if (!ar1.includes(text)) return m.reply(`'${text}' tidak ditemukan!\n\n${ar1.map(v => ' ' + v).join`\n`}`)
+    if (!ar1.includes(text)) return m.reply(`No se encontro el plugin *${text}*!\n\nLista de plugins disponibles:\n${ar1.map(v => '- ' + v).join`\n`}`)
     m.reply(require('fs').readFileSync('./plugins/' + text + '.js', 'utf-8'))
 }
 
