8dfc8e2960116ebed526c13ea2d05120905b20e3
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-09 14:41:04 -0500

    Update nsfw-pechos.js

--- plugins/nsfw-pechos.js
+++ plugins/nsfw-pechos.js
@@ -2,12 +2,11 @@ const axios = require('axios')
 
 let handler = async(m, { conn, usedPrefix, command }) => {
 let chat = global.DATABASE._data.chats[m.chat]
-if (chat.nsfw) {
+if (!chat.nsfw) return m.reply(global.nsfw)
 conn.reply(m.chat, wait, m)
 let res = await axios("https://meme-api.herokuapp.com/gimme/boobs")
 let json = res.data
 conn.sendFile(m.chat, json.url, 'pechos', '*PECHOS*', m)
-} else m.reply('En este grupo no se permite el contenido +18')
 }
 
 handler.help = ['pechos']
