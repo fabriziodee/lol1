4cf11fa388d920695c3373b90ac9d527b7af49c4
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-09 14:45:34 -0500

    Update nsfw-yaoi.js

--- plugins/nsfw-yaoi.js
+++ plugins/nsfw-yaoi.js
@@ -2,28 +2,14 @@ const axios = require('axios')
 
 let handler = async(m, { conn }) => {
 let chat = global.DATABASE._data.chats[m.chat]
-if (chat.nsfw) {
+if (!chat.nsfw) return m.reply(global.nsfw)
 conn.reply(m.chat, wait, m)
 let les = await axios.get('https://meme-api.herokuapp.com/gimme/yaoi')
 conn.sendFile(m.chat, les.data.url, 'yaoi', `*YAOI*`, m)
-} else m.reply('En este grupo no se permite el contenido +18')
 }
 
 handler.help = ['yaoi']
 handler.tags = ['nsfw']
 handler.command = /^(yaoi)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-handler.register = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
-handler.limit = false
 
 module.exports = handler
