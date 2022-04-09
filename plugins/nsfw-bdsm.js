e7fdd43ac0122a76f9a62b937144e42e70927409
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-09 14:50:46 -0500

    Update and rename nsfwbdsm.js to nsfw-bdsm.js

--- plugins/nsfw-bdsm.js
+++ plugins/nsfw-bdsm.js
@@ -4,11 +4,13 @@ let handler  = async (m, { conn, usedPrefix, command }) => {
 let chat = global.DATABASE._data.chats[m.chat]
 if (!chat.nsfw) return m.reply(global.nsfw)
 conn.reply(m.chat, wait, m)
-heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/bdsm?apikey=apirey`)
-json = await heum.buffer()
-conn.sendButtonImg(m.chat, json, '*Disfrutalo!!*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
+let heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/bdsm?apikey=apirey`)
+let json = await heum.buffer()
+conn.sendFile(m.chat, json, 'bdsm', '*BDSM*', m)
 }
 
+handler.help = ['bdsm']
+handler.tags = ['nsfw']
 handler.command = /^(bdsm|nsfwbdsm)$/i
 
 module.exports = handler
