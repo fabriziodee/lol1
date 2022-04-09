5d168c963fd69253ce46f89f6485fe8e110429ee
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-09 14:41:51 -0500

    Update nsfw-pussy.js

--- plugins/nsfw-pussy.js
+++ plugins/nsfw-pussy.js
@@ -2,25 +2,14 @@ const axios = require('axios')
 
 let handler = async(m, { conn }) => {
 let chat = global.DATABASE._data.chats[m.chat]
-if (chat.nsfw) {
+if (!chat.nsfw) return m.reply(global.nsfw)
 conn.reply(m.chat, wait, m)
 let porn = await axios.get('https://meme-api.herokuapp.com/gimme/pussy')
 conn.sendFile(m.chat, `${porn.data.url}`, 'cuca', `*${porn.data.title}*`, m)
-} else m.reply('En este grupo no se permite el contenido +18')
 }
 
 handler.help = ['pussy']
 handler.tags = ['nsfw']
 handler.command = /^(concha|vajina|cuca|pussy)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-handler.register = false
-handler.admin = false
-handler.botAdmin = false
-handler.fail = null
-handler.limit = false
 
 module.exports = handler
