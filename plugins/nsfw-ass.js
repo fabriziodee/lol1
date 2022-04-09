487e148ffb1ce906fddd4df271d03866f9f7392e
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-09 14:27:18 -0500

    Update nsfw-ass.js

--- plugins/nsfw-ass.js
+++ plugins/nsfw-ass.js
@@ -2,12 +2,11 @@ let fetch = require('node-fetch')
 
     let handler  = async (m, { conn, usedPrefix, command }) => {
     let chat = global.DATABASE.data.chats[m.chat]
-    if (chat.nsfw) {
+    if (!chat.nsfw) return m.reply(global.nsfw)
     conn.reply(m.chat, wait, m)
     heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/ass?apikey=apirey`)
     json = await heum.buffer()
     conn.sendFile(m.chat, json, 'ass', '*ASS*', m)
-} else m.reply(nsfw)
 }
 
 handler.help = ['ass']
