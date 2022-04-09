531edcdf0be8f59e1dbcb86e40b985ae371d89f0
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-09 14:40:20 -0500

    Update nsfw-neko.js

--- plugins/nsfw-neko.js
+++ plugins/nsfw-neko.js
@@ -1,18 +1,16 @@
 let axios = require("axios")
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-let chat = global.DATABASE.data.chats[m.chat]
-if (chat.nsfw) {
+let chat = global.DATABASE._data.chats[m.chat]
+if (!chat.nsfw) return m.reply(global.nsfw)
 conn.reply(m.chat, wait, m)
 let res = await axios("https://api.waifu.pics/nsfw/neko")
 let json = res.data
 conn.sendFile(m.chat, json.url, 'hneko', '*NEKO*', m)
-} else m.reply('En este grupo no se permite el contenido +18')
 }
 
 handler.help = ['hneko']
 handler.tags = ['nsfw']
 handler.command = /^(hneko)$/i
-handler.limit = true
 
 module.exports = handler
