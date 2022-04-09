423ee67155b30b16025fe0c18d21abf393e50bff
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-09 14:44:14 -0500

    Update and rename nsfw-trap2.js to nsfw-trapito2.js

--- plugins/nsfw-trapito2.js
+++ plugins/nsfw-trapito2.js
@@ -1,18 +1,16 @@
 let axios = require("axios")
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-let chat = global.DATABASE.data.chats[m.chat]
-if (chat.nsfw) {
+let chat = global.DATABASE._data.chats[m.chat]
+if (!chat.nsfw) return m.reply(global.nsfw)
 conn.reply(m.chat, wait, m)
 let res = await axios("https://api.waifu.pics/nsfw/trap")
 let json = res.data
 conn.sendFile(m.chat, json.url, 'trapito2', '*UN TRAPITO <3*', m)
-} else m.reply('En este grupo no se permite el contenido +18')
 }
 
 handler.help = ['trapito2']
 handler.tags = ['nsfw']
 handler.command = /^(trapito2|trap2|htrap2)$/i
-handler.limit = true
 
 module.exports = handler
