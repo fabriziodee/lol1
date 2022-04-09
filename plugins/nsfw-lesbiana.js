31ecaaac59f77b83f79a0cf2c7ae0f6bd5e4b10f
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-09 14:30:23 -0500

    Update nsfw-lesbiana.js

--- plugins/nsfw-lesbiana.js
+++ plugins/nsfw-lesbiana.js
@@ -2,17 +2,16 @@ const axios = require('axios')
 
 let handler = async(m, { conn, usedPrefix, command }) => {
 let chat = global.DATABASE.data.chats[m.chat]
-if (chat.nsfw) {
+let chat = global.DATABASE.data.chats[m.chat]
+if (!chat.nsfw) return m.reply(global.nsfw)
 conn.reply(m.chat, wait, m)
 let res = await axios("https://meme-api.herokuapp.com/gimme/lesbians")
 let json = res.data
 conn.sendFile(m.chat, json.url, 'lesbiana', '*LESBIANA*', m)
-} else m.reply('En este grupo no se permite el contenido +18')
 }
 
 handler.help = ['lesbiana']
 handler.tags = ['nsfw']
 handler.command = /^(lesbiana)$/i
-handler.limit = true
 
 module.exports = handler
