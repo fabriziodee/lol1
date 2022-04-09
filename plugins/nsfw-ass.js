908a08be0e73c8ecc5b9c34067327b8563217e32
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-09 14:16:19 -0500

    Update nsfw-ass.js

--- plugins/nsfw-ass.js
+++ plugins/nsfw-ass.js
@@ -6,8 +6,8 @@ let fetch = require('node-fetch')
     conn.reply(m.chat, wait, m)
     heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/ass?apikey=apirey`)
     json = await heum.buffer()
-    conn.sendFile(m.chat, json, 'wass', '*WASS*', m)
-} else m.reply('En este grupo no se permite el contenido +18')
+    conn.sendFile(m.chat, json, 'ass', '*ASS*', m)
+} else m.reply(nsfw)
 }
 
 handler.help = ['ass']
