c2da97eef373544cea66cf4726ba29f43db8fc52
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-09 14:31:54 -0500

    Update nsfw-nalgas.js

--- plugins/nsfw-nalgas.js
+++ plugins/nsfw-nalgas.js
@@ -2,7 +2,6 @@ const axios = require('axios')
 
 let handler = async(m, { conn, usedPrefix, command }) => {
 let chat = global.DATABASE._data.chats[m.chat]
-let chat = global.DATABASE.data.chats[m.chat]
 if (!chat.nsfw) return m.reply(global.nsfw)
 conn.reply(m.chat, wait, m)
 let res = await axios("https://meme-api.herokuapp.com/gimme/booty")
