0b786b037b7cb7bd9f84240181f35b843a96bbd0
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-09 22:22:59 -0500

    Update tag-contactall.js

--- plugins/tag-contactall.js
+++ plugins/tag-contactall.js
@@ -2,10 +2,12 @@ let handler = async function (m, { conn, participants }) {
   let list = []
   for (let i of participants.map(u => u.jid)) {
     //let users = participants.map(u => u.jid)
+    let _bio = conn.getStatus(`${i.split('@')[0]}@c.us`)
+    let bio = _bio.status == 401 ? 'Sin info' : _bio.status
     let name = conn.getName(i)
     list.push({
       "displayName": name,
-      "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:;${name};;;\nFN:${name}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Test\nEND:VCARD`
+      "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:;${name};;;\nFN:${name}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:${bio}\nEND:VCARD`
     })
   }
   await conn.sendMessage(m.chat, {
