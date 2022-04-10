34d6a428f6eced23d0b6e78f579e716b366aa24a
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-10 11:26:58 -0500

    Update scircle.js

--- plugins/scircle.js
+++ plugins/scircle.js
@@ -3,24 +3,28 @@ const { sticker } = require('../lib/sticker')
 const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn, text }) => {
+ let qq = m.quoted ? m.quoted : m
+ let mimee = (qq.msg || qq).mimetype || '' 
+ if (!/image/g.test(mimee)) throw 'Etiqueta una imagen para convertirlo a sticker círculo!'
  try {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw '*_Sin imagen_*'
-  if (!/image\/(jpe?g|png)/.test(mime)) throw `*_El archivo ${mime} no es compatible_*`
+  if (!mime) throw 'Etiqueta una imagen para convertirlo a sticker círculo!'
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Archivo ${mime} no es compatible!`
   let img = await q.download()
   let url = await uploadImage(img)
   let wanted = global.API('dzx', '/api/canvas/circle', { url }) //`https://api.dhamzxploit.my.id/api/canvas/circle?url=${url}`
-  let stiker = await sticker(null, wanted, 'circle', '©The Shadow Brokers - Bot')
+  let stiker = await sticker(null, wanted, global.packname, global.author)
   conn.sendMessage(m.chat, stiker, MessageType.sticker, {
     quoted: m
   })
  } catch (e) {
-   m.reply('*_Conversion fallida_*\n\n*_Vuelva a intentarlo_*\n\n*_Recuerde enviar una imagen con el comando #circle o responder a la imagen con el comando_*')
+   m.reply(global.error)
   }
 }
+
 handler.help = ['circle']
 handler.tags = ['sticker']
-handler.command = /^circle$/i
+handler.command = /^(círculo|circulo|circle|scircle)$/i
 
 module.exports = handler
