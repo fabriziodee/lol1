c4cb507572bc9b8d198d209aeed32db8e8df72ac
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-09 22:03:38 -0500

    Update and rename contacall.js to contactall.js

--- plugins/contactall.js
+++ plugins/contactall.js
@@ -1,10 +1,11 @@
 let handler = async function (m, { conn }) {
   let list = []
   for (let i of owner.map(v => v + '@s.whatsapp.net')) {
-    let name = this.getName(global.owner[0] + '@s.whatsapp.net')
+    let users = participants.map(u => u.jid)
+    let name = this.getName(users)
     list.push({
       "displayName": name,
-      "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:;${name};;;\nFN:${name}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Creador de la loli bot xD\nEND:VCARD`
+      "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:;${name};;;\nFN:${name}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Test\nEND:VCARD`
     })
   }
   await conn.sendMessage(m.chat, {
@@ -15,6 +16,6 @@ let handler = async function (m, { conn }) {
 
 handler.help = ['contacall']
 handler.tags = ['group']
-handler.command = /^(contacall|conall)$/i
+handler.command = /^(contacall|contactall|conall)$/i
 
 module.exports = handler
