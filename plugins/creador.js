d3e6723e122e6b27921399765f921ffba05287c6
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-09 20:02:22 -0500

    Update creador.js

--- plugins/creador.js
+++ plugins/creador.js
@@ -4,11 +4,11 @@ let handler = async function (m, { conn }) {
     let name = this.getName(global.owner[0] + '@s.whatsapp.net')
     list.push({
       "displayName": name,
-      "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:;${name};;;\nFN:${name}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+      "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:;${name};;;\nFN:${name}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Creador de la loli bot xD\nEND:VCARD`
     })
   }
   await conn.sendMessage(m.chat, {
-    "displayName": '7 Contact',
+    "displayName": `${list.length} Contact`,
     "contacts": list
   }, 'contactsArrayMessage', { quoted: m })
 }
