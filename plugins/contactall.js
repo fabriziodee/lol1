1dbd6f0d61fb0a5605f78ef47e882e552f15d045
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-09 22:11:35 -0500

    Update contactall.js

--- plugins/contactall.js
+++ plugins/contactall.js
@@ -1,8 +1,8 @@
 let handler = async function (m, { conn, participants }) {
   let list = []
-  for (let i of participants.map(u => u.jid))) {
-    //let users = participants.map(u => u.jid)
-    let name = conn.getName(i)
+  for (let i of participants.map(u => u.jid)) {
+    let users = participants.map(u => u.jid)
+    let name = conn.getName(users)
     list.push({
       "displayName": name,
       "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:;${name};;;\nFN:${name}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Test\nEND:VCARD`
