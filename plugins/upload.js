f6ca793972b4757567a9ec5d34a5e6e79681bd97
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-10 23:22:56 -0500

    Update upload.js

--- plugins/upload.js
+++ plugins/upload.js
@@ -4,7 +4,7 @@ const uploadImage = require('../lib/uploadImage')
 let handler = async (m, { usedPrefix, command }) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw `Balas media dengan perintah *${usedPrefix + command}*`
+  if (!mime) throw `Etiqueta una imagen con el comando *${usedPrefix + command}*!`
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
