f3eb834587215663babbac4f089648a2117a2a79
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-10 23:06:42 -0500

    Update ajolote.js

--- plugins/ajolote.js
+++ plugins/ajolote.js
@@ -3,9 +3,9 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
   conn.reply(m.chat, wait, m)
   let res = await fetch('https://axoltlapi.herokuapp.com')
-  if (!res.ok) throw `${res.status} ${res.statusText}`
+  //if (!res.ok) throw `${res.status} ${res.statusText}`
   let json = await res.json()
-  if (json.url) throw json
+  //if (json.url) throw json
   conn.sendFile(m.chat, json.url, 'ajolote', `*${json.facts}*`, m)
 }
 
