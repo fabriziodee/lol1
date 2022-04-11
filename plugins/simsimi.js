39098fe727fad7f3938c2f3569d3b5f6e03ed018
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-11 08:23:09 -0500

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { text }) => {
-  let res = await fetch(`https://simsimi.info/api/?text=${text}&lc=es`)
+  let res = await fetch(`https://simsimi.info/api/?text=${encodeURIComponent(text)}&lc=es`)
   let json = await res.json()
   if (json.success) m.reply(`${json.success}`)
   else throw json
