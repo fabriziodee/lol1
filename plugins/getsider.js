771d31e6ee1199cb9b64316b7cb74955181df271
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-10 23:00:40 -0500

    Update getsider.js

--- plugins/getsider.js
+++ plugins/getsider.js
@@ -4,14 +4,14 @@ let handler = async (m, { conn, command, usedPrefix }) => {
   if (!m.quoted.id) throw false
   let members = m.quoted.chat.endsWith('g.us') ? (await conn.groupMetadata(m.quoted.chat)).participants.length - 1 : m.quoted.chat.endsWith('@broadcast') ? -1 : 1
   let { reads, deliveries } = await conn.messageInfo(m.quoted.chat, m.quoted.id)
-  let txt = `
-*Leído por ${members > 1 ? `${members - reads.length} Miembros` : ''}*
+  let txt = `\t\t\t\t*‧ 💌 Info Mensaje 💌 ‧*
+
+*👀 Leído por ${members > 1 ? `${members - reads.length} Miembros` : ''}*
 ${reads.sort((a, b) => b.t - a.t).map(({ jid, t }) => `*• Usuario:* @${jid.split`@`[0]}\n*• Fecha:* ${formatDate(t * 1000)}\n*• Hora:* ${formatHour(t * 1000)}`).join('\n\n')}
 
 
-*Enviado a ${members > 1 ? `${members - reads.length - deliveries.length} Miembros*` : ''}
-${deliveries.sort((a, b) => b.t - a.t).map(({ jid, t }) => `*• Usuario:* @${jid.split`@`[0]}\n*• Fecha:* ${formatDate(t * 1000)}\n*• Hora:* ${formatHour(t * 1000)}`).join('\n\n')}
-`.trim()
+*✅ Enviado a ${members > 1 ? `${members - reads.length - deliveries.length} Miembros*` : ''}
+${deliveries.sort((a, b) => b.t - a.t).map(({ jid, t }) => `*• Usuario:* @${jid.split`@`[0]}\n*• Fecha:* ${formatDate(t * 1000)}\n*• Hora:* ${formatHour(t * 1000)}`).join('\n\n')}`
   m.reply(txt, null, {
     contextInfo: {
       mentionedJid: conn.parseMention(txt)
