cf1ed8e040260163444a492555e3eff8138145db
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-11 12:43:21 -0500

    Update spamwa.js

--- plugins/spamwa.js
+++ plugins/spamwa.js
@@ -7,7 +7,7 @@ if (jumlah && isNaN(jumlah)) throw '*[❗] La cantidad debe ser un número!*'
 
   let fixedNumber = nomor.replace(/[-+<>@]/g, '').replace(/ +/g, '').replace(/^[0]/g, '62') + '@s.whatsapp.net'
   let fixedJumlah = jumlah ? jumlah * 1 : 10
-  if (fixedJumlah > 50) throw '*[ ⚠ ] Demasiados mensajes!! ingrese una cantidad por debajo de 50 mensajes*️'
+  //if (fixedJumlah > 50) throw '*[ ⚠ ] Demasiados mensajes!! ingrese una cantidad por debajo de 50 mensajes*️'
   await m.reply(`*[❗] Spam de mensajes al número ${nomor} realizado con exito*\n*Cantidad enviada: ${fixedJumlah} veces!*`)
   for (let i = fixedJumlah; i > 1; i--) {
   if (i !== 0) conn.reply(fixedNumber, pesan.trim(), m)
