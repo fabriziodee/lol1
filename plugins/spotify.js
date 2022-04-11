5f6c177214bbc66d943e1852ae12e87b858430b7
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-11 09:42:13 -0500

    Update spotify.js

--- plugins/spotify.js
+++ plugins/spotify.js
@@ -1,6 +1,7 @@
 let fetch = require('node-fetch')
+
 let handler = async(m, { conn, text }) => {
-if (!text) throw `*Ingrese el nombre de alguna cancion a buscar!!*`
+if (!text) throw `*Ingrese el nombre de una música*\n\n- Ejemplo: ${usedPrefix + command} It's Been So Long`
 let res = await fetch(global.API('zeks', '/api/spotify', { q: text }, 'apikey'))
 if (!res.ok) throw await res.text()
 let json = await res.json()
@@ -25,6 +26,9 @@ let spotifyi = `❒═════❬ SPOTIFY ❭═════╾❒
 await conn.sendFile(m.chat, thumb, '', spotifyi, m)
 await conn.sendFile(m.chat, preview_mp3, 'spotify.mp3', spotifyi, m)
 }
+
+handler.help = ['spotify'] 
+handler.tags = ['tools'] 
 handler.command = /^(spotify|music)$/i
 
 
