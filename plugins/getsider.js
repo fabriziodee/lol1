2c2ddf2b354d4f7d571effb5d980a9ab78f7f18f
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-10 22:40:22 -0500

    Update getsider.js

--- plugins/getsider.js
+++ plugins/getsider.js
@@ -5,11 +5,11 @@ let handler = async (m, { conn, command, usedPrefix }) => {
   let members = m.quoted.chat.endsWith('g.us') ? (await conn.groupMetadata(m.quoted.chat)).participants.length - 1 : m.quoted.chat.endsWith('@broadcast') ? -1 : 1
   let { reads, deliveries } = await conn.messageInfo(m.quoted.chat, m.quoted.id)
   let txt = `
-*Dibaca oleh:*
+*Leído por:*
 ${reads.sort((a, b) => b.t - a.t).map(({ jid, t }) => `@${jid.split`@`[0]}\n_${formatDate(t * 1000)}_`).join('\n')}
 ${members > 1 ? `${members - reads.length} tersisa` : ''}
 
-*Terkirim ke:*
+*Enviado a:*
 ${deliveries.sort((a, b) => b.t - a.t).map(({ jid, t }) => `wa.me/${jid.split`@`[0]}\n_${formatDate(t * 1000)}_`).join('\n')}
 ${members > 1 ? `${members - reads.length - deliveries.length} tersisa` : ''}
 `.trim()
@@ -26,7 +26,7 @@ handler.command = /^(getsider)$/
 
 module.exports = handler
 
-function formatDate(n, locale = 'id') {
+function formatDate(n, locale = 'en-US') {
   let d = new Date(n)
   return d.toLocaleDateString(locale, {
     weekday: 'long',
