4541a0497020d59a2b6eeb6496ff098fa1cc6532
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-10 22:50:34 -0500

    Update getsider.js

--- plugins/getsider.js
+++ plugins/getsider.js
@@ -5,13 +5,12 @@ let handler = async (m, { conn, command, usedPrefix }) => {
   let members = m.quoted.chat.endsWith('g.us') ? (await conn.groupMetadata(m.quoted.chat)).participants.length - 1 : m.quoted.chat.endsWith('@broadcast') ? -1 : 1
   let { reads, deliveries } = await conn.messageInfo(m.quoted.chat, m.quoted.id)
   let txt = `
-*Leído por:*
-${reads.sort((a, b) => b.t - a.t).map(({ jid, t }) => `@${jid.split`@`[0]}\n_${formatDate(t * 1000)}_`).join('\n')}
-${members > 1 ? `${members - reads.length} tersisa` : ''}
+*Leído por ${members > 1 ? `${members - reads.length} Miembros` : ''}*
+${reads.sort((a, b) => b.t - a.t).map(({ jid, t }) => `*• Usuario:* @${jid.split`@`[0]}\n*• Fecha:* ${formatDate(t * 1000)}\n*• Hora:* ${formatHour(t * 1000)}`).join('\n\n')}
 
-*Enviado a:*
-${deliveries.sort((a, b) => b.t - a.t).map(({ jid, t }) => `wa.me/${jid.split`@`[0]}\n_${formatDate(t * 1000)}_`).join('\n')}
-${members > 1 ? `${members - reads.length - deliveries.length} tersisa` : ''}
+
+*Enviado a ${members > 1 ? `${members - reads.length - deliveries.length} Miembros`` : ''}
+${deliveries.sort((a, b) => b.t - a.t).map(({ jid, t }) => `*• Usuario:* @${jid.split`@`[0]}\n*• Fecha:*${formatDate(t * 1000)}\n*• Hora:* ${formatHour(t * 1000)}`).join('\n\n')}
 `.trim()
   m.reply(txt, null, {
     contextInfo: {
@@ -32,7 +31,13 @@ function formatDate(n, locale = 'es-US') {
     weekday: 'long',
     day: 'numeric',
     month: 'long',
-    year: 'numeric',
+    year: 'numeric'
+  })
+}
+
+function formatHour(n, locale = 'en-US') {
+  let d = new Date(n)
+  return d.toLocaleDateString(locale, {
     hour: 'numeric',
     minute: 'numeric',
     second: 'numeric'
