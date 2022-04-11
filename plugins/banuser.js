d3fd6073b28ccd6b07d2add5710c06135e9b6d61
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-11 16:56:24 -0500

    Update banuser.js

--- plugins/banuser.js
+++ plugins/banuser.js
@@ -1,13 +1,14 @@
 let { MessageType, mentionedJid } = require("@adiwajshing/baileys");
 
 let handler = async (m, { conn, text }) => {
-    //let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender 
+    let buser = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender 
     //if (!m.chat) return m.reply('Etiqueta a alguien del grupo para eliminar!')
-    let buser = m.quoted.sender ? m.quoted.sender : m.quoted.sender && m.mentionedJid[0] ? m.quoted.sender : m.mentionedJid[0]
+    //let buser = m.quoted.sender ? m.quoted.sender : m.quoted.sender && m.mentionedJid[0] ? m.quoted.sender : m.mentionedJid[0]
     if (!buser) throw 'Etiqueta a alguien del grupo!'
+    let _buser = `${buser.split("@s.whatsapp.net")[0]}`
     let users = global.DATABASE._data.users
     users[buser].banned = true
-    users[buser].BannedReason = `${text}`
+    users[buser].BannedReason = `${text.replace(_buser, '')}`
     conn.sendMessage(m.chat, `*🔕 El usuario @${buser.split("@s.whatsapp.net")[0]} fue baneado*\n\n*Razón:* ${text}\n\n- Ahora ya no podrá utilizar los comandos`, MessageType.text, { quoted: m, contextInfo: { mentionedJid: [buser] } })
 }
 
