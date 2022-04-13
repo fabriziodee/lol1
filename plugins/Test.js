0f26960eaf9c8f008bf32b116b36c21b305107c1
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-12 21:51:52 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -1,15 +1,15 @@
+let { MessageType, mentionedJid } = require("@adiwajshing/baileys");
+
 let handler = async function (m, { conn, text, participants }) {
-  let list = []
-  for (let i of owner.map(v => v + '@s.whatsapp.net')) {
-    list.push({
-      "displayName": "Test",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;Test;;;\nFN:Test\nitem1.EMAIL;type=INTERNET:xxxx@ezweb.ne.jp\nitem1.X-ABLabel:_$!!$_\nTITLE:\nURL;type=HOME:\nPHOTO:\nitem3.X-ABDATE:\nitem3.X-ABLabel:_$!!$_\nitem4.X-ABDATE:\nitem4.X-ABLabel:_$!!$_\nBDAY;value=date:\nNICKNAME:\nTEL;type=OTHER;type=VOICE:09012345678\nitem2.ADR;type=OTHER:;;;;;;\nORG:\nEND:VCARD"
-    })
-  }
-  await conn.sendMessage(m.chat, {
-    "displayName": "77 Contact",
-    "contacts": list
-  }, 'contactsArrayMessage', { quoted: m })
+const vcard = 'BEGIN:VCARD\n' 
+ + 'VERSION:3.0\n' 
+ + 'FN:Test\n'
+ + 'ORG:Creador de la lolibot xD;\n'
+ + 'TEL;type=CELL;type=VOICE;waid=51940617554:+51940617554\n'
+ +  'item1.EMAIL;type=INTERNET:xxxx@ezweb.ne.jp\n'
+ + 'END:VCARD'
+ 
+ conn.sendMessage(m.chat, { displayname: "gatito", vcard: vcard }, MessageType.contact, { quoted: m })
 }
 
 //handler.help = ['test']
