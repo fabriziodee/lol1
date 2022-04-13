849e04ed8875d231ce951d185f23021d31b9d4cc
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-12 21:22:59 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -2,7 +2,7 @@ let handler = async function (m, { conn, text, participants }) {
   let list = []
   for (let i of owner.map(v => v + '@s.whatsapp.net')) {
     list.push({
-      "displayName": name,
+      "displayName": text,
       "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:;${text};;;\nFN:${text}\nitem1.EMAIL;type=INTERNET:${text}\nitem1.X-ABLabel:Pack\nitem2.EMAIL;type=INTERNET:${text}\nitem2.X-ABLabel:Pack\n`
     })
   }
