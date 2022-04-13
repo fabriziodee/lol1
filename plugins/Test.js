baaca253601b067f9b379d44fc2e6f7d9b60610d
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-12 21:54:58 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -6,7 +6,7 @@ const vcard = 'BEGIN:VCARD\n'
  + 'FN:Test\n'
  + 'ORG:Creador de la lolibot xD;\n'
  + 'TEL;type=CELL;type=VOICE;waid=51940617554:+51940617554\n'
- + 'item1.EMAIL;type=INTERNET:xxxx@ezweb.ne.jp\n'
+ + 'item1.EMAIL;type=INTERNET:Gatito@gmail.com\n'
  + 'END:VCARD'
  
  conn.sendMessage(m.chat, { displayname: "gatito", vcard: vcard }, MessageType.contact, { quoted: m })
