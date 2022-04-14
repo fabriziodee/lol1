bae63f4eeba4e44c00247e755e68c77a1064694f
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-13 23:22:57 -0500

    Update rpg-apostar.js

--- plugins/rpg-apostar.js
+++ plugins/rpg-apostar.js
@@ -4,7 +4,7 @@ async function handler(m, { conn, args, text, usedPrefix, command }) {
     if (!text) return m.reply(`*Ingrese la cantidad de dinero que desea apostar*\n\n- Ejemplo: ${usedPrefix + command} 100`)
     let user = global.DATABASE._data.users[m.sender]
     let count = (args[0] && number(parseInt(args[0])) ? Math.max(parseInt(args[0]), 1) : /all/i.test(args[0]) ? Math.floor(parseInt(user.money)) : 1) * 1
-    if ((user.money * 1) < count) return conn.sendMessage(m.chat, button('*Tu dinero no es suficiente para apostar*', user), MessageType.buttonsMessage, { quoted: m })
+    if ((user.money * 1) < count) return conn.sendMessage(m.chat, 'Tu dinero no es suficiente para apostar!', { quoted: m })
     let moneyDulu = user.money * 1
     let Bot = (Math.ceil(Math.random() * 100)) * 1
             let Kamu = (Math.floor(Math.random() * 86)) * 1
@@ -15,7 +15,7 @@ async function handler(m, { conn, args, text, usedPrefix, command }) {
             } else if (Bot > Kamu) {
                 user.money -= count * 1
             } else {
-                status = 'Seri'
+                status = 'Igual'
                 user.money += (Math.floor(count / 1.5)) * 1
             }
             m.reply(`
