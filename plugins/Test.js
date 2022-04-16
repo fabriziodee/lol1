be0ea2b7734a7273a5f342cbfd0b7734967911de
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-15 20:06:34 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -3,9 +3,8 @@ let crypto = require('crypto')
 let { MessageType, mentionedJid } = require("@adiwajshing/baileys");
 
 let handler = async function (m, { conn, text, participants }) {
-let cd = crypto.createHash('sha256', 'Test')
-m.reply(cd)
- conn.sendMessage(m.chat, { displayname: "By 「gatitoツ」", vcard: vcard }, MessageType.contact, { quoted: m })
+let test = crypto.createHash('sha256', 'Test')
+await m.reply(test)
 }
 
 //handler.help = ['test']
