7419bebb24eb1a67f3ce34d0cc07e684787f1350
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-16 02:43:41 -0500

    Update logo technology.js

--- plugins/logo technology.js
+++ plugins/logo technology.js
@@ -1,8 +1,14 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn, args }) => {
-response = args.join(' ').split('|')
-if (!args[0]) throw '*[❗] Ingrese un texto*\n*Ejemplo:*\n*#technology Shadow*'        
+
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+if (!text) throw `*Ingrese un texto para el logo*\n\n- Ejemplo: ${usedPrefix + command} lolibot`
+conn.reply(m.chat, wait, m)
 let res = `https://api-alc.herokuapp.com/api/textpro/futuristic-technology?texto=${response[0]}&apikey=ConfuMods`
-conn.sendFile(m.chat, res, 'error.jpg', `*Logo terminado*`, m, false)}
-handler.command = /^(technology)$/i
+conn.sendFile(m.chat, res, 'error.jpg', `*Logo tecnología*\nTexto: ${text}`, m, false)
+}
+
+handler.help = ['ltecnologia']
+handler.tags = ['maker']
+handler.command = /^(ltecnologia|ltecnología|technology)$/i
+
 module.exports = handler
