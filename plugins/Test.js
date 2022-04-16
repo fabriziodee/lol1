d2e70761b406cbca280642bd7410ce7deae5e01f
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-16 01:58:19 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -1,7 +1,7 @@
 let crypto = require('crypto')
 
 let handler = async (m, { conn, text, usedPrefix }) => {
-await m.reply(`${formatNumber(text)}`)
+await m.reply(`${shortNum(text)}`)
 }
 
 //handler.help = ['test']
@@ -10,39 +10,8 @@ handler.command = /^(test)$/i
 
 module.exports = handler
 
-var ranges = [{
-      divider: 1e18,
-      suffix: 'E'
-   },
-   {
-      divider: 1e15,
-      suffix: 'P'
-   },
-   {
-      divider: 1e12,
-      suffix: 'T'
-   },
-   {
-      divider: 1e9,
-      suffix: 'G'
-   },
-   {
-      divider: 1e6,
-      suffix: 'M'
-   },
-   {
-      divider: 1e3,
-      suffix: 'k'
-   }
-];
-
-function formatNumber(n) {
-   for (var i = 0; i < ranges.length; i++) {
-      if (n >= ranges[i].divider) {
-         return (n / ranges[i].divider).toString() + ranges[i].suffix;
-      }
-   }
-   return n.toString();
+function shortNum(num) {
+new Intl.NumberFormat('en-GB', { notation: "compact", compactDisplay: "short" }).format(num)
 }
 
 function createHash(data, len) {
