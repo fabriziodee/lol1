8fbbcc121b8332d0b8554182070dc6508f7085e6
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-16 02:57:13 -0500

    Update logo neonfire.js

--- plugins/logo neonfire.js
+++ plugins/logo neonfire.js
@@ -1,8 +1,13 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn, args }) => {
-response = args.join(' ').split('|')
-if (!args[0]) throw '*[❗] Ingrese un texto*\n*Ejemplo:*\n*#neonfire Shadow*'        
-let res = `https://api-alc.herokuapp.com/api/textpro/neon-devil-wings?texto=${response[0]}&apikey=ConfuMods`
-conn.sendFile(m.chat, res, 'error.jpg', `*Logo terminado*`, m, false)}
-handler.command = /^(neonfire)$/i
+
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+if (!text) throw `*Ingrese un texto para el logo*\n\n- Ejemplo: ${usedPrefix + command} lolibot`
+conn.reply(m.chat, wait, m)
+let res = `https://api-alc.herokuapp.com/api/textpro/neon-devil-wings?texto=${text}&apikey=ConfuMods`
+conn.sendFile(m.chat, res, 'error.jpg', `*Logo alas fuego neon*\nTexto: ${text}`, m, false)
+}
+
+handler.help = ['lalas']
+handler.tags = ['maker']
+handler.command = /^(|neonfire)$/i
 module.exports = handler
