bba06a0cf76f1830b45ca9d9789afdaede3612f8
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-16 02:51:18 -0500

    Update logo flaming.js

--- plugins/logo flaming.js
+++ plugins/logo flaming.js
@@ -1,8 +1,13 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn, args }) => {
-response = args.join(' ').split('|')
-if (!args[0]) throw '*[❗] Ingrese un texto*\n*Ejemplo:*\n*#flaming Shadow*'        
-let res = `https://api-alc.herokuapp.com/api/photooxy/flaming-fire?texto=${response[0]}&apikey=ConfuMods`
-conn.sendFile(m.chat, res, 'error.jpg', `*Logo terminado*`, m, false)}
-handler.command = /^(flaming|logoflaming)$/i
+
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+if (!text) throw `*Ingrese un texto para el logo*\n\n- Ejemplo: ${usedPrefix + command} lolibot`
+conn.reply(m.chat, wait, m)
+let res = `https://api-alc.herokuapp.com/api/photooxy/flaming-fire?texto=${text}&apikey=ConfuMods`
+conn.sendFile(m.chat, res, 'error.jpg', `*Logo llamas de fuego*\nTexto: ${text}`, m, false)
+}
+
+handler.help = ['lfuego']
+handler.tags = ['maker']
+handler.command = /^(lfuego|flaming|logoflaming)$/i
 module.exports = handler
