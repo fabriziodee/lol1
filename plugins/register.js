169c38ea5d4ef8ed47a66366031408f7ca4eb7ca
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-15 19:47:28 -0500

    Update register.js

--- plugins/register.js
+++ plugins/register.js
@@ -11,8 +11,8 @@ let handler = async function (m, { conn, text, usedPrefix, command}) {
   if (user.registered === true) throw `*Ya estas registrado*, quieres volver a registrarte?\nUse ${usedPrefix}unreg <codigo>`
   if (!Reg.test(text)) throw `*Registro no valido*\n- Ejemplo: ${usedPrefix + command} <nombre>.<edad>\n\nNo se olvide del punto`
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) throw 'El nombre no puede estar vacio'
-  if (!age) throw 'La edad no puede estar vacia'
+  if (!name) throw 'El nombre no puede estar vacio!'
+  if (!age) throw 'La edad no puede estar vacia!'
   if (!Rname.test(name)) throw 'Ponga un nombre valido'
   user.name = name
   user.age = parseInt(age)
@@ -27,12 +27,14 @@ let handler = async function (m, { conn, text, usedPrefix, command}) {
   } finally {
   let repp = await(await fetch(reuser)).buffer()
   retext = `
-*✅ Registro exitoso*
+\t\t\t*‧ 🧇 Usuario Registrado 🧇 ‧*
 
- • Nombre: ${name}
- • Edad: ${age} años
- • Codigo: ${sn}
-`.trim()
+ *◦ Nombre:* ${name}
+ *◦ Edad:* ${age} años
+ *◦ Codigo:* ${sn}
+
+ *◦ Fecha:* ${date}
+ *◦ Hora:* ${time}`
   py =  await conn.prepareMessage(m.chat, repp, MessageType.image)
 gbutsan = [
 {buttonId: `${usedPrefix}menu`, buttonText: {displayText: '🍟 MENU'}, type: 1},
@@ -41,7 +43,7 @@ gbutsan = [
 gbuttonan = {
 imageMessage: py.message.imageMessage,
 contentText: retext,
-footerText: `Lolibot - OFC`,
+footerText: 'No olvides tu *codigo* por qué será necesario para próximas actuaciones',
 buttons: gbutsan,
 headerType: 4
 }
@@ -49,10 +51,25 @@ conn.sendMessage(m.chat, gbuttonan, MessageType.buttonsMessage, { quoted: m })
 }
 conn.sendMessage(m.sender, `Codigo de registro: ${sn}`, MessageType.text, { quoted: m })
 }
-handler.help = ['register']
-handler.tags = ['xp']
 
-handler.command = /^(registrarse|daftar|register|reg)$/i
+handler.help = ['registrar']
+handler.tags = ['xp']
+handler.command = /^(registrar|registrarse|daftar|register|reg)$/i
 
 module.exports = handler
 
+
+let d = new Date(new Date + 3600000)
+
+let date = d.toLocaleDateString(locale, {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    })
+
+let time = d.toLocaleString('en-US', { 
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      hour12: true 
+    })
