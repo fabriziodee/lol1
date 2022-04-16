9439b86feb0d1fbed3846dabf145d7d219ae1971
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-15 20:26:47 -0500

    Update desregistrar.js

--- plugins/desregistrar.js
+++ plugins/desregistrar.js
@@ -3,7 +3,8 @@ const { createHash } = require('crypto')
 let handler = async function (m, { args }) {
   if (!args[0]) throw 'Ingrese el codigo que se le dio al momento de registrarse'
   let user = global.DATABASE._data.users[m.sender]
-  let sn = createHash('md5').update(m.sender).digest('hex')
+  let sn = createHash(m.sender, 7)
+  //createHash('md5').update(m.sender).digest('hex')
   if (args[0] !== sn) throw 'El codigo es incorrecto!'
   user.registered = false
   m.reply(`Se elimino tu registro con exito!`)
