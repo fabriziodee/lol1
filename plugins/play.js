28e33d8328f19ce43fffc6de963038806784cc84
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-15 23:20:18 -0500

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -5,7 +5,8 @@ if (!text) throw `*Ingrese en nombre de una música*\n\n- Ejemplo: ${usedPrefix 
 let res = await fetch(`https://leyscoders-api.herokuapp.com/api/playmp3?q=${text}&apikey=MIMINGANZ`)
 await m.reply(`${JSON.stringify(res, null, 1)}`)
 //let json = await res.json()
-//conn.sendFile(m.chat, json.result.link.link, 'error.mp4', `_The Shadow Brokers - Bot_`, m)}
+//conn.sendFile(m.chat, json.result.link.link, 'error.mp4', `_The Shadow Brokers - Bot_`, m)
+}
 
 handler.command = /^(testplay)$/i
 
