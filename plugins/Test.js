5f6a374213693f77fd908b032e9c8672b20e4f61
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-16 01:49:59 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -1,7 +1,7 @@
 let crypto = require('crypto')
 
-let handler = async (m, { conn, usedPrefix }) => {
-await m.reply(`${formatNumber(1000000)}`)
+let handler = async (m, { conn, text, usedPrefix }) => {
+await m.reply(`${formatNumber(text)}`)
 }
 
 //handler.help = ['test']
