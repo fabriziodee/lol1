ae85d607121372c995085308494cf87c35fbbee1
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-16 08:35:19 -0500

    Update kick.js

--- plugins/kick.js
+++ plugins/kick.js
@@ -1,7 +1,7 @@
 let handler = async (m, { command, text, groupMetadata }) => {
   let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
   if (!groupMetadata.participants.some(v => v.jid === user)) return m.reply('El usuario ya no está en el grupo!')
-  if (!user) return m.reply('Etiqueta a alguien del grupo para eliminar!')
+  if (!m.chat) return m.reply('Etiqueta a alguien del grupo para eliminar!')
   conn.groupRemove(m.chat, [user])
   conn.reply(m.chat, 'Se eliminó al usuario!', m)
 }
