f7b44b31f71955e0b0dd62ce5b9654e3917c28aa
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-15 20:46:41 -0500

    Update lista-reg.js

--- plugins/lista-reg.js
+++ plugins/lista-reg.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     let reg = Object.entries(global.DATABASE._data.users).filter(user => user[1].registered)
     m.reply(`*❛ 📧 Usuarios registrados 📧 ❜*
 
-Total: ${reg.length} Usuarios${reg ? '\n' + users.map(([jid], i) => `${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
+Total: ${reg.length} Usuarios${reg ? '\n' + reg.map(([jid], i) => `${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
 ID: ${jid}`.trim()).join('\n') : ''}
 `.trim())
 }
