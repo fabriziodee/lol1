0ec7f30781fc282f21aba4f0c0d564b7b69d5dee
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-16 01:39:35 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -1,7 +1,7 @@
 let crypto = require('crypto')
 
 let handler = async (m, { conn, usedPrefix }) => {
-await m.reply(`${toShort(65512)}`)
+await m.reply(`${tnumberto(65512)}`)
 }
 
 //handler.help = ['test']
@@ -10,10 +10,12 @@ handler.command = /^(test)$/i
 
 module.exports = handler
 
-function toShort(number) {
-  const int16 = new Int16Array(1)
-  int16[0] = number
-  return int16[0]
+function numberto(n){
+x=(''+n).length;
+p=Math.pow;
+d=p(10,true);
+x-=x%3;
+return Math.round(n*d/p(10,x))/d+" kMGTPE"[x/3];
 }
 
 function createHash(data, len) {
