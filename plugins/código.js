a368003462fc4388d649cffdf1d762392faa371e
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-15 20:24:43 -0500

    Update código.js

--- plugins/código.js
+++ plugins/código.js
@@ -6,7 +6,7 @@ let handler = async (m) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
-    let sn = createHash('md5').update(m.sender).digest('hex')
+    let sn = createHash(m.sender, 7)
     m.reply(`*Tu codigo es:* ${sn}`)
 }
 
@@ -16,3 +16,9 @@ handler.command = /^(código|sn|codigo)$/i
 handler.register = true
 
 module.exports = handler
+
+function createHash(data, len) {
+    return crypto.createHash("shake256", { outputLength: len })
+      .update(data)
+      .digest("hex");
+}
