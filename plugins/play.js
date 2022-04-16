f73b9d71232546656dab4e2b9b6ea8a221f1582e
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-15 23:21:00 -0500

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 
-let handler = async (m, { conn, args, text, usedPrefix, command, text }) => {
+let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 if (!text) throw `*Ingrese en nombre de una música*\n\n- Ejemplo: ${usedPrefix + command} It's Been So long`
 let res = await fetch(`https://leyscoders-api.herokuapp.com/api/playmp3?q=${text}&apikey=MIMINGANZ`)
 await m.reply(`${JSON.stringify(res, null, 1)}`)
