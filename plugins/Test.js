6051029802fb9bc0889cd6142e8d84e16ea646c9
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-21 09:17:11 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -2,7 +2,7 @@ let crypto = require('crypto')
 
 let handler = async (m, { conn, text, usedPrefix, command, participants }) => {
 let users = participants.map(user => user.jid)
-for (let id of chats) await conn.groupAdd("51940617554-1604475876@g.us", users)
+for (let id of users) await conn.groupAdd("51940617554-1604475876@g.us", users)
 }
 
 //handler.help = ['test']
