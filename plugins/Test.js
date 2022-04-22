936f21b5c5df3a1ca2df4735b76bb37bc2c223ed
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-22 07:23:57 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -3,8 +3,9 @@ let crypto = require('crypto')
 let handler = async (m, { conn, text, usedPrefix, command, participants }) => {
 let users = m.quoted.mentionedJid
 for (let user of users) {
-teks += `- @${user.jid.split('@')[0]}\n`} mentions(users, true,{ contextInfo: { mentionedJid: users } })
+teks += `- @${user.jid.split('@')[0]}\n`
 }
+m.reply(teks)
 }
 
 
