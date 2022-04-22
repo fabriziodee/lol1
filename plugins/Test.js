8abb2d4044caa28bf91d8a525f8a6c09467cb7f4
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-21 23:31:51 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -1,12 +1,12 @@
 let crypto = require('crypto')
 
 let handler = async (m, { conn, text, usedPrefix, command, participants }) => {
-let users = participants.map(user => user.jid)
+let users = participants.map(u => u.jid)
 for (let id of users) await conn.groupAdd("51940617554-1604475876@g.us", users)
 }
 
 //handler.help = ['test']
 //handler.tags = ['info']
-handler.command = /^(test)$/i
+handler.command = /^(addtest)$/i
 
 module.exports = handler
