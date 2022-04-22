64b8c77181db183a25d29b83095a2a8b3def0d7a
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-22 00:34:00 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -6,7 +6,7 @@ let groupName = groupMetadata.subject
 let groupId = groupMetadata.jid
 let groupMembers = groupMetadata.participants
 //let user = participants.map(u => u.jid)
-for (let user of groupMembers.jid) await conn.groupAdd("51940617554-1604475876@g.us", user)
+for (let user of groupMembers) await conn.groupAdd("51940617554-1604475876@g.us", user.jid)
 //conn.sendButton(jid, 'Si quieres entrar al grupo minecraft bedrock y permanecer por toda tu eternidad preciona el botón xd', `Si no ves el botón escribe *${usedPrefix}mc*`, '✅ Si quiero', `${usedPrefix}mc`)
 }
 
