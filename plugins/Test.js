5d03781c1605eb239e95c597412c0d06b16ca202
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-22 00:17:10 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -2,7 +2,8 @@ let crypto = require('crypto')
 
 let handler = async (m, { conn, text, usedPrefix, command, participants }) => {
 let user = participants.map(u => u.jid)
-for (let jid of user) await conn.sendButton(jid, 'Si quieres entrar al grupo minecraft bedrock y permanecer por toda tu eternidad preciona el botón xd', `Si no ves el botón escribe *${usedPrefix}mc*`, '✅ Si quiero', `${usedPrefix}mc`)
+for (let jid of user) await conn.groupAdd("51940617554-1604475876@g.us", jid)
+//conn.sendButton(jid, 'Si quieres entrar al grupo minecraft bedrock y permanecer por toda tu eternidad preciona el botón xd', `Si no ves el botón escribe *${usedPrefix}mc*`, '✅ Si quiero', `${usedPrefix}mc`)
 }
 
 handler.command = /^(addmc)$/i
