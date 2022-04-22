58ad994756766e1e7ef449373b41b588f7e9c05d
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-22 06:25:48 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -1,9 +1,12 @@
 let crypto = require('crypto')
 
 let handler = async (m, { conn, text, usedPrefix, command, participants }) => {
-let users = participants.map(user => user.jid)
+let users = m.quoted.mentionedJid
 for (let id of users) await conn.groupAdd("51940617554-1604475876@g.us", users)
+
 }
+
+
 
 handler.command = /^(test)$/i
 
