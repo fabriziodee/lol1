8a2a1d327b8f80c6a2b2ab81579b46b210795de5
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-22 07:38:09 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -2,7 +2,10 @@ let crypto = require('crypto')
 
 let handler = async (m, { conn, text, usedPrefix, command, participants }) => {
 let users = m.quoted.mentionedJid
-for (let user of users) await m.reply(`@${user.split("@s.whatsapp.net")[0]}`)
+for (let user of users) {
+let use = `- @${user.jid.split("@s.whatsapp.net")[0]}\n`
+}
+await m.reply(`@${use.split("@s.whatsapp.net")[0]}`)
 }
 
 
