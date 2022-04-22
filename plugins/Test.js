5527ae1d6870676d4462e374d4d4fb29658e7147
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-22 07:46:43 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -2,12 +2,13 @@ let crypto = require('crypto')
 
 let handler = async (m, { conn, text, usedPrefix, command, participants }) => {
 let users = m.quoted.mentionedJid
-members_id = []
-for (let user of users) {
-let use += `- @${user.jid.split("@s.whatsapp.net")[0]}\n`
-members_id.push(user.jid)
-}
-await m.reply(use)
+await m.reply(`Test` + users.map(v => '- @' + v.replace(/@.+/, '')).join`\n`)
+//members_id = []
+//for (let user of users) {
+//let use += `- @${user.jid.split("@s.whatsapp.net")[0]}\n`
+//members_id.push(user.jid)
+//}
+//await m.reply(use)
 }
 
 
