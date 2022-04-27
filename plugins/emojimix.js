f1384e022b5cc7a8a1ca1786a0aab5bb79165e8d
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-27 09:24:36 -0500

    Update emojimix.js

--- plugins/emojimix.js
+++ plugins/emojimix.js
@@ -6,8 +6,9 @@ let fs = require("fs")
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 if (!args[0]) throw `*Ingrese dos emojis*\n\n- Ejemplo: ${usedPrefix + command} 🐱+👻`
 let [emoji1, emoji2] = text.split`+`
-let anu = await (await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)).json().catch(async (e) => { await m.reply(`*No se encontro el emoji!*\n\n*Error:* ${e}`) })
+let anu = await (await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)).json()
 let emix = anu.results[0].media_formats.png_transparent.url
+.catch(async (e) => { await m.reply(`*No se encontro el emoji!*\n\n*Error:* ${e}`) })
 let stiker = await sticker(null, emix, global.packname, global.author)
 conn.sendMessage(m.chat, stiker, MessageType.sticker, { quoted: m })
 }
