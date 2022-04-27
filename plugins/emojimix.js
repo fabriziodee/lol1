a2c23c9a21740cffee41239de438095916a0468b
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-27 09:40:03 -0500

    Update emojimix.js

--- plugins/emojimix.js
+++ plugins/emojimix.js
@@ -7,8 +7,8 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 if (!args[0]) throw `*Ingrese dos emojis*\n\n- Ejemplo: ${usedPrefix + command} 🐱+👻`
 let [emoji1, emoji2] = text.split`+`
 try {
-let anu = await (await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)).json()
-let emix = anu.results[0].media_formats.png_transparent.url
+const anu = await (await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)).json()
+const emix = anu.results[0].media_formats.png_transparent.url
 } catch (e) {
 return m.reply(`*No se encontro el emoji!*\n\n*Error:* ${e}`)
 }
