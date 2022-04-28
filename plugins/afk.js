888a625ecf497401192b34375061c732fba4d2e5
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-28 09:13:36 -0500

    Update afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -1,12 +1,13 @@
 let { MessageType, mentionedJid } = require("@adiwajshing/baileys");
 
 let handler = async (m, { text, usedPrefix, command }) => {
-  if (!text) throw `*Ingresé el motivo de afk*\n\n- Ejemplo: ${usedPrefix + command} A mimir`
+  if (!text) throw `*• Ingrese su motivo de AFK*\n\n*Ejemplo de uso:*\n1. ${usedPrefix + command} <motivo>\n2. ${usedPrefix + command} se fue ɑ mimir`
+  if (text.length > 300) return m.reply('El texto es demɑsiɑdo lɑrgo mínimo 300 cɑrɑcteres')
   let user = global.DATABASE.data.users[m.sender]
   user.afk = + new Date
   user.afkReason = text
   //conn.sendMessage(m.chat, ``, MessageType.text, { quoted: m, contextInfo: { mentionedJid: [m.sender] } }) 
-  m.reply(`\t\t*‧ ⏰ Ahora estas en AFK ⏰ ‧*\n\n• Nombre: @${m.sender.split("@s.whatsapp.net")[0]}\n• Razón: ${text ? `${text}` : 'No hay'}\n\nSi alguien te etiqueta será notificado`)
+  m.reply(`\t\t*‧ ⏰ Ahorɑ estɑs en AFK ⏰ ‧*\n\n*• Nombre:* @${m.sender.split("@s.whatsapp.net")[0]}\n*• Rɑzón:* ${text ? `${text}` : 'No hɑy'}\n\nSi ɑlguien te etiquetɑ serά notificɑdo`)
 }
 
 handler.help = ['afk']
