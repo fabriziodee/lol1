ac3292be11b2b14ede2f7c32069bcf4c1597d797
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-28 14:54:42 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -1,18 +1,16 @@
-let crypto = require('crypto')
-
-let handler = async (m, { conn, text, usedPrefix, command, participants }) => {
-let users = m.quoted.mentionedJid
-await m.reply(`Test` + users.map(v => '- @' + v.replace(/@.+/, '')).join`\n`)
-//members_id = []
-//for (let user of users) {
-//let use += `- @${user.jid.split("@s.whatsapp.net")[0]}\n`
-//members_id.push(user.jid)
-//}
-//await m.reply(use)
+let { MessageType } = require('@adiwajshing/baileys')
+let handler = async (m, { conn }) => {
+  let q = m.quoted ? m.quoted : m 
+  let mime = (q.msg || q).mimetype || ''
+  if (/image|video/.test(mime)) {
+    let img = await q.download()
+    if (!img) throw 'Foto/Sticker tidak ditemukan'
+    conn.sendMessage(m.chat, {degreesLatitude: 25.21465, degreesLongitude: 49.33476, jpegThumbnail: img, Image: img, caption: "DZX NGEBUG:V"}, MessageType.location)
+  } else m.reply('Tag foto/videonya!!')
 }
 
-
-
+//handler.help = ['jadilokasi']
+//handler.tags = ['tools']
 handler.command = /^(test)$/i
 
 module.exports = handler
