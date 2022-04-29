96ee345542aff1e013348d5351bb680ce919a9cf
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-29 12:47:15 -0500

    Update creador.js

--- plugins/creador.js
+++ plugins/creador.js
@@ -5,7 +5,7 @@ let handler = async function (m, { conn }) {
     let name = this.getName(global.owner[0] + '@s.whatsapp.net')
     list.push({
       "displayName": name,
-      "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:;${name};;;\nFN:${name}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Creador de la loli bot xD\nEND:VCARD`
+      "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:;${name};;;\nFN:${name}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Creador de la loli bot xD\nitem2.EMAIL;type=INTERNET:Gatito@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://github.com/g4tito/lolibot\nitem3.X-ABLabel:Github\nitem4.ADR:;;Perú;;;;\nitem4.X-ABLabel:Region\nEND:VCARD`
     })
   }
   await conn.sendMessage(m.chat, {
