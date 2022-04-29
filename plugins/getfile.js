43a707669a980ca3fcbf8838403e2728958c6cea
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-29 11:21:26 -0500

    Update getfile.js

--- plugins/getfile.js
+++ plugins/getfile.js
@@ -1,25 +1,28 @@
-let cp = require('child_process')
-let { promisify } = require('util')
-let exec = promisify(cp.exec).bind(cp)
-let handler = async (m, { conn, isROwner, usedPrefix, command, text }) => {
-    if (!text) throw `uhm.. teksnya mana?\n\ncontoh\n${usedPrefix + command} main`
-    m.reply('Executing...')
-    let o
-    try {
-        o = await exec('type ' + text)
-    } catch (e) {
-        o = e
-    } finally {
-        let { stdout, stderr } = o
-        if (stdout.trim()) m.reply(stdout)
-        if (stderr.trim()) m.reply(stderr)
+let fs = require('fs')
+
+let handler = async(m, { conn, usedPrefix, args, command }) => {
+    let teks = `
+┌〔 List Write 〕
+├ document
+├ text
+└────
+`.trim()
+    if (!args[0]) throw teks
+    if (args[0] === 'document') {
+        if(!args[1]) throw `Contoh: ${usedPrefix + command} document plugins/haruno.js`
+        let saveas = await m.quoted.download()
+        fs.writeFileSync(`./${args[1]}`, saveas)
+        conn.sendButton(m.chat, `Berhasil tersimpan sebagai ${args[1]}.`, watermark, 'Debounce', '.debounce')
+    } else if (args[0] === 'text') {
+        if (!args[1]) throw `Masukkan nama dan tujuan file.\n${usedPrefix + command} plugins/join.js`
+        require('fs').writeFileSync(`./${args[1]}`, m.quoted.text)
+        conn.sendButton(m.chat, `Berhasil tersimpan sebagai ${args[1]}.`, watermark, 'Debounce', '.debounce')
+    } else {
+        throw teks
     }
 }
 
-handler.help = ['getfile']
-handler.tags = ['owner']
-handler.command = /^(getfile|gf)$/i
-
-handler.rowner = true
+handler.command = /^(getfile)$/i
+handler.owner = true
 
 module.exports = handler
