192347e4b0b97552dc1867c0d63a148be6d7825d
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-30 13:45:41 -0500

    Update and rename adivinanza.js to acertijo.js

--- plugins/acertijo.js
+++ plugins/acertijo.js
@@ -26,7 +26,7 @@ let handler = async (m, { conn, usedPrefix }) => {
        await conn.reply(m.chat, caption, m),
         json, poin,
         setTimeout(async () => {
-            if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabó el tiempo!\n*Respuesta:* ${json.jawaban}`, conn.tekateki[id][0])
+            if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabó el tiempo!\n*Respuesta:* ${json.response}`, conn.tekateki[id][0])
             delete conn.tekateki[id]
         }, timeout)
     ]
