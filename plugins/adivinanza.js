d4e071a0a4f4bcaa16f756e03dde2e8ef4371068
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-30 13:25:44 -0500

    Update adivinanza.js

--- plugins/adivinanza.js
+++ plugins/adivinanza.js
@@ -1,22 +1,22 @@
 let fs = require('fs')
+
 let timeout = 120000
 let poin = 500
+
 let handler = async (m, { conn, usedPrefix }) => {
     conn.tekateki = conn.tekateki ? conn.tekateki : {}
     let id = m.chat
     if (id in conn.tekateki) {
-        conn.reply(m.chat, 'Todavía hay preguntas sin responder en este chat', conn.tekateki[id][0])
+        conn.reply(m.chat, 'Todavía hay acertijos sin responder en este chat', conn.tekateki[id][0])
         throw false
     }
-    let tekateki = JSON.parse(fs.readFileSync(`./src/tekateki.json`))
+    let tekateki = JSON.parse(fs.readFileSync(`./storage/game/acertijo.json`))
     let json = tekateki[Math.floor(Math.random() * tekateki.length)]
     let caption = `
-Pregunta *${json.pertanyaan}*
+*${json.pertanyaan}*
 
 *• Tiempo:* ${(timeout / 1000).toFixed(2)} segundos
-*• Bono:* ${poin} Exp
-
-Escriba *${usedPrefix}tete* para obtener ayuda
+*• Bono:* +${poin} Exp
 `.trim()
     conn.tekateki[id] = [
        await conn.reply(m.chat, caption, m),
@@ -28,8 +28,8 @@ Escriba *${usedPrefix}tete* para obtener ayuda
     ]
 }
 
-handler.help = ['pregunta']
+handler.help = ['acertijo']
 handler.tags = ['game']
-handler.command = /^(pregunta|adivinanza|tekateki)$/i
+handler.command = /^(acertijo|pregunta|adivinanza|tekateki)$/i
 
 module.exports = handler
