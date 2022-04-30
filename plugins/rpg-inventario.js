7b2cc8ddec137786d46395fe0245cbdede5597eb
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-30 11:34:23 -0500

    Update rpg-inventario.js

--- plugins/rpg-inventario.js
+++ plugins/rpg-inventario.js
@@ -59,7 +59,7 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
     let usersmythic = sortedmythic.map(v => v[0])
     let userslegendary = sortedlegendary.map(v => v[0])
     let invt = fs.readFileSync('./storage/image/inventario.png')
-    let str = `\t\t\t*• 📬 ESTADO 📬 •*
+    let str = `\t\t\t\t*• 🏡 ESTADO 🏡 •*
 
 *∙ 👤 Nombre:* @${who.split("@s.whatsapp.net")[0]}
 *∙ ♥️ Vida:* ${healt}
@@ -99,8 +99,9 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
     
 if (text.includes('simple')) { 
     conn.sendMessage(m.chat, str, MessageType.text, { quoted: m, contextInfo: { mentionedJid: [who], forwardingScore: 750, isForwarded: true }})
-	} else conn.sendMessage(m.chat, { contentText: str, footerText: `Si no puedes ver tu inventario completo puedes poner *${usedPrefix + command} simple*`, buttons: [{buttonId: `${usedPrefix}shop`, buttonText: {displayText: 'Tienda ⛺'}, type: 1}], "headerType": "DOCUMENT", "documentMessage": { "url": "https://mmg.whatsapp.net/d/f/Ano5cGYOFQnC51uJaqGBWiCrSJH1aDCi8-YPQMMb1N1y.enc", "mimetype": "application/vnd.ms-excel", "title": "⤿❛ Gɑme - RPG ₊˚๑’-⁩", "fileSha256": "8Xfe3NQDhjwVjR54tkkShLDGrIFKR9QT5EsthPyxDCI=", "fileLength": 99999999999, "pageCount": "0", "mediaKey": "XWv4hcnpGY51qEVSO9+e+q6LYqPR3DbtT4iqS9yKhkI=", "fileName": "⤿❛ Gɑme - RPG ₊˚๑’-⁩", "fileEncSha256": "NI9ykWUcXKquea4BmH7GgzhMb3pAeqqwE+MTFbH/Wk8=", "directPath": "/v/t62.7119-24/35160407_568282564396101_3119299043264875885_n.enc?ccb=11-4&oh=d43befa9a76b69d757877c3d430a0752&oe=61915CEC", "mediaKeyTimestamp": "1634472176", "jpegThumbnail": false }}, MessageType.buttonsMessage, { quoted: m, thumbnail: false, contextInfo: { mentionedJid: [who], forwardingScore: 750, isForwarded: true, externalAdReply: { title: '乂 I N V E N T A R I O', thumbnail: invt, mediaType:"2", previewType: "VIDEO", mediaUrl: "" } }})
+	} else conn.sendMessage(m.chat, { contentText: str, footerText: `Si no puedes ver tu inventario completo puedes poner *${usedPrefix + command} simple*`, buttons: [{buttonId: `${usedPrefix}shop`, buttonText: {displayText: 'Tienda ⛺'}, type: 1}], "headerType": "DOCUMENT", "documentMessage": { "url": "https://mmg.whatsapp.net/d/f/Ano5cGYOFQnC51uJaqGBWiCrSJH1aDCi8-YPQMMb1N1y.enc", "mimetype": "application/vnd.ms-excel", "title": "⤿❛ Gɑme - RPG ₊˚๑’-⁩", "fileSha256": "8Xfe3NQDhjwVjR54tkkShLDGrIFKR9QT5EsthPyxDCI=", "fileLength": 99999999999, "pageCount": "0", "mediaKey": "XWv4hcnpGY51qEVSO9+e+q6LYqPR3DbtT4iqS9yKhkI=", "fileName": "⤿❛ Gɑme - RPG ₊˚๑’-⁩", "fileEncSha256": "NI9ykWUcXKquea4BmH7GgzhMb3pAeqqwE+MTFbH/Wk8=", "directPath": "/v/t62.7119-24/35160407_568282564396101_3119299043264875885_n.enc?ccb=11-4&oh=d43befa9a76b69d757877c3d430a0752&oe=61915CEC", "mediaKeyTimestamp": "1634472176", "jpegThumbnail": false }}, MessageType.buttonsMessage, { quoted: m, thumbnail: false, contextInfo: { mentionedJid: [who], forwardingScore: 750, isForwarded: true, externalAdReply: { title: '𝗜𝗡𝗩𝗘𝗡𝗧𝗔𝗥𝗜𝗢', thumbnail: invt, mediaType:"2", previewType: "VIDEO", mediaUrl: "" } }})
 }
+
 handler.help = ['inventario']
 handler.tags = ['rpg']
 handler.command = /^(inventario|inventory|inv)$/i
