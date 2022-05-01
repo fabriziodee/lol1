8ecdd5770131c6eb17bc363ab31d4d4841f52d9d
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-30 21:54:37 -0500

    Update leaderboard.js

--- plugins/leaderboard.js
+++ plugins/leaderboard.js
@@ -1,33 +1,15 @@
 let handler = async (m, { conn, args }) => {
-  const name = m.fromMe ? conn.user : conn.contacts[m.sender] 
-  const sortedExp = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].exp - a[1].exp)
-  const sortedLim = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].limit - a[1].limit)
-  const sortedmoney = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].money - a[1].money)
-  const sortedlevel = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].level - a[1].level)
-  const usersExp = sortedExp.map(v => v[0])
-  const usersLim = sortedLim.map(v => v[0])
-  const usersmoney = sortedmoney.map(v => v[0])
-  const userslevel = sortedlevel.map(v => v[0])
-  const len = args[0] && args[0].length > 0 ? Math.min(100, Math.max(parseInt(args[0]), 5)) : Math.min(7, sortedExp.length)
-
-let type = (args[0] || ' ').toLowerCase()
-
-switch (type) {
-case 'dinero':
-case 'money':
-          let dinero = `\t\t\t*T O P - D I N E R O*
-
-${sortedmoney.slice(0, len).map(([user, data], i) => (i + 1) + '. @' + user.split`@`[0] + `\n*💵 Dinero:* ${shortNum(data.money)}\n\n`).join`\n`}
-`
-conn.reply(m.chat, dinero, m, {
-    contextInfo: {
-      mentionedJid: [...userslevel.slice(0, len), ...usersmoney.slice(0, len), ...usersLim.slice(0, len)]
-    }
-  })
-break
-
-default:
-let text = `
+  let name = m.fromMe ? conn.user : conn.contacts[m.sender] 
+  let sortedExp = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].exp - a[1].exp)
+  let sortedLim = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].limit - a[1].limit)
+  let sortedmoney = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].money - a[1].money)
+  let sortedlevel = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].level - a[1].level)
+  let usersExp = sortedExp.map(v => v[0])
+  let usersLim = sortedLim.map(v => v[0])
+  let usersmoney = sortedmoney.map(v => v[0])
+  let userslevel = sortedlevel.map(v => v[0])
+  let len = args[0] && args[0].length > 0 ? Math.min(100, Math.max(parseInt(args[0]), 5)) : Math.min(7, sortedExp.length)
+  let text = `
 • *💫 Top ${len} de clasificacion de nivel* •
 Posicion: *${userslevel.indexOf(m.sender) + 1}* de *${userslevel.length}*
 
@@ -48,7 +30,6 @@ ${sortedLim.slice(0, len).map(([user, data], i) => (i + 1) + '. @' + user.split`
       mentionedJid: [...userslevel.slice(0, len), ...usersmoney.slice(0, len), ...usersLim.slice(0, len)]
     }
   })
-}
 }
 
 handler.help = ['lb']
