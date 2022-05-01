ed0bbbdd8b3d5753cf4dc8a7cc7ca648023ab497
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-30 20:10:18 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -2,7 +2,7 @@ let { MessageType } = require('@adiwajshing/baileys')
 let fs = require('fs')
 
 let handler = async (m, { conn }) => {
-let tumb = fs.readFileSync('./storage/gif/Shop.gif')
+let tumb = fs.readFileSync('./storage/image/tienda.jpg')
 let menux =  await conn.prepareMessage(m.chat, tumb, MessageType.image, { quoted: m, thumbnail: tumb })
 gbutsan = [{ buttonId: 'test', buttonText: { displayText: 'Test' }, type: 1 }, { buttonId: 'test', buttonText: { displayText: 'Test 2' }, type: 1 }]
 gbuttonan = { imageMessage: menux.message.imageMessage, contentText: '*Test shop*', footerText: '-', buttons: gbutsan, headerType: 4 }
