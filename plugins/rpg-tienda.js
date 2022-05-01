0d665557b05bcd5edb7be18b11bb381b759855d1
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-30 20:34:40 -0500

    Update rpg-tienda.js

--- plugins/rpg-tienda.js
+++ plugins/rpg-tienda.js
@@ -3,6 +3,30 @@ let fs = require('fs')
 
 let handler = async (m, { conn, usedPrefix, command }) => {
 let tumb = fs.readFileSync('./storage/image/tienda.jpg')
+let shop = `*Herramientas para Comprar*
+
+*• Pico de madera:*
+Costo: $15000
+
+*• Pico de piedra:*
+Costo: $25000
+
+*• Pico de hierro:*
+Costo: $45000
+
+*• Pico de oro:*
+Costo: $55000
+
+*• Pico de diamante:*
+Costo: $75000
+
+*Intems para Vender*
+
+*• Minerales para vender:*
+Diamante:     $750
+Oro:                $500
+Hierro:           $150
+Piedra:           $10`
 let menux =  await conn.prepareMessage(m.chat, tumb, MessageType.image, { quoted: m, thumbnail: tumb })
 gbutsan = [{ buttonId: `${usedPrefix}inv`, buttonText: { displayText: 'Inventario 🎒' }, type: 1 }, { buttonId: `${usedPrefix}energía`, buttonText: { displayText: 'Energía ⚡' }, type: 1 }]
 gbuttonan = { imageMessage: menux.message.imageMessage, contentText: '*Test shop*', footerText: '-', buttons: gbutsan, headerType: 4 }
