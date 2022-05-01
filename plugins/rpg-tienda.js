0f4eb64a894c514b543b57267a18118e5bf7291b
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-30 20:37:21 -0500

    Update rpg-tienda.js

--- plugins/rpg-tienda.js
+++ plugins/rpg-tienda.js
@@ -29,7 +29,7 @@ Hierro:           $150
 Piedra:           $10`
 let menux =  await conn.prepareMessage(m.chat, tumb, MessageType.image, { quoted: m, thumbnail: tumb })
 gbutsan = [{ buttonId: `${usedPrefix}inv`, buttonText: { displayText: 'Inventario 🎒' }, type: 1 }, { buttonId: `${usedPrefix}energía`, buttonText: { displayText: 'Energía ⚡' }, type: 1 }]
-gbuttonan = { imageMessage: menux.message.imageMessage, contentText: '*Test shop*', footerText: '-', buttons: gbutsan, headerType: 4 }
+gbuttonan = { imageMessage: menux.message.imageMessage, contentText: shop, footerText: '-', buttons: gbutsan, headerType: 4 }
 await conn.sendMessage(m.chat, gbuttonan, MessageType.buttonsMessage, { contextInfo: { mentionedJid: [m.sender], forwardingScore: 750, isForwarded: true }, quoted: m })
 }
 
