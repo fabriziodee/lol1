2ad52d2dd8598b3b288a457ba2cc564deed692f9
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-30 19:42:38 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -4,7 +4,7 @@ let fs = require('fs')
 let handler = async (m, { conn }) => {
 let tumb = fs.readFileSync('./storage/gif/Shop.gif')
 await conn.sendFile(m.chat, tumb, 'out.gif', 'Test', m, 0, { mimetype: 'video/gif' })
-//await conn.sendMessage(m.chat, tumb, MessageType.video, { mimetype: 'video/gif', filename: `Shop.gif`, quoted: m, caption: 'Test'})
+//await conn.sendMessage(m.chat, "https://telegra.ph/file/1b948dd3985abfb5aecf4.mp4", MessageType.video, { mimetype: 'video/gif', filename: `Shop.gif`, quoted: m, caption: 'Test'})
 }
 
 handler.command = /^(test)$/i
