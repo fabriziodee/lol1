adf1c3b21f8be419d0fafeac3f9f2291f4ffb74a
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-30 19:35:20 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -3,7 +3,8 @@ let fs = require('fs')
 
 let handler = async (m, { conn }) => {
 let tumb = fs.readFileSync('./storage/gif/Shop.gif')
-await conn.sendMessage(m.chat, tumb, MessageType.video, { mimetype: 'video/gif', filename: `Shop.gif`, quoted: m, caption: 'Test'})
+await conn.sendFile(m.chat, tumb, 'out.gif', 'Test', m, 0, { mimetype: 'video/gif' })
+//await conn.sendMessage(m.chat, tumb, MessageType.video, { mimetype: 'video/gif', filename: `Shop.gif`, quoted: m, caption: 'Test'})
 }
 
 handler.command = /^(test)$/i
