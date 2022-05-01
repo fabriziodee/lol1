92c07be044239966cfffea2fcd1811e8570a0f36
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-04-30 19:55:14 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -3,7 +3,7 @@ let fs = require('fs')
 
 let handler = async (m, { conn }) => {
 let tumb = fs.readFileSync('./storage/gif/Shop.gif')
-let menux =  await conn.prepareMessage(m.chat, tumb, MessageType.video, { quoted: m, thumbnail: tumb })
+let menux =  await conn.prepareMessage(m.chat, tumb, MessageType.image, { quoted: m, thumbnail: tumb })
 gbutsan = [{ buttonId: 'test', buttonText: { displayText: 'Test' }, type: 1 }, { buttonId: 'test', buttonText: { displayText: 'Test 2' }, type: 1 }]
 gbuttonan = { imageMessage: menux.message.imageMessage, contentText: '*Test shop*', footerText: '-', buttons: gbutsan, headerType: 4 }
 await conn.sendMessage(m.chat, gbuttonan, MessageType.buttonsMessage, { contextInfo: { mentionedJid: [m.sender], forwardingScore: 750, isForwarded: true }, quoted: m })
