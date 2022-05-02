ecbb77bcdc5f3ce3a652b648b8aa49481bf3af5b
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-02 11:45:56 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -2,11 +2,13 @@ let { MessageType } = require('@adiwajshing/baileys')
 let fs = require('fs')
 
 let handler = async (m, { conn }) => {
-let tumb = fs.readFileSync('./storage/image/tienda.jpg')
-let menux =  await conn.prepareMessage(m.chat, tumb, MessageType.image, { quoted: m, thumbnail: tumb })
-gbutsan = [{ buttonId: 'test', buttonText: { displayText: 'Test' }, type: 1 }, { buttonId: 'test', buttonText: { displayText: 'Test 2' }, type: 1 }]
-gbuttonan = { imageMessage: menux.message.imageMessage, contentText: '*Test shop*', footerText: '-', buttons: gbutsan, headerType: 4 }
-await conn.sendMessage(m.chat, gbuttonan, MessageType.buttonsMessage, { contextInfo: { mentionedJid: [m.sender], forwardingScore: 750, isForwarded: true }, quoted: m })
+
+let ss = ["./storage/sticker/Bot.webp", "./storage/sticker/Bot1.webp"]
+let vn = ss[Math.floor(Math.random() * ss.length)]
+let pp = await conn.getProfilePicture(m.sender) 
+let ppp = await(await fetch(pp)).buffer()
+await conn.sendMessage(m.chat, `*Test*`, MessageType.text, { quoted: m, contextInfo: { externalAdReply: {title: conn.getName(m.sender), body:"© lolibot", previewType: "VIDEO", thumbnail: ppp, sourceUrl: "https://youtu.be/3_5zlC6NExY"}}})
+       
 }
 
 handler.command = /^(test)$/i
