6bb14b143146874aa9c513edd5a943c97240e257
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-04 05:46:38 -0500

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -8,15 +8,15 @@ handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
   let isGroupLink = linkRegex.exec(m.text)
 
   if (chat.antilink && isGroupLink) {
+    if (isAdmin) return m.reply('Menos mal que eres un administrador')
+    if (!isBotAdmin) return m.reply('Menos mal que no soy administradora del grupo')
     await this.groupRemove(m.chat, [m.sender])
-    await this.sendMessage(m.chat, `\t\t\t\t*‧ [ ⭕ Enlace Detectado ⭕ ] ‧*\n\n• Nombre: @${m.sender.split("@s.whatsapp.net")[0]}\n• Fecha: ${date}\n• Enlace: WhatsApp.com`, MessageType.text, { quoted: m, contextInfo: { mentionedJid: [m.sender] } })
-    //if (isAdmin) return m.reply('Menos mal que eres un administrador')
-    //if (!isBotAdmin) return m.reply('Menos mal que no soy administradora del grupo')
+    await this.sendMessage(m.chat, `\t\t\t\t*‧ ⭕ Enlace Detectado ⭕ ‧*\n\n*• Nombre:* @${m.sender.split("@s.whatsapp.net")[0]}\n*• Fecha:* ${date}\n*• Enlace:* WhatsApp.com`, MessageType.text, { quoted: m, contextInfo: { mentionedJid: [m.sender] } })
     //let linkGC = ('https://chat.whatsapp.com/' + await this.groupInviteCode(m.chat))
     //let isLinkThisGc = new RegExp(linkGC, 'i')
     //let isgclink = isLinkThisGc.test(m.text)
     //if (isgclink) return m.reply('Menos mal que este enlace es del grupo :v')
-    await this.groupRemove(m.chat, [m.sender])
+    //await this.groupRemove(m.chat, [m.sender])
   }
   return true
 }
