f16d93f955d002ef290cdf00180ad543ca6a5b7d
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-03 19:41:30 -0500

    Update vote-start.js

--- plugins/vote-start.js
+++ plugins/vote-start.js
@@ -2,8 +2,10 @@ let handler = async (m, { conn, text, usedPrefix, command, isPrems, }) => {
     let user = global.DATABASE._data.users[m.sender]
     if (!text) throw `*Escriva un texto para la votación*\n\n- Ejemplo: ${usedPrefix + command} soy gay?`
     if (!isPrems && user.limit < 1) return conn.reply(m.chat, `Te quedaste sin limites, compra más escribiendo ${usedPrefix}buylimit`, m)
+    if (!isPrems && user.limit < 1) {
     global.DATABASE._data.users[m.sender].limit -= 1
     conn.reply(m.chat, `🎟️ Se utilizó *1* de su limite!`, m)
+    }
     conn.vote = conn.vote ? conn.vote : {}
     let id = m.chat
     if (id in conn.vote) {
