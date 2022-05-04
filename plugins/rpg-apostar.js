5cbe8c4a4fcb43aefb7b19d4f7e5120f187b4014
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-03 19:12:44 -0500

    Update rpg-apostar.js

--- plugins/rpg-apostar.js
+++ plugins/rpg-apostar.js
@@ -4,7 +4,7 @@ async function handler(m, { conn, args, text, usedPrefix, command }) {
     if (!text) return m.reply(`*Ingrese la cantidad de dinero que desea apostar*\n\n- Ejemplo: ${usedPrefix + command} 100`)
     let user = global.DATABASE._data.users[m.sender]
     let count = (args[0] && number(parseInt(args[0])) ? Math.max(parseInt(args[0]), 1) : /all/i.test(args[0]) ? Math.floor(parseInt(user.money)) : 1) * 1
-    if ((user.money * 1) < count) return conn.sendMessage(m.chat, 'Tu dinero no es suficiente para apostar!', { quoted: m })
+    if ((user.money * 1) < count) return m.reply('Tu dinero no es suficiente para apostar!')
     let moneyDulu = user.money * 1
     let Bot = (Math.ceil(Math.random() * 100)) * 1
             let Kamu = (Math.floor(Math.random() * 86)) * 1
@@ -19,12 +19,12 @@ async function handler(m, { conn, args, text, usedPrefix, command }) {
                 user.money += (Math.floor(count / 1.5)) * 1
             }
             m.reply(`
-💸 *APUESTA* 💸
+*‧ 💸 APUESTA 💸 ‧*
 
 Bot roll: *${Bot}*
 User roll: *${Kamu}*
 
-Tu *${status}* ${status == 'Ganas!' ? `*+${count * 2}*` : status == 'Pierdes!' ? ` *-${count * 1}*` : `*+${Math.floor(count / 1.5)}*`} de dinero
+*Tu ${status}* ${status == 'Ganas!' ? `\n+${count * 2}` : status == 'Pierdes!' ? `\n-${count * 1}` : `\n+${Math.floor(count / 1.5)}`} de dinero
 `.trim())
 }
   
