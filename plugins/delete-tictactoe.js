86538d6c119732856b417d3764923efb60561782
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-03 19:02:14 -0500

    Update delete-tictactoe.js

--- plugins/delete-tictactoe.js
+++ plugins/delete-tictactoe.js
@@ -2,6 +2,7 @@ let { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn, usedPrefix, command }) => {
 let room = Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender) && room.state == 'PLAYING')
+if (room) return m.reply('Todavía no estas en ninguna partida')
 delete conn.game[room.id]
 await m.reply('Se eliminó la partida!')
 }
