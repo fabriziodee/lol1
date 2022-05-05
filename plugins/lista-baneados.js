f92a5da41ab214912d4655655970e102d4433da4
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-05 16:47:24 -0500

    Update lista-baneados.js

--- plugins/lista-baneados.js
+++ plugins/lista-baneados.js
@@ -1,14 +1,13 @@
 let handler = async (m, { conn, usedPrefix }) => {
     let chats = Object.entries(global.DATABASE._data.chats).filter(chat => chat[1].isBanned)
     let users = Object.entries(global.DATABASE._data.users).filter(user => user[1].Banneduser)
-    m.reply(`\t\t*‧ 💌 Lista de chats Baneados 💌 ‧*
+    m.reply(`\t\t*‧ 📝 Lista de Baneados 📝 ‧*
 
+*Grupos*
 *Total:*  ${chats.length} Chats${chats ? '\n\n' + chats.map(([jid], i) => `${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
 *Jid:* ${jid}`.trim()).join('\n\n') : ''}
 
-
-\t\t*‧ 📧 Lista de usuarios Baneados 📧 ‧*
-
+*Usuarios*
 *Total:* ${users.length} Usuarios${users ? '\n\n' + users.map(([jid], i) => `${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
 *Jid:* ${jid}`.trim()).join('\n\n') : ''}`
 }
