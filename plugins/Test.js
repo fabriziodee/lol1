24d634d17657a64554ffbf9c57457862c8cace78
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-08 02:35:53 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -6,7 +6,7 @@ let fetch = require('node-fetch')
 
 let handler = async (m, { conn }) => {
 let giftest = fs.readFileSync('./storage/gif/Shop.gif')
-let out = './storage/gif/Shop.gif'
+let out = '../storage/gif/Shop.gif'
 conn.sendFile(m.chat, out, 'out.gif', '*Se convirtió a GIF*', m, 0, { mimetype: 'video/gif' })
 //conn.sendMessage(m.chat, giftest, MessageType.video, { mimetype: 'video/gif', filename: 'stick.gif', quoted: m, caption: 'Test' })    
 //conn.sendMessage(m.chat, giftest, MessageType.video, { mimetype: 'video/mp4', filename: 'stick.gif', quoted: m, caption: 'Test' })
