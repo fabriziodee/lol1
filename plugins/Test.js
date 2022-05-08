8cf8db6774bcefcc6ef2981b6d00cdcd742f01aa
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-08 02:31:08 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -6,8 +6,8 @@ let fetch = require('node-fetch')
 
 let handler = async (m, { conn }) => {
 let giftest = fs.readFileSync('./storage/gif/Shop.gif')
-conn.sendMessage(from, giftest, MessageType.video, { mimetype: 'video/gif', filename: 'stick.gif', quoted: m, caption: 'Test' })    
-conn.sendMessage(from, giftest, MessageType.video, { mimetype: 'video/mp4', filename: 'stick.gif', quoted: m, caption: 'Test' })
+conn.sendMessage(m.chat, giftest, MessageType.video, { mimetype: 'video/gif', filename: 'stick.gif', quoted: m, caption: 'Test' })    
+conn.sendMessage(m.chat, giftest, MessageType.video, { mimetype: 'video/mp4', filename: 'stick.gif', quoted: m, caption: 'Test' })
 }
 
 handler.command = /^(test)$/i
