6dcaa18820b516fb9e6e5d22d032295a2cac8a20
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-08 05:52:20 -0500

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -179,7 +179,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
      let tumbb = fs.readFileSync('./storage/image/menu2.jpg')
      let tumbv = fs.readFileSync('./storage/gif/loli_logo.gif')
      let chatp = global.DATABASE._data.chats[m.chat]
-     await conn.sendMessage(m.chat, tumbv, MessageType.video, { mimetype: 'video/gif', filename: 'loli_logo.gif', quoted: m, thumbnail: tumbv, caption: text.trim() })
+     //await conn.sendMessage(m.chat, tumbv, MessageType.video, { mimetype: 'video/gif', filename: 'loli_logo.gif', quoted: m, thumbnail: tumbv, caption: text.trim() })
      if (chatp.menu == 1) {
      let imeg = await conn.prepareMessage(m.chat, tumbb, 'imageMessage')
      let imeu = imeg.message.imageMessage
