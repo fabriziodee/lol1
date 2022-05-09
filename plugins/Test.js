6e044acfbbc895045393aabeed0a8326b8ce5332
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-09 09:59:15 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -5,7 +5,8 @@ let fs = require("fs")
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn }) => {
-let giftest = fs.readFileSync('./storage/gif/loli_logo.gif')
+let giftest = fs.readFileSync('./storage/gif/loli_logo.mp4')
+conn.sendMessage(m.chat, giftest, MessageType.video, { mimetype: Mimetype.gif, quoted : m })
 let out = 'https://raw.githubusercontent.com/g4tito/lolibot/master/storage/gif/Shop.gif'
 //conn.sendFile(m.chat, out, 'out.gif', '*Se convirtió a GIF*', m, 0, { mimetype: 'video/gif' })
 conn.sendMessage(m.chat, giftest, MessageType.video, { mimetype: 'video/gif', filename: 'stick.gif', quoted: m, thumbnail: giftest, caption: `${'Test Test test test test\n'.repeat(1000)}` })    
