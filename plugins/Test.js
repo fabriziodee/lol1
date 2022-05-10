da29db789811335687213a29bc3b8f3f4e5616a5
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-10 16:13:14 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -5,15 +5,16 @@ let fs = require("fs")
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn }) => {
-let giftest = fs.readFileSync('./storage/video/shop.mp4')
-conn.sendMessage(m.chat, giftest, MessageType.video, { mimetype: Mimetype.gif, quoted : m })
-let out = 'https://raw.githubusercontent.com/g4tito/lolibot/master/storage/gif/Shop.gif'
-//conn.sendFile(m.chat, out, 'out.gif', '*Se convirtió a GIF*', m, 0, { mimetype: 'video/gif' })
-//conn.sendMessage(m.chat, giftest, MessageType.video, { mimetype: 'video/gif', filename: 'stick.gif', quoted: m, thumbnail: giftest, caption: `${'Test Test test test test\n'.repeat(1000)}` })    
-//conn.sendMessage(m.chat, giftest, MessageType.video, { mimetype: 'video/mp4', filename: 'stick.gif', quoted: m, caption: 'Test' })
+let tt = 150000
+m.reply(`${shortNum(tt)}`)
 }
 
 handler.command = /^(test)$/i
 handler.owner = true
 
 module.exports = handler
+
+function shortNum(num) {
+return new (num).toLocaleString('es-US', { style: 'currency', currency: 'USD', }).replace(/,/g, '.')
+//return new Intl.NumberFormat('en-GB', { notation: "compact", compactDisplay: "short" }).format(num)
+}
