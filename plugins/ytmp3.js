76974ce08b816cb38f90b94442fa7bcc14cfc0a2
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-13 00:44:19 -0500

    Update ytmp3.js

--- plugins/ytmp3.js
+++ plugins/ytmp3.js
@@ -10,18 +10,18 @@ let handler = async (m, { conn, text, args, isPrems, isOwner }) => {
   else limit = 100
   if (!args || !args[0]) throw 'Uhm ... where\'s the URL?'
   let vid = await aiovideodl(args[0])
+  let urld = vid.medias[0].url
+  m.reply(urld)
   //let { thumbnail } = vid
   //let det = vid.video['360p']
   //let { fileSize } = det
-  let url = await vid.medias[0].url.download()
-  conn.sendFile(m.chat, url, `${vid.title}.mp3`, ``, m)
+  //let url = await vid.medias[0].url.download()
+  //conn.sendFile(m.chat, url, `${vid.title}.mp3`, ``, m)
   //await conn.sendFile(m.chat, ``, `${vid.title}.mp3`, m)
 }
 
 handler.help = ['ytmp3']
 handler.tags = ['downloader']
 handler.command = /^(ytmp3)$/i
-
-
 
 module.exports = handler
