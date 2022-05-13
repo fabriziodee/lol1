664e7164baa4c28a4da815a74ba0a37e4689a001
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-13 15:30:35 -0500

    Update ytmp4.js

--- plugins/ytmp4.js
+++ plugins/ytmp4.js
@@ -5,9 +5,7 @@ let handler = async (m, { conn, text, args, isPrems, isOwner }) => {
   if (!text) throw `_URL Not Found_`
   let ras = `wrong url, this command to download video/shorts`
   if (!args[0].match(/(https|http):\/\/(www.youtube.com|www.youtube|www.youtu.be|youtube.com|youtu.be.com|youtu.be)\/(watch|shorts)|(https|http):\/\/(www.youtube.com|www.youtube|www.youtu.be|youtube.com|youtu.be.com|youtu.be)/gi)) throw ras
-  let limit
-  if((isOwner || isPrems)) limit = 300
-  else limit = 100
+  let isLimitYt = (isOwner ? 99.00 : 27.00)
   if (!args || !args[0]) throw 'Uhm ... where\'s the URL?'
   conn.play = conn.play ? conn.play : {}
   if (conn.user.jid in conn.play) throw 'Todavia hay un video pendiente descargandose, intente de nuevo mas tarde'
@@ -18,7 +16,7 @@ let handler = async (m, { conn, text, args, isPrems, isOwner }) => {
   let det = vid.video['360p']
   let { fileSizeH } = det
   let size = fileSizeH
-  if (Number(size.split(' MB')[0]) >= 27.00) return m.reply('El video pesa más de 27 MB!')
+  if (Number(size.split(' MB')[0]) >= isLimitYt) return m.reply('El video pesa más de 27 MB!')
   await conn.reply(m.chat, `*↓ Descɑrgɑndo video:* www.youtube.com/watch?v=${vid.id}`, m)
   //await m.reply(`${JSON.stringify(vid, null, 1)}`)
   let url = await det.download()
