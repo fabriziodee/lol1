8d0f3be03b5f32a2dbb41c6b0cf0b7cec576030a
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-17 16:19:47 -0500

    Update spamchat.js

--- plugins/spamchat.js
+++ plugins/spamchat.js
@@ -36,22 +36,12 @@ let handler = async(m, { conn, text }) => {
     await m.reply(pesan)
 
 }
-handler.help = ['spamchat'].map(v => v + ' <teks>')
+
+handler.help = ['spamchat']
 handler.tags = ['General']
 handler.command = /^(spamchat)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-handler.register = false
-handler.limit = false
-
-handler.admin = false
-handler.botAdmin = false
+handler.premium = true
 
 handler.fail = null
-handler.exp = 100
-handler.limit = false
 
 module.exports = handler
