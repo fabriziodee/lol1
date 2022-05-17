ee07761b1c03d89ea5a6d5d8d7870cf1b05ede18
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-17 13:18:23 -0500

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -170,7 +170,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
       version: conn.browserDescription[2],
       bio: biot.status == 401 ? 'Sin info' : biot.status,
       batery: conn.battery ? `${conn.battery.value}%` : 'Desconocido',
-      batery2: conn.battery.live ? 'Cargando...' : 'Desconectado',
+      batery2: conn.battery ? `${conn.battery.live ? 'Cargando...' : 'Desconectado'}` : 'Desconocido',
       greeting: ucapan(),
       lang: 'JavaScript',
       botname: 'LoliBot',
