3ca84822ebdf4f724800b821e2976d7efcb775cb
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-18 16:29:04 -0500

    Update getmsg.js

--- plugins/getmsg.js
+++ plugins/getmsg.js
@@ -5,7 +5,8 @@ let handler = async (m, { conn, command, usedPrefix, text }) => {
     if (!text in msgs) throw `El mensaje *${text}* no esta registrado`
     //let _m = await conn.serializeM(msgs[text])
     //await _m.copyNForward(m.chat, true)
-    await conn.copyNForward(m.chat, await conn.loadMessage(msgs.jid, msgs.msg), false, { quoted: m })
+    //let _msgs = global.DATABASE._data.msgs[text]
+    await conn.copyNForward(m.chat, await conn.loadMessage(msgs[text].jid, msgs[text].msg), false, { quoted: m })
 }
 
 handler.help = ['getmsg']
