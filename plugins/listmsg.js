2464c4ba8a25eb1d6aece405f1af958d84112522
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-18 17:27:51 -0500

    Update listmsg.js

--- plugins/listmsg.js
+++ plugins/listmsg.js
@@ -2,16 +2,19 @@
 let handler = async (m, { conn, command, usedPrefix, text }) => {
     let msgs = global.DATABASE._data.msgs
     let totalmsg = Object.keys(msgs).length
+    let msga = Object.entries(global.DATABASE._data.msgs).filter(ms => msg[1].mtype)
     m.reply(`\t\t\t*‧ 📂 Almɑcenɑmiento 📂 ‧*
 
 *Totɑl:* ${totalmsg} mensajes
 
-*• Mensɑjes:*
-${Object.keys(msgs).map(v => '- ' + v).join('\n')}
+*• Stickers:*
+${msga ? '\n\n' + msga.map(([msgt], i) => `- ${msgt.mtype == 'image/webp' ? '```Error```' : `${msgt}``).join('\n\n') : ''}
 
 Escriba *${usedPrefix}getmsg < mensaje >* para
 enviar un archivo del almacenamiento`)
 }
+
+//${Object.keys(msgs).map(v => '- ' + v).join('\n')}
 
 handler.help = ['listmsg']
 handler.tags = ['database']
