858c74a1b8df4d36943de838b9753c78ef0337bb
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-18 16:14:11 -0500

    Update addmsg.js

--- plugins/addmsg.js
+++ plugins/addmsg.js
@@ -1,16 +1,17 @@
 let handler = async (m, { command, usedPrefix, text }) => {
-    let M = m.constructor
-    let which = command.replace(/add/i, '')
-    if (!m.quoted) throw 'Etiqueta un mensaje!'
-    if (!text) throw `Escriba *${usedPrefix}list${which}* para ver la lista de mensajes`
+    //let M = m.constructor
+    //let which = command.replace(/add/i, '')
+    if (!m.quoted) throw 'Etiqueta un mensaje para añadir a la database!'
+    if (!text) throw `Escriba *${usedPrefix}listmsg* para ver la lista de mensajes`
     let msgs = global.DATABASE._data.msgs
-    if (text in msgs) throw `'${text}' se ha registrado en la lista de mensajes`
-    msgs[text] = M.toObject(await m.getQuotedObj())
-    m.reply(`*El mensaje se agregó a la lista de mensajes como '${text}'*\n\nSi quieres ver el mensaje escriba ${usedPrefix}ver${which} ${text}`)
+    if (text in msgs) throw `Se añadió el mensaje con el nombre *${text}*`
+    msgs[text] = { jid: m.chat, idmsg: m.quoted.id, mtype: m.quoted.mimetype }
+    m.reply(`Se añadió el mensaje con el nombre *${text}*\n\nSi quieres ver el mensaje escriba ${usedPrefix}getmsg ${text}`)
 }
-handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'sticker'].map(v => 'add' + v + '')
+
+handler.help = ['addmsg']
 handler.tags = ['database']
-handler.command = /^add(vn|msg|video|audio|img|sticker)$/
+handler.command = /^add|añadir(mensaje|msg)$/
 handler.rowner = true
 
 module.exports = handler
