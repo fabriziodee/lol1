49b727e424f9e995c9da744de2c8775aa3123fc5
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-18 19:12:08 -0500

    Update delmsg.js

--- plugins/delmsg.js
+++ plugins/delmsg.js
@@ -2,6 +2,8 @@ let handler = async (m, { command, usedPrefix, text }) => {
     let which = command.replace(/del/i, '')
     if (!text) throw `Usar *${usedPrefix}list${which}* para ver la lista`
     let msgs = global.DATABASE._data.msgs
+    let _msgs global.DATABASE._data.msgs[text]
+    if (_msgs == undefined) return m.reply(`El mensaje *${text}* no está registrado en la database!`)
     if (!text in msgs) throw `'${text}' no registrado en la lista de mensajes`
     delete msgs[text]
     m.reply(`Eliminó con éxito el mensaje en la lista de mensajes con el nombre '${text}'`)
