c90a683ddf776f9dfd0a299750ecc751bd982b17
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-18 19:11:37 -0500

    Update getmsg.js

--- plugins/getmsg.js
+++ plugins/getmsg.js
@@ -2,7 +2,9 @@ let handler = async (m, { conn, command, usedPrefix, text }) => {
     //let which = command.replace(/ver/i, '')
     if (!text) throw `Escriba ${usedPrefix}listmsg para ver la lista de mensajes!`
     let msgs = global.DATABASE._data.msgs
-    if (!text in msgs) throw `El mensaje *${text}* no esta registrado`
+    let _msgs global.DATABASE._data.msgs[text]
+    if (_msgs == undefined) return m.reply(`El mensaje *${text}* no está registrado en la database!`)
+    if (!text in msgs) throw `El mensaje *${text}* no esta registrado!`
     //let _m = await conn.serializeM(msgs[text])
     //await _m.copyNForward(m.chat, true)
     //let _msgs = global.DATABASE._data.msgs[text]
