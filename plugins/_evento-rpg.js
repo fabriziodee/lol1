6990b4f9f019b244de05967d9d7cd190b78cfeac
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-19 15:01:41 -0500

    Update _evento-rpg.js

--- plugins/_evento-rpg.js
+++ plugins/_evento-rpg.js
@@ -3,8 +3,8 @@ let moment = require('moment-timezone')
 let handler = m => m
 
 handler.all = async function (m) {
-	//let eve = await moment.tz('Asia/Jakarta').format('H')
-	//if (eve == 3) {
+	let eve = await moment.tz('Asia/Jakarta').format('H')
+	if (eve == 3) {
 	//this.reply(m.chat, '*Test event . . .*')
 	//this.event = this.event ? this.event : {}
 	//this.event[m.chat] = true
