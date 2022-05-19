d7f5cfcf5352c8c695c684b1631eb5acb89cdeb8
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-19 15:08:09 -0500

    Update _evento-rpg.js

--- plugins/_evento-rpg.js
+++ plugins/_evento-rpg.js
@@ -3,11 +3,12 @@ let moment = require('moment-timezone')
 let handler = m => m
 
 handler.all = async function (m) {
+	this.event = this.event ? this.event : {}
+	if (m.chat in this.event) return false
+	this.event[m.chat] = true
 	let eve = await moment.tz('Asia/Jakarta').format('H')
 	if (eve == 3) {
-	//this.reply(m.chat, '*Test event . . .*')
-	//this.event = this.event ? this.event : {}
-	//this.event[m.chat] = true
+	this.reply(m.chat, '*Test event . . .*')
 	//this.sendButton(m.chat, `\n*Caba de caer una caja 📦 misteriosa*\n`, '', 'Recojer!', '.recoger')
     }
 
