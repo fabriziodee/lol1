1e3cd593eebc998ff3ed464c0e8531f8e7d651ef
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-20 17:30:45 -0500

    Update slots.js

--- plugins/slots.js
+++ plugins/slots.js
@@ -6,6 +6,7 @@ let handler = async (m, { conn, text }) => {
     if (!text) throw 'Ingrese la una cantidad de dinero!'
     if (!num.test(text)) throw 'La cantidad debe ser un número'
     let money = text * 1
+    let _money = text / 2
     let jackpot = Math.ceil(money * 5)
     let win = Math.ceil(money * 2)
     if (money < 70) throw 'Minimo 70 de dinero'
@@ -39,20 +40,20 @@ let handler = async (m, { conn, text }) => {
         hasil = `*Ganaste x3!*\n+${jackpot} de Dinero`;
         gcha = `${x[0]} | ${y[0]} | ${z[0]}\n${x[1]} | ${y[1]} | ${z[1]} *⇐*\n${x[2]} | ${y[2]} | ${z[2]}`;
         global.DATABASE._data.users[m.sender].money += jackpot
-        await conn.fakeReply(m.chat, `*[ 🎰 SLOT 🎰 ]*\n\n${gcha}\n\n*[ 🎰 VIRTUAL SLOT 🎰 ]*`, '0@s.whatsapp.net', `${hasil}`, 'status@broadcast')
+        await conn.fakeReply(m.chat, `*[ 🎰 SLOT 🎰 ]*\n\n${gcha}\n\n*[ 🎰 SLOT 🎰 ]*`, '0@s.whatsapp.net', `${hasil}`, 'status@broadcast')
 
     } else if (a == b || a == c || b == c) {
         end = "";
         hasil = `*Ganaste x2!*\n+${win} de Dinero`;
         gcha = `${x[0]} | ${y[0]} | ${z[0]}\n${x[1]} | ${y[1]} | ${z[1]} *⇐*\n${x[2]} | ${y[2]} | ${z[2]}`;
         global.DATABASE._data.users[m.sender].money += win
-        await conn.fakeReply(m.chat, `*[ 🎰 SLOT 🎰 ]*\n\n${gcha}\n\n*[ 🎰 VIRTUAL SLOT 🎰 ]*`, '0@s.whatsapp.net', `${hasil}`, 'status@broadcast')
+        await conn.fakeReply(m.chat, `*[ 🎰 SLOT 🎰 ]*\n\n${gcha}\n\n*[ 🎰 SLOT 🎰 ]*`, '0@s.whatsapp.net', `${hasil}`, 'status@broadcast')
     } else {
         end = "";
-        hasil = `*Suerte la próxima!*\n-${money} de Dinero`;
+        hasil = `*Suerte la próxima!*\n-${_money} de Dinero`;
         gcha = `${x[0]} | ${y[0]} | ${z[0]}\n${x[1]} | ${y[1]} | ${z[1]} *⇐*\n${x[2]} | ${y[2]} | ${z[2]}`;
-        global.DATABASE._data.users[m.sender].money -= money
-        await conn.fakeReply(m.chat, `*[ 🎰 SLOT 🎰 ]*\n\n${gcha}\n\n*[ 🎰 VIRTUAL SLOT 🎰 ]*`, '0@s.whatsapp.net', `${hasil}`, 'status@broadcast')
+        global.DATABASE._data.users[m.sender].money -= _money * 1
+        await conn.fakeReply(m.chat, `*[ 🎰 SLOT 🎰 ]*\n\n${gcha}\n\n*[ 🎰 SLOT 🎰 ]*`, '0@s.whatsapp.net', `${hasil}`, 'status@broadcast')
     }
 }
 
