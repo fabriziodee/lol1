bd9c933353007cfaa94ad5d485500df4c27ac09e
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-21 18:42:29 -0500

    Update nsfw-yaoi.js

--- plugins/nsfw-yaoi.js
+++ plugins/nsfw-yaoi.js
@@ -1,8 +1,6 @@
 const axios = require('axios')
 
 let handler = async(m, { conn }) => {
-let chat = global.DATABASE._data.chats[m.chat]
-if (!chat.nsfw) return m.reply(global.nsfw)
 conn.reply(m.chat, wait, m)
 let les = await axios.get('https://meme-api.herokuapp.com/gimme/yaoi')
 conn.sendFile(m.chat, les.data.url, 'yaoi', `*YAOI*`, m)
@@ -11,5 +9,6 @@ conn.sendFile(m.chat, les.data.url, 'yaoi', `*YAOI*`, m)
 handler.help = ['yaoi']
 handler.tags = ['nsfw']
 handler.command = /^(yaoi)$/i
+handler.nsfw = true
 
 module.exports = handler
