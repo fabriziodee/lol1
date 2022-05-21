c229f6194c46dba94da757dc415d021b7163aadd
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-21 00:48:09 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -5,17 +5,10 @@ let fs = require("fs")
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn }) => {
-
-await conn.fakeReply(m.chat, `*[ 🎰 SLOT 🎰 ]*
-
-🍌 | 🪙 | 🍋
-🍑 | 🍑 | 🍑 *⇐*
-🍎 | 🍅 | 🪙
-
-*[ 🎰 SLOT 🎰 ]*`, '0@s.whatsapp.net', `*Ganaste x3*\n${shortNum(7518783450)} de Dinero`, 'status@broadcast')
+//Test
 }
 
-handler.command = /^(‎slot)$/i
+handler.command = /^(test)$/i
 handler.owner = true
 
 module.exports = handler
