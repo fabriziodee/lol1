29d22384f4812c6b5aad3f502df49dee18f5c4c3
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-20 23:14:43 -0500

    Update slots.js

--- plugins/slots.js
+++ plugins/slots.js
@@ -5,7 +5,7 @@ let num = /([0-9])$/i
 let handler = async (m, { conn, text }) => {
 
     conn.slot = conn.slot ? conn.slot : {}
-    let __waktur = (new Date - conn.slot[m.chat].lastslot)
+    let __waktur = (new Date - conn.slot[m.chat].lastslot || 0)
     let _waktur = (180000 - __waktur)
     let waktur = clockString(_waktur)
 
@@ -22,7 +22,9 @@ let handler = async (m, { conn, text }) => {
     let users = global.DATABASE._data.users
     if (money > users[m.sender].money) throw 'Su dinero no es suficiente'
     if (new Date - conn.slot[m.chat].lastslot > 180000) {
+
     conn.slot[m.chat] = { lastslot: new Date * 1 }
+
     let emojis = ["🍏","🍎","🍊","🍋","🍑","🪙","🍅","🍐","🍒","🥥","🍌"];
     let a = Math.floor(Math.random() * emojis.length);
     let b = Math.floor(Math.random() * emojis.length);
