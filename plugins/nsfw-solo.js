93af18dd5a5c9f7458297e0dc4ab3386dedff893
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-21 18:43:09 -0500

    Update nsfw-solo.js

--- plugins/nsfw-solo.js
+++ plugins/nsfw-solo.js
@@ -1,8 +1,6 @@
 let axios = require("axios")
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-let chat = global.DATABASE._data.chats[m.chat]
-if (!chat.nsfw) return m.reply(global.nsfw)
 conn.reply(m.chat, wait, m)
 let res = await axios("https://nekos.life/api/v2/img/solo")
 let json = res.data
@@ -12,5 +10,6 @@ conn.sendFile(m.chat, json.url, 'solo', '*SOLO*', m)
 handler.help = ['solo']
 handler.tags = ['nsfw']
 handler.command = /^(solo)$/i
+handler.nsfw = true
 
 module.exports = handler
