98a043f11f1288c1c8cbd5b7eadd644ef74a4963
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-21 00:56:14 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -1,18 +1,16 @@
-let { MessageType } = require("@adiwajshing/baileys");
-let util = require('util')
-let path = require('path')
-let fs = require("fs")
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn }) => {
+
+await conn.fakeReply(m.chat, `*[ 🎰 SLOT 🎰 ]*
+🍌 | 🪙 | 🍋
+🍑 | 🍑 | 🍑 *⇐*
+🍎 | 🍅 | 🪙
+*[ 🎰 SLOT 🎰 ]*`, '0@s.whatsapp.net', `*Ganaste x3*\n${shortNum(25062811.5)} de Dinero`, 'status@broadcast')
 //Test
 }
 
-handler.command = /^(test)$/i
+handler.command = /^(‎slot)$/i
 handler.owner = true
 
 module.exports = handler
-
-function shortNum(num) {
-return new Intl.NumberFormat('en-GB', { notation: "compact", compactDisplay: "short" }).format(num)
-}
