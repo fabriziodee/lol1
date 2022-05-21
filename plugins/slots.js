11834da9c1256a222dd95891367a86412872ae85
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-20 23:53:31 -0500

    Update slots.js

--- plugins/slots.js
+++ plugins/slots.js
@@ -4,6 +4,7 @@ let num = /([0-9])$/i
 
 let handler = async (m, { conn, text }) => {
 
+    conn.slot = conn.slot ? conn.slot : {}
     conn.slot = conn.slot ? conn.slot : conn.slot[m.sender] = { lastslot: 0 }
     let __waktur = (new Date - conn.slot[m.sender].lastslot)
     let _waktur = (3000 - __waktur)
