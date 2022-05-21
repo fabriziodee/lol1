6dfc462122e1b95c5fc28617a51d0355299ce13b
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-21 18:41:27 -0500

    Update nsfw-pack.js

--- plugins/nsfw-pack.js
+++ plugins/nsfw-pack.js
@@ -2,8 +2,6 @@ let fetch = require('node-fetch')
 let fs = require('fs')
 
 let handler = async (m, { conn }) => {
-let chat = global.DATABASE._data.chats[m.chat]
-if (!chat.nsfw) return m.reply(global.nsfw)
 conn.reply(m.chat, wait, m)
 let ra = Math.floor(Math.random() * 74)
 let pack = fs.readFileSync(`./storage/image/pack/gawr_gura/0${ra}.jpg`)
@@ -13,5 +11,6 @@ conn.sendFile(m.chat, pack, 'pack', '*Patreon Umeko J hololive Gawr gura - COSPL
 handler.help = ['pack']
 handler.tags = ['nsfw']
 handler.command = /^(pack)$/i
+handler.nsfw = true
 
 module.exports = handler
