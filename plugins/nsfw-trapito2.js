788cd6934d14c71a8450aa4760f859f171dfd5d5
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-21 18:38:04 -0500

    Update nsfw-trapito2.js

--- plugins/nsfw-trapito2.js
+++ plugins/nsfw-trapito2.js
@@ -1,8 +1,6 @@
 let axios = require("axios")
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-let chat = global.DATABASE._data.chats[m.chat]
-if (!chat.nsfw) return m.reply(global.nsfw)
 conn.reply(m.chat, wait, m)
 let res = await axios("https://api.waifu.pics/nsfw/trap")
 let json = res.data
@@ -12,5 +10,6 @@ conn.sendFile(m.chat, json.url, 'trapito2', '*UN TRAPITO <3*', m)
 handler.help = ['trapito2']
 handler.tags = ['nsfw']
 handler.command = /^(trapito2|trap2|htrap2)$/i
+handler.nsfw = true
 
 module.exports = handler
