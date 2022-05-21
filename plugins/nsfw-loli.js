6959cf14802a4a62887f07ddb39a76fbcdfe347b
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-21 18:43:45 -0500

    Update nsfw-loli.js

--- plugins/nsfw-loli.js
+++ plugins/nsfw-loli.js
@@ -1,8 +1,6 @@
 let fetch = require('node-fetch')
 
 let handler  = async (m, { conn, usedPrefix, command }) => {
-let chat = global.DATABASE._data.chats[m.chat]
-if (!chat.nsfw) return m.reply(global.nsfw)
 conn.reply(m.chat, wait, m)
 let heum = await fetch(`https://server-api-rey.herokuapp.com/api/wallpaper/nsfwloli?apikey=apirey`)
 let json = await heum.buffer()
@@ -12,5 +10,6 @@ conn.sendFile(m.chat, json, 'hloli', '*LOLI*', m)
 handler.help = ['hloli']
 handler.tags = ['nsfw']
 handler.command = /^(hloli|nsfwloli)$/i
+handler.nsfw = true
 
 module.exports = handler
