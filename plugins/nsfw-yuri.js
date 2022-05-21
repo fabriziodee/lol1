a035ddefbe721b5864ba3e0ee8df6d1a80d8746b
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-21 18:39:28 -0500

    Update nsfw-yuri.js

--- plugins/nsfw-yuri.js
+++ plugins/nsfw-yuri.js
@@ -1,8 +1,6 @@
 let fetch = require('node-fetch')
 
 let handler  = async (m, { conn, usedPrefix, command }) => {
-let chat = global.DATABASE._data.chats[m.chat]
-if (!chat.nsfw) return m.reply(global.nsfw)
 conn.reply(m.chat, wait, m)
 heum = await fetch(`https://api-alc.herokuapp.com/api/nsfw/yuri2?apikey=ConfuMods`)
 json = await heum.buffer()
@@ -12,5 +10,6 @@ conn.sendFile(m.chat, json, 'yuri', '*YURI*', m)
 handler.help = ['yuri']
 handler.tags = ['nsfw']
 handler.command = /^(yuri)$/i
+handler.nsfw = true
 
 module.exports = handler
