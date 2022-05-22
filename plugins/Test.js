ca2f02e6e34d2b7980336d074789ce0cbe2f352c
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-21 21:36:24 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -1,12 +1,13 @@
+let { MessageType } = require("@adiwajshing/baileys");
 let fetch = require('node-fetch')
+const axios = require('axios')
 
 let handler = async (m, { conn }) => {
 
 let encmediats = JSON.parse(JSON.stringify(m).replace('quotedM','m')).message.extendedTextMessage.contextInfo
 let dlfile = await conn.downloadMediaMessage(encmediats)
 let bas64 = `data:image/jpeg;base64,${dlfile.toString('base64')}`
-m.reply(bas64)
-var mantap = await convertSticker(bas64, `-`, `-`)
+var mantap = await convertSticker(bas64, `test`, `test 2`)
 var imageBuffer = new Buffer.from(mantap, 'base64');
 conn.sendMessage(m.chat, imageBuffer, MessageType.sticker, { quoted: m })
 
@@ -17,6 +18,22 @@ handler.owner = true
 
 module.exports = handler
 
-function shortNum(num) {
-return new Intl.NumberFormat('en-GB', { notation: "compact", compactDisplay: "short" }).format(num)
+const axios = require('axios')
+
+function convertSticker(base64, author, pack){
+ return new Promise((resolve, reject) =>{
+axios('https://sticker-api-tpe3wet7da-uc.a.run.app/prepareWebp', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      'Content-Type': 'application/json;charset=utf-8',
+      'User-Agent': 'axios/0.21.1',
+      'Content-Length': 151330
+    },
+    data: `{"image": "${base64}","stickerMetadata":{"author":"${author}","pack":"${pack}","keepScale":true,"removebg":"HQ"},"sessionInfo":{"WA_VERSION":"2.2106.5","PAGE_UA":"WhatsApp/2.2037.6 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36","WA_AUTOMATE_VERSION":"3.6.10 UPDATE AVAILABLE: 3.6.11","BROWSER_VERSION":"HeadlessChrome/88.0.4324.190","OS":"Windows Server 2016","START_TS":1614310326309,"NUM":"6247","LAUNCH_TIME_MS":7934,"PHONE_VERSION":"2.20.205.16"},"config":{"sessionId":"session","headless":true,"qrTimeout":20,"authTimeout":0,"cacheEnabled":false,"useChrome":true,"killProcessOnBrowserClose":true,"throwErrorOnTosBlock":false,"chromiumArgs":["--no-sandbox","--disable-setuid-sandbox","--aggressive-cache-discard","--disable-cache","--disable-application-cache","--disable-offline-load-stale-cache","--disk-cache-size=0"],"executablePath":"C:\\\\Program Files (x86)\\\\Google\\\\Chrome\\\\Application\\\\chrome.exe","skipBrokenMethodsCheck":true,"stickerServerEndpoint":true}}`
+  }).then(({data}) =>{
+    resolve(data.webpBase64)
+  }).catch(reject)
+     
+ }) 
 }
