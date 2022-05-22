eceae36b0302083fa652414f3f4efec74fa1d2a7
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-21 21:29:02 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -1,7 +1,15 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn }) => {
-//Test
+
+let encmediats = JSON.parse(JSON.stringify(m).replace('quotedM','m')).message.extendedTextMessage.contextInfo
+let dlfile = await conn.downloadMediaMessage(encmediats)
+let bas64 = `data:image/jpeg;base64,${dlfile.toString('base64')}`
+m.reply(bas64)
+var mantap = await convertSticker(bas64, `-`, `-`)
+var imageBuffer = new Buffer.from(mantap, 'base64');
+conn.sendMessage(m.chat, imageBuffer, MessageType.sticker, { quoted: m })
+
 }
 
 handler.command = /^(test)$/i
