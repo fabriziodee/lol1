52aa6ce82d51badd6318b3a694ed912d9f474e52
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-24 17:37:51 -0500

    Update grouplist.js

--- plugins/grouplist.js
+++ plugins/grouplist.js
@@ -1,9 +1,10 @@
 let handler = async (m, { conn }) => {
-  let group = conn.chats.all().filter(v => v.jid.endsWith('g.us'))
+  let group = await conn.chats.all().filter(v => v.jid.endsWith('g.us'))
   let txt = ''
   for (let v of group) txt += `${await conn.getName(v.jid)}\n${v.jid} [${v.read_only ? 'Left' : 'Joined'}]\n\n`
-  m.reply( 'List Groups:\n' + txt.trim())
+  m.reply( '*Lista de grupos:*\n' + txt.trim())
 }
+
 handler.help = ['groups', 'grouplist']
 handler.tags = ['info']
 handler.command = /^(gro?up(s|list)|listgro?ups?)$/i
