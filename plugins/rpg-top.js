a2d618500b8bf6ff4288cbbe07cb9f4b7764463f
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-27 20:10:01 -0500

    Update rpg-top.js

--- plugins/rpg-top.js
+++ plugins/rpg-top.js
@@ -1,14 +1,14 @@
 let handler = async (m, { conn, args }) => {
-  let name = m.fromMe ? conn.user : conn.contacts[m.sender] 
-  let sortedExp = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].exp - a[1].exp)
-  let sortedLim = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].limit - a[1].limit)
-  let sortedmoney = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].money - a[1].money)
-  let sortedlevel = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].level - a[1].level)
-  let usersExp = sortedExp.map(v => v[0])
-  let usersLim = sortedLim.map(v => v[0])
-  let usersmoney = sortedmoney.map(v => v[0])
-  let userslevel = sortedlevel.map(v => v[0])
-  let len = args[0] && args[0].length > 0 ? Math.min(100, Math.max(parseInt(args[0]), 5)) : Math.min(10, sortedExp.length)
+  const name = m.fromMe ? conn.user : conn.contacts[m.sender] 
+  const sortedExp = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].exp - a[1].exp)
+  const sortedLim = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].limit - a[1].limit)
+  const sortedmoney = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].money - a[1].money)
+  const sortedlevel = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].level - a[1].level)
+  const usersExp = sortedExp.map(v => v[0])
+  const usersLim = sortedLim.map(v => v[0])
+  const usersmoney = sortedmoney.map(v => v[0])
+  const userslevel = sortedlevel.map(v => v[0])
+  const len = args[0] && args[0].length > 0 ? Math.min(100, Math.max(parseInt(args[0]), 5)) : Math.min(10, sortedExp.length)
 
 let type = (args[0] || ' ').toLowerCase()
 
