daf985c22c4afe5d781e0aec626686d946fcfc48
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-27 20:28:24 -0500

    Update rpg-top.js

--- plugins/rpg-top.js
+++ plugins/rpg-top.js
@@ -5,10 +5,12 @@ let handler = async (m, { conn, args }) => {
   let sortedLim = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].limit - a[1].limit)
   let sortedmoney = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].money - a[1].money)
   let sortedlevel = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].level - a[1].level)
+
   let usersExp = sortedExp.map(v => v[0])
   let usersLim = sortedLim.map(v => v[0])
   let usersmoney = sortedmoney.map(v => v[0])
   let userslevel = sortedlevel.map(v => v[0])
+
   let len = args[1] && args[1].length > 1 ? Math.min(100, Math.max(parseInt(args[1]), 5)) : Math.min(10, sortedExp.length)
 
 let type = (args[0] || ' ').toLowerCase()
@@ -22,6 +24,18 @@ case 'money':
 *• Posicion:* *${usersmoney.indexOf(m.sender) + 1}* de *${usersmoney.length}*
 
 ${sortedmoney.slice(0, len).map(([user, data], i) => '*' + (i + 1) + `. @${user.split`@`[0]}*\n*💵 Dinero:* ${shortNum(data.money)}`).join`\n╶\n`}
+`
+  m.reply(text)
+break
+
+case 'exp':
+case 'xp':
+case 'experiencia':
+  let text = `\t\t\t\t*乂 T O P  -  E X P*
+
+*• Posicion:* *${userslevel.indexOf(m.sender) + 1}* de *${userslevel.length}*
+
+${sortedExp.slice(0, len).map(([user, data], i) => '*' + (i + 1) + `. @${user.split`@`[0]}*\n*✨ Exp:* ${shortNum(data.exp)}`).join`\n╶\n`}
 `
   m.reply(text)
 break
