b6679dcd31c81f5875028f67cab23417620607aa
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-29 15:16:23 -0500

    Update mensajes.js

--- plugins/mensajes.js
+++ plugins/mensajes.js
@@ -10,13 +10,13 @@ let handler = async (m, { conn }) => {
         totalM++
     }, 1000)
     let sorted = Object.entries(mCount).sort((a, b) => b[1] - a[1])
-    let pesan = sorted.map(v => `- ${v[0].replace(/(\d+)@.+/, '@$1')}: ${v[1]} mensajes`).join('\n')
+    let pesan = sorted.map(v => `*• Usuario:* ${v[0].replace(/(\d+)@.+/, '@$1')}\n*• Mensajes:* ${v[1]} total`).join('\n\n')
     m.reply(`*Total:* ${totalM} mensajes\n\n${pesan}`, false, { contextInfo: { mentionedJid: sorted.map(v => v[0]) } })
 }
 
 handler.help = ['totalmsg']
 handler.tags = ['group']
-handler.command = /^(mensajes)$/i
+handler.command = /^(mensajes|totalmsg)$/i
 handler.admin = true 
 
 module.exports = handler
