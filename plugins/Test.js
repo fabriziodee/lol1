d3470b6ff196c61f0060fa4b5141618261e9c6ff
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-29 08:15:06 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -3,7 +3,25 @@ let fetch = require('node-fetch')
 let axios = require('axios')
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  //Test
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+let valueAppearances = [];
+
+for (let index = 0; index < 10; index++) {
+  valueAppearances.push(0);
+}
+
+
+for (let i = 0; i <= 10000; i++) {
+  
+  let randomIndex = random(2, 9);
+  
+  valueAppearances[randomIndex]++;
+}
+console.log(valueAppearances)
+m.reply(valueAppearances)
 }
 
 handler.command = /^(test)$/i
