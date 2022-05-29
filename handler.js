58b91e9c4b67d9fa57aee305a26993e86634375e
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-28 22:36:38 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -514,19 +514,16 @@ switch (action) {
         }
         break
 
-      case 'promote':
-      if (chat.welcome) {
+      /*case 'promote':
       text = (chat.sPromote || this.spromote || conn.spromote || '@user ahora es administrador')
+
       case 'demote':
-        if (!text) text = (chat.sDemote || this.sdemote || conn.sdemote || '@user ya no es administrador')
-        text = text.replace('@user', '@' + participants[0].split('@')[0])
-        this.sendMessage(jid, text, MessageType.extendedText, {
-          contextInfo: {
-            mentionedJid: this.parseMention(text)
-          }
-        })
+      if (chat.welcome) {
+      if (!text) text = (chat.sDemote || this.sdemote || conn.sdemote || '@user ya no es administrador')
+      text = text.replace('@user', '@' + participants[0].split('@')[0])
+      this.sendMessage(jid, text, MessageType.extendedText, { contextInfo: { mentionedJid: this.parseMention(text) }})
       }
-        break
+        break*/
     }
   },
   async delete(m) {
