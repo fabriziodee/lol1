b104ede412e90e5479e0857153cf6253fc77b218
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-29 20:40:42 -0500

    Update reenviar.js

--- plugins/reenviar.js
+++ plugins/reenviar.js
@@ -2,8 +2,10 @@ let fs = require ('fs')
 
 let handler  = async (m, { args, conn, text, participants }) => {
   if (!m.quoted) throw 'Etiqueta un mensaje para reenviar!'
+  let lolitumb = fs.readFileSync('./storage/image/menu2.jpg')
+  let fgif = { key: {participant: `0@s.whatsapp.net`, ...("6289643739077-1613049930@g.us" ? { remoteJid: "6289643739077-1613049930@g.us" } : {})},message: {"videoMessage": { "title": 'lolibot', "h": `Hmm`,'seconds': '99999', 'gifPlayback': 'true', 'caption': '*OwO*', 'jpegThumbnail': lolitumb }}}
   let rr = (args[0] ? `${args[0]}` : m.chat)
-  await conn.copyNForward(rr, await conn.loadMessage(m.chat, m.quoted.id), false, { quoted: m })
+  await conn.copyNForward(rr, await conn.loadMessage(m.chat, m.quoted.id), false, { quoted: fgif })
   }
 
 handler.help = ['reenviar']
