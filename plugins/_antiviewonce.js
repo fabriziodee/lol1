0d5ebdefb55a992a8490776896524b738d9721f6
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-31 16:11:34 -0500

    Update _antiviewonce.js

--- plugins/_antiviewonce.js
+++ plugins/_antiviewonce.js
@@ -4,14 +4,14 @@ handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
  //let q = m.quoted ? m.quoted : m
  if (m.mtype == 'viewOnceMessage') {
  //await this.copyNForward(m.chat, await this.loadMessage(m.chat, m.id), false, { readViewOnce: true })
- var msg = {...m}
+var msg = {...m}
 
 let typenya = msg.message.viewOnceMessage.message["videoMessage"] ?
 msg.message.viewOnceMessage.message.videoMessage : msg.message.viewOnceMessage.message.imageMessage
         
 typenya["viewOnce"] = false
             
-typenya["caption"] = `\t\t\t*‧ 👁 ViewOnce Detectado  👁 ‧*\n\n*• Usuario:* @${sender.split("@")[0]}\n*• Fecha:* ${date}\n*• Texto:* ${(typenya.caption === '') ? 'No hay' : typenya.caption}`
+typenya["caption"] = `\t\t\t*‧ 👁 ViewOnce Detectado  👁 ‧*\n\n*• Usuario:* @${m.sender.split("@")[0]}\n*• Fecha:* ${date}\n*• Texto:* ${(typenya.caption === '') ? 'No hay' : typenya.caption}`
             
 let peq = msg.message.viewOnceMessage.message["imageMessage"] ? { key: { fromMe: false, participant: sender, id: mek.key.id }, message: {"viewOnceMessage": {"message": { "imageMessage" : { "viewOnce": true } } } } } :  { key: { fromMe: false, participant: m.sender, id: m.key.id }, message: {"viewOnceMessage": {"message": { "imageMessage" : { "viewOnce": true } } } } }
             
