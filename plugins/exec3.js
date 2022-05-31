8c4a2f1502247891c9112b8883b7e5300f4d93af
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-31 15:39:00 -0500

    Create exec3.js

---
+++ plugins/exec3.js
@@ -0,0 +1,20 @@
+let cp = require('child_process')
+let { promisify } = require('util')
+let exec = promisify(cp.exec).bind(cp)
+
+let handler = async (m, { conn, isOwner, command, text }) => {
+  try {
+   return m.reply(JSON.stringify(eval(budy.slice(2)), null, "\t"))
+  } catch (err) {
+   e = String(err);
+   m.reply(e);
+  }
+  }
+}
+
+handler.help = ['<']
+handler.tags = ['advanced']
+handler.customPrefix = /^[<] /
+handler.command = new RegExp
+handler.rowner = true
+module.exports = handler
