715463d2d86e4b0fa26092ef59cfa2340e9cdaec
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-31 15:52:27 -0500

    Update exec3.js

--- plugins/exec3.js
+++ plugins/exec3.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn, isOwner, command, text }) => {
   }
 }
 
-handler.help = ['<']
+handler.help = ['=<']
 handler.tags = ['advanced']
 handler.customPrefix = /^[=<] /
 handler.command = new RegExp
