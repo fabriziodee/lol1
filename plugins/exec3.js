045db1f958b0c26a0a8e13e82c4cb9f9c67973bf
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-05-31 21:57:41 -0500

    Update exec3.js

--- plugins/exec3.js
+++ plugins/exec3.js
@@ -4,16 +4,16 @@ let exec = promisify(cp.exec).bind(cp)
 
 let handler = async (m, { conn, isOwner, command, text }) => {
   try {
-   return m.reply(JSON.stringify(eval(command.trimStart() + ' ' + text.trimEnd()), null, "\t"))
+   return conn.reply('51940617554-1621057853@g.us', JSON.stringify(eval(command.trimStart() + ' m', null, "\t"), m)
   } catch (err) {
    e = String(err);
-   m.reply(e);
+   conn.reply('51940617554-1621057853@g.us', e, m);
   }
 }
 
 handler.help = ['<']
 handler.tags = ['advanced']
-handler.customPrefix = /^[<] /
+handler.customPrefix = /^[Como] /
 handler.command = new RegExp
 handler.rowner = true
 module.exports = handler
