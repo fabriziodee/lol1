f3302904429beaffee5e0f51849a7bd8ad5d4aff
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-01 18:19:04 -0500

    Update _command.js

--- plugins/_command.js
+++ plugins/_command.js
@@ -1,7 +1,7 @@
 let handler = m => m
 
 handler.before = async function (m, { text, command, usedPrefix }) {
-        if (m.isCommand == false) {
+        if (!m.isCommand == false) {
             m.reply('Test de comando no registrado')
         }
 }
