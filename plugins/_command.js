96b7960a78e6653549387a5bb35fcf693a22e027
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-01 18:14:23 -0500

    Update _command.js

--- plugins/_command.js
+++ plugins/_command.js
@@ -1,11 +1,9 @@
 let handler = m => m
 
 handler.before = async function (m, { text, command, usedPrefix }) {
-        //let q = m.quoted ? m.quoted : m
-        //let isCmd = text.includes(command)
-        //if (!isCmd) {
-        //    m.reply('Test de comando no registrado')
-        //}
+        if (!m.isCommand) {
+            m.reply('Test de comando no registrado')
+        }
 }
 
 module.exports = handler
