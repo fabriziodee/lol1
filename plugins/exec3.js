b858dc1969d942cd9b17d027168527d652c8d866
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-01 18:56:21 -0500

    Update exec3.js

--- plugins/exec3.js
+++ plugins/exec3.js
@@ -4,7 +4,7 @@ let exec = promisify(cp.exec).bind(cp)
 
 let handler = async (m, { conn, isOwner, command, text }) => {
   try {
-   return m.reply(JSON.stringify(eval(command.trimStart() + ' m', null, "\t"))
+   return m.reply(JSON.stringify(eval(command.trimStart() + ' m', null, "\t")))
   } catch (err) {
    e = String(err);
    m.reply(e);
