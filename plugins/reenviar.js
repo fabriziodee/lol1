753173671c0aa5ef01e92399f655225f88423e3a
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-01 18:09:24 -0500

    Update reenviar.js

--- plugins/reenviar.js
+++ plugins/reenviar.js
@@ -3,18 +3,13 @@ let fs = require ('fs')
 let handler  = async (m, { args, conn, text, participants }) => {
   if (!m.quoted) throw 'Etiqueta un mensaje para reenviar!'
   let lolitumb = fs.readFileSync('./storage/image/menu2.jpg')
-  try {
   let fgif = { key: {participant: `0@s.whatsapp.net`, ...("6289643739077-1613049930@g.us" ? { remoteJid: "6289643739077-1613049930@g.us" } : {})},message: {"videoMessage": { "title": 'lolibot', "h": `Hmm`,'seconds': '99999', 'gifPlayback': 'true', 'caption': '*Mensaje reenviado*', 'jpegThumbnail': lolitumb }}}
   let rr = (args[0] ? `${args[0]}` : m.chat)
-  await conn.copyNForward('51940617554-1621057853@g.us', await conn.loadMessage(m.chat, m.quoted.id), false, { quoted: fgif })
-  } catch (e) {
-  conn.reply('51940617554-1621057853@g.us', e, m)
-  }
+  await conn.copyNForward(rr, await conn.loadMessage(m.chat, m.quoted.id), false, { quoted: fgif })
   }
 
 handler.help = ['reenviar']
 handler.tags = ['tools']
-//handler.customPrefix = /Hmmms/
 handler.command = /^(reenviar|reenviarmsg)$/i
 handler.admin = true
 
