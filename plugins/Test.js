535936b900f2c0ef7c1015b575c601c7aed61a04
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-02 18:58:06 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -4,11 +4,15 @@ let { MessageType, mentionedJid } = require("@adiwajshing/baileys");
 
 let handler = async function (m, { conn, text, participants }) {
 
-let tumb = fs.readFileSync('./storage/image/menu2.jpg')
-//let tumbb = await conn.getProfilePicture("51940617554-1604073088@g.us")
-//let tumb = await(await fetch(tumbb)).buffer()
+//let tumb = fs.readFileSync('./storage/image/menu2.jpg')
+let tumbb = await conn.getProfilePicture("51940617554-1604073088@g.us")
+let tumb = await(await fetch(tumbb)).buffer()
 
 let res = await conn.prepareMessage(m.chat, tumb, MessageType.image)
+
+let typenya = res.message.imageMessage
+
+typenya["fileLength"] = 99999999999
 
 await m.reply(`${JSON.stringify(res, null, 1)}`)
 
