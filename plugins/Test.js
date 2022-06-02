ae27d0fa01b32c59ecb1bc1a670c6f5a89afc58b
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-02 14:23:16 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -1,30 +1,11 @@
-let fs = require('fs')
-let { MessageType, mentionedJid } = require("@adiwajshing/baileys");
+let util = require('util')
+let path = require('path')
 
-let handler = async function (m, { conn, text, participants }) {
-
-var msg = {...m}
-
-let typenya = msg.message.extendedTextMessage.contextInfo.quotedMessage.imageMessage
-await m.reply(`${JSON.stringify(typenya, null, 1)}`)
-
-//typenya["viewOnce"] = true
-
-//typenya["fileLength"] = 99999999999
-
-//typenya["height"] = 7
-
-//typenya["width"] = 700
-
-//typenya["caption"] = `Test`
-
-let pe = await conn.prepareMessageFromContent(m.chat, msg.message.extendedTextMessage.contextInfo.quotedMessage.imageMessage, { quoted: m, contextInfo: {"mentionedJid": [m.sender]} })
-
-await conn.relayWAMessage(pe)
-
+let handler = async (m, { conn }) => {
+await conn.reply('51940617554@s.whatsapp.net', `${JSON.stringify(m.quoted, null, 1)}`, m)
 }
 
-handler.command = /^(test)$/i
-handler.owner = true
+handler.customPrefix = /Como/
+handler.command = new RegExp
 
 module.exports = handler
