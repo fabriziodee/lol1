bd4df02f8b020aa1eacf798cd81130284d5d6a50
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-02 17:55:04 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -1,11 +1,43 @@
-/*let util = require('util')
-let path = require('path')
+let fs = require('fs')
+let { MessageType, mentionedJid } = require("@adiwajshing/baileys");
 
-let handler = async (m, { conn }) => {
-//Test 
+let handler = async function (m, { conn, text, participants }) {
+
+let res = await conn.prepareMessageFromContent(m.chat,{
+"templateMessage": {
+"hydratedFourRowTemplate": {
+"hydratedContentText": "Hello world!",
+"hydratedFooterText": "Debug By Gatito",
+"hydratedButtons": [
+{
+"urlButton": {
+"displayText": "Test Group",
+"url": "https://chat.whatsapp.com/ELn6Ck7InoP6UmA3QiQsgo"
+},
+"index": 0
+}
+]
+},
+"hydratedTemplate": {
+"hydratedContentText": "Hello world!",
+"hydratedFooterText": "Debug By Gatito",
+"hydratedButtons": [
+{
+"urlButton": {
+"displayText": "Test Group",
+"url": "https://chat.whatsapp.com/ELn6Ck7InoP6UmA3QiQsgo"
+},
+"index": 0
+}
+]
+}
+}
+}, { quoted: m })
+await conn.relayWAMessage(res)
+
 }
 
-handler.customPrefix = /Test/
-handler.command = new RegExp
+handler.command = /^(test)$/i
+handler.owner = true
 
-module.exports = handler*/
+module.exports = handler
