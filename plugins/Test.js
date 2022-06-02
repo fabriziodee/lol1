954aa0da8e41b57c186b305b68049db4c9d89bbc
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-02 18:05:12 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -4,64 +4,14 @@ let { MessageType, mentionedJid } = require("@adiwajshing/baileys");
 let handler = async function (m, { conn, text, participants }) {
 
 let res = await conn.prepareMessageFromContent(m.chat,{
-"templateMessage": {
-"hydratedFourRowTemplate": {
-"hydratedContentText": "Hi MyMans APIs 👋,\n\nThank you for your message.\n\nHow can I help you today?",
-"hydratedFooterText": "WATI's Chatbot",
-"hydratedButtons": [
-{
-"quickReplyButton": {
-"displayText": "Know the Pricing",
-"id": "60dd75b0081979507a679f99"
-},
-"index": 0
-},
-{
-"quickReplyButton": {
-"displayText": "Know how WATI works?",
-"id": "60dd75b0081979507a679f99"
-},
-"index": 1
-},
-{
-"quickReplyButton": {
-"displayText": "Get Started",
-"id": "60dd75b0081979507a679f99"
-},
-"index": 2
-}
-]
-},
-"hydratedTemplate": {
-"hydratedContentText": "Hi Ivanzz",
-"hydratedFooterText": "Selamat Datang",
-"hydratedButtons": [
-{
-"quickReplyButton": {
-"displayText": "Debug",
-"id": "60dd75b0081979507a679f99"
-},
-"index": 0
-},
-{
-"quickReplyButton": {
-"displayText": "By",
-"id": "60dd75b0081979507a679f99"
-},
-"index": 1
-},
-{
-"quickReplyButton": {
-"displayText": "Ivanzz",
-"id": "60dd75b0081979507a679f99"
-},
-"index": 2
-}
-]
-}
-}
-}, { quoted: m })
-conn.relayWAMessage(res)
+"sendPaymentMessage": {
+      "noteMessage": {
+       "extendedTextMessage": {
+        "text": "Test"
+       }
+      }
+     }, { quoted: m })
+await conn.relayWAMessage(res)
 
 }
 
