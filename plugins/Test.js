a0dab7c89aa4cb52b81095a4af277f55c2188fe7
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-02 18:38:32 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -3,22 +3,12 @@ let { MessageType, mentionedJid } = require("@adiwajshing/baileys");
 
 let handler = async function (m, { conn, text, participants }) {
 
-let tumbb = fs.readFileSync('./storage/image/menu2.jpg')
+let tumb = fs.readFileSync('./storage/image/menu2.jpg')
 
-let res = await conn.prepareMessageFromContent(m.chat,{
-"orderMessage": {
-"orderId": '155157279766079',
-"itemCount": '2022',
-"status": 'INQUIRY',
-"surface":'CATALOG',
-"message": 'Test',
-"thumbnail": tumbb,
-"orderTitle": 'SPNb',
-"sellerJid": '0@s.whatsapp.net',
-"token": 'AR5wc3iY2NY8yJaK9MMXdlK/aguUxoA8yPtSFcvt0lrE5g=='
-}
-}, 
- { quoted: m, contextInfo: {} }) 
+let res = await conn.prepareMessageFromContent(m.chat, tumb, MessageType.image, { quoted: m })
+
+await m.reply(`${JSON.stringify(res, null, 1)}`)
+
 await conn.relayWAMessage(res)
 
 }
