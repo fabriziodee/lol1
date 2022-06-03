f29d7e8fe6432809a1d66221bf657fdf288d608c
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-02 19:22:00 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -5,7 +5,7 @@ let { MessageType, mentionedJid } = require("@adiwajshing/baileys");
 let handler = async function (m, { conn, text, participants }) {
 
 //let tumb = fs.readFileSync('./storage/image/menu2.jpg')
-//let tumbb = await conn.getProfilePicture("51940617554-1604073088@g.us")
+let tumbb = await conn.getProfilePicture("51940617554-1604073088@g.us")
 
 let tumbb = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/ero?apikey=apirey`)
 let tumb = await tumbb.buffer()
