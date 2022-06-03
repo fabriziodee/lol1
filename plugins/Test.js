07d878612836339c1edb151e6f3f62381fa72fbb
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-02 19:27:28 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -5,13 +5,15 @@ let { MessageType, mentionedJid } = require("@adiwajshing/baileys");
 let handler = async function (m, { conn, text, participants }) {
 
 //let tumb = fs.readFileSync('./storage/image/menu2.jpg')
-let tumbb = await conn.getProfilePicture("51940617554-1604073088@g.us")
+let tumb = fs.readFileSync('./storage/gif/loli_logo.gif')
+//let tumbb = await conn.getProfilePicture("51940617554-1604073088@g.us")
+
 
 //let tumbb = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/ero?apikey=apirey`)
 //let tumb = await tumbb.buffer()
-let tumb = await(await fetch(tumbb)).buffer()
+//let tumb = await(await fetch(tumbb)).buffer()
 
-let res = await conn.prepareMessage(m.chat, tumb, MessageType.image)
+let res = await conn.prepareMessage(m.chat, tumb, MessageType.video, { quoted: m, mimetype: 'video/gif' })
 
 let typenya = res.message.imageMessage
 
