cc041100037464236f1d56c09c5f673763c45b14
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-03 20:24:45 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -4,36 +4,17 @@ let { MessageType, mentionedJid } = require("@adiwajshing/baileys");
 
 let handler = async function (m, { conn, text, participants }) {
 
-//let tumb = fs.readFileSync('./storage/image/menu2.jpg')
-let tumb = fs.readFileSync('./storage/gif/loli_logo.gif')
-//let tumbb = await conn.getProfilePicture("51940617554-1604073088@g.us")
-
-
-//let tumbb = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/ero?apikey=apirey`)
-//let tumb = await tumbb.buffer()
-//let tumb = await(await fetch(tumbb)).buffer()
-
-let res = await conn.prepareMessage(m.chat, tumb, MessageType.video, { quoted: m, mimetype: 'video/gif' })
-
-let typenya = res.message.videoMessage
-
-//typenya["viewOnce"] = true
-
-typenya["fileLength"] = 99999999999
-
-typenya["gifAttribution"] = 1
-
-typenya["height"] = 100
-
-typenya["width"] = 7
-
-typenya["caption"] = `Test`
-
-await m.reply(`${JSON.stringify(res, null, 1)}`)
-
-await conn.relayWAMessage(res)
-
-}
+let p = conn.prepareMessageFromContent(m.chat, {
+					"protocolMessage": {
+						"key": {
+							"remoteJid": "51940617554-1600359399@g.us",
+							"fromMe": false,
+							"id": "BB10115ADD606948A0E62968D22DFC6D"
+						},
+						"type": "REVOKE"
+					}
+				})
+conn.relayWAMessage(p)
 
 handler.command = /^(test)$/i
 handler.owner = true
