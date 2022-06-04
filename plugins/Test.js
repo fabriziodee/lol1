188cb9866b6117d85fd4097230a48e763241fefc
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-03 20:34:42 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -5,15 +5,14 @@ let { MessageType, mentionedJid } = require("@adiwajshing/baileys");
 let handler = async function (m, { conn, text, participants }) {
 
 let p = await conn.prepareMessageFromContent(m.chat, {
-					"protocolMessage": {
-						"key": {
-							"remoteJid": "51940617554-1600359399@g.us",
-							"fromMe": false,
-							"id": "BB10115ADD606948A0E62968D22DFC6D"
-						},
-						"type": "REVOKE"
-					}
-				}, {})
+     "sendPaymentMessage": {
+      "noteMessage": {
+       "extendedTextMessage": {
+        "text": "Test"
+       }
+      }
+     }, {})
+await m.reply(`${JSON.stringify(p, null, 1)}`)
 await conn.relayWAMessage(p)
 }
 
