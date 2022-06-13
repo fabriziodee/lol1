6b2a1e03ef57b16e457ad6fc7a0d588a34e7e624
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-13 18:24:07 -0500

    Update tag.js

--- plugins/tag.js
+++ plugins/tag.js
@@ -1,7 +1,10 @@
 const { MessageType } = require('@adiwajshing/baileys')
+let fs = require('fs')
 
 let handler = async (m, { conn, text, participants }) => {
   let users = participants.map(u => u.jid)
+  let lolitumb = fs.readFileSync('./storage/image/menu2.jpg')
+  let fgif = { key: {participant: `0@s.whatsapp.net`, ...("6289643739077-1613049930@g.us" ? { remoteJid: "6289643739077-1613049930@g.us" } : {})},message: {"videoMessage": { "title": 'lolibot', "h": `Hmm`,'seconds': '99999', 'gifPlayback': 'true', 'caption': '*Anuncio 🕊️*', 'jpegThumbnail': lolitumb }}} f
   let q = m.quoted ? m.quoted : m
   let c = m.quoted ? m.quoted : m.msg
   let msg = conn.cMod(
@@ -15,7 +18,7 @@ let handler = async (m, { conn, text, participants }) => {
         contextInfo: {
           mentionedJid: users
         },
-        quoted: m
+        quoted: fgif
       }
     ),
     text || q.text 
@@ -25,7 +28,7 @@ let handler = async (m, { conn, text, participants }) => {
 
 handler.help = ['tag']
 handler.tags = ['adm']
-handler.command = /^(hidetag|tag)$/i
+handler.command = /^(hidetag|tag|notify)$/i
 
 handler.group = true
 handler.admin = true
