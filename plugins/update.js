020b393a369c0660a7c2c4798179c295552fff32
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-13 21:39:52 -0500

    Update update.js

--- plugins/update.js
+++ plugins/update.js
@@ -1,16 +1,16 @@
 let { execSync } = require('child_process')
+
 let handler = async (m, { conn, text }) => {
   if (global.conn.user.jid == conn.user.jid) {
     let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-    require('fs').readdirSync('plugins').map(v=>global.reload('', v))
     conn.reply(m.chat, stdout.toString(), m)
   }
 }
 
 handler.help = ['update']
 handler.tags = ['host']
-handler.command = /^(update|fix|actualizar)$/i
+handler.command = /^(update|fix|gitpull|actualizar)$/i
+
 handler.owner = true
-handler.fail = null
 
 module.exports = handler
