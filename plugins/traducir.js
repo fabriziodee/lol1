89b640bb09a94ec9a89ea17da63523215317a62b
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-13 22:48:55 -0500

    Update traducir.js

--- plugins/traducir.js
+++ plugins/traducir.js
@@ -1,34 +1,37 @@
-let fetch = require('node-fetch')
+const translate = require('translate-google-api')
 const defaultLang = 'es'
+const tld = 'cn'
 
 let handler = async (m, { args, usedPrefix, command }) => {
-    let err = `*Ingrese un texto para traducir o etiqueta uno*\n\n- Ejemplo: ${usedPrefix + command} es Hello`
+    let err = `Escribe un texto junto al comando para traducir o simplemente etiqueta uno`
     let lang = args[0]
     let text = args.slice(1).join(' ')
     if ((args[0] || '').length !== 2) {
         lang = defaultLang
         text = args.join(' ')
     }
-    
     if (!text && m.quoted && m.quoted.text) text = m.quoted.text
 
+    let result
     try {
-        let res = await fetch(global.API('bg', '/translate', { q: text, lang }))
-        let json = await res.json()
-        if (json.status !== true) throw json
+        result = await translate(`${text}`, {
+            tld,
+            to: lang,
+        })
     } catch (e) {
-        let res = await fetch(global.API('bg', '/translate', { q: text, defaultLang }))
-        let json = await res.json()
-        if (json.status !== true) throw json
+        result = await translate(`${text}`, {
+            tld,
+            to: defaultLang,
+        })
+        throw err
     } finally {
-        conn.reply(m.chat, `\t\t*‧ 🌐 Google Traductor 🌐 ‧*\n\n*${json.result.from.language.iso}:* ${text}\n*${lang}:* ${json.result.text}`, m)
+        m.reply(`\t\t*‧ 🌐 Google Traductor 🌐 ‧*\n\n${result[0]}`)
     }
 
 }
 
 handler.help = ['traducir']
 handler.tags = ['tools']
-handler.command = /^(traducir|trad|translate)$/i
-handler.fail = null
+handler.command = /^(traductor|traducir|traduce|translate|tl|trad|tr)$/i
 
 module.exports = handler
