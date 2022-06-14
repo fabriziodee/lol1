5cef7b1b91473df4edbfabb44f743caaef5e7c4c
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-13 22:06:00 -0500

    Update _cmdWithMedia.js

--- plugins/_cmdWithMedia.js
+++ plugins/_cmdWithMedia.js
@@ -6,11 +6,8 @@ module.exports = {
         if (!m.message) return
         if (!m.msg.fileSha256) return
         if (!(Buffer.from(m.msg.fileSha256).toString('base64') in global.DATABASE._data.sticker)) return
-        //if (!(m.msg.fileSha256.toString('hex') in global.DATABASE._data.sticker)) return
-        
+
         let hash = global.DATABASE._data.sticker[Buffer.from(m.msg.fileSha256).toString('base64')]
-        //let hash = global.DATABASE._data.sticker[m.msg.fileSha256.toString('hex')]
-        m.reply('Test')
         let { text, mentionedJid } = hash
         this.emit('chat-update', {
             ...chatUpdate,
