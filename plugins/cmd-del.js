da7428d9a2eac85fc467339562cc3d24d995a88a
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-13 22:54:25 -0500

    Create cmd-del.js

---
+++ plugins/cmd-del.js
@@ -0,0 +1,15 @@
+let handler = async (m, { conn, usedPrefix, text, command }) => {
+    let hash = text
+    if (m.quoted && m.quoted.fileSha256) hash = m.quoted.fileSha256.toString('hex')
+    if (!hash) throw 'Ingrese el nombre del comamdo'
+    let sticker = global.DATABASE._data.sticker
+    if (sticker[hash] && sticker[hash].locked) throw 'No puedes borrar este comando'
+    delete sticker[hash]
+    m.reply(`✅ Comando eliminado`)
+}
+
+handler.help = ['delcmd']
+handler.tags = ['database']
+handler.command = /^(delcmd|cmddel)$/i
+
+module.exports = handler
