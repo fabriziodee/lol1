b4f0b8961bfbdd2e91eee3b2b7182883231ac956
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-14 22:14:46 -0500

    Update _command.js

--- plugins/_command.js
+++ plugins/_command.js
@@ -1,9 +1,10 @@
 let handler = m => m
 
 handler.before = async function (m, { text, command, usedPrefix }) {
-        if (m.isCommand = true) {
-            m.reply('✅')
-        }
+   m.reply('✅')
+  if (m.isCommand = true) {
+   m.reply('✅')
+  }
 }
 
 module.exports = handler
