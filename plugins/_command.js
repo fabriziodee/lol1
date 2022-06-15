19cdb66ebae48993423d9b2a03acc8359c5be6f7
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-14 23:02:48 -0500

    Update _command.js

--- plugins/_command.js
+++ plugins/_command.js
@@ -3,10 +3,10 @@ let handler = m => m
 handler.before = async function (m, { text, command, usedPrefix }) {
   m.isCommand = true
   let isCmd = m.isCommand
-  //await m.reply(`${JSON.stringify(isCmd, null, 1)}`)
-  if (isCmd == true) {
+  await m.reply(`${JSON.stringify(isCmd, null, 1)}`)
+  //if (isCmd == true) {
   //m.reply(`${isCmd ? '✅' : '❎'}`)
-  }
+  //}
 }
 
 module.exports = handler
