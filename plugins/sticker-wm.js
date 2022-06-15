e68cb191934baf809ea9ad67288eee276a9455f7
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-14 21:30:33 -0500

    Update sticker-wm.js

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -1,4 +1,4 @@
-let { MessageType } = require("@adiwajshing/baileys");
+/*let { MessageType } = require("@adiwajshing/baileys");
 let fetch = require('node-fetch')
 let axios = require('axios')
 
@@ -41,4 +41,34 @@ axios('https://sticker-api-tpe3wet7da-uc.a.run.app/prepareWebp', {
   }).catch(reject)
      
  }) 
+}*/
+
+
+const { addExif } = require('../lib/sticker.js')
+
+let handler = async (m, { conn, text }) => {
+  let isSticker = 'Etiqueta un sticker'
+  if (!m.quoted) throw isSticker
+  let stiker = false
+  try {
+    let [packname, ...author] = text.split('|')
+    author = (author || []).join('|')
+    let mime = m.quoted.mimetype || ''
+    if (!/webp/.test(mime)) throw isSticker
+    let img = await m.quoted.download()
+    if (!img) throw isSticker
+    stiker = await addExif(img, packname || '', author || '')
+  } catch (e) {
+    console.error(e)
+    if (Buffer.isBuffer(e)) stiker = e
+  } finally {
+    if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, false, { asSticker: true })
+    else throw 'La conversión falló'
+  }
 }
+
+handler.help = ['wm']
+handler.tags = ['sticker']
+handler.command = /^(tak|wm)$/i
+
+module.exports = handler
