4b59418429cb5a77976f19c6c5fc880448925022
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-15 19:51:18 -0500

    Create cartel-loli.js

---
+++ plugins/cartel-loli.js
@@ -0,0 +1,43 @@
+let { spawn } = require('child_process')
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (text.length > 19) return m.reply('El texto es demɑsiɑdo lɑrgo mínimo 19 cɑrɑcteres')
+  let str = '*Cartel loli kawaii 🪧*'
+  if (global.support.convert || global.support.magick || global.support.gm) {
+  let fontTexts = 'src/font/Zahraaa.ttf'
+  let xtsx = 'storage/image/cartel_loli.jpg'
+  let bufs = []
+  const [_spawnprocess, ..._spawnargs] = [...(global.support.gm ? ['gm'] : global.support.magick ? ['magick'] : []),
+       'convert',
+       xtsx,
+       '-font',
+       fontTexts,
+       '-fill',
+       '#26619c',
+       '-size',
+       '100x100',
+       '-pointsize',
+       '20',
+       '-interline-spacing',
+       '-7.5',
+       '-annotate',
+       '+57+255',
+       text,
+       '-append',
+       'jpg:-'
+     ]
+     spawn(_spawnprocess, _spawnargs)
+       .on('close', () => {
+         conn.sendFile(m.chat, Buffer.concat(bufs), 'result.jpg', str, m)
+       })
+       .stdout.on('data', chunk => bufs.push(chunk))
+  } else {
+    m.reply(str)
+  }
+}
+
+handler.help = ['cloli']
+handler.tags = ['maker']
+handler.command = /^(cartelloli|cloli)$/i
+
+module.exports = handler
