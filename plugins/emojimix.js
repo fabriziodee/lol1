970bc41c3967e4683087fd501f566b84e5f4df7c
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-16 21:13:58 -0500

    Update emojimix.js

--- plugins/emojimix.js
+++ plugins/emojimix.js
@@ -6,16 +6,11 @@ let fs = require("fs")
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 if (!args[0]) throw `*• Ingrese dos emojis pɑrɑ mezclɑr*\n\n*Ejemplo de uso:*\n1. ${usedPrefix + command} <emoji1 + emoji2>\n2. ${usedPrefix + command} 🐱+👻`
 let [emoji1, emoji2] = text.split`+`
-//try {
 const anu = await (await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)).json()
-//await m.reply(`${JSON.stringify(anu.results[0], null, 1)}`)
 if (anu.results[0] == undefined) throw 'No se encontró el emoji'
 let emix = anu.results[0].media_formats.png_transparent.url
 let stiker = await sticker(null, emix, global.packname, global.author)
 conn.sendMessage(m.chat, stiker, MessageType.sticker, { quoted: m })
-//} catch (e) {
-//return m.reply('No se encontro el emoji!')
-//}
 }
 
 handler.help = ['emojimix']
