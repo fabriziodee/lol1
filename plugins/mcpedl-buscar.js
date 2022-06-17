8e5b99b96939f9d16af11e6ffd433753546c8573
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-16 23:10:09 -0500

    Update mcpedl-buscar.js

--- plugins/mcpedl-buscar.js
+++ plugins/mcpedl-buscar.js
@@ -4,12 +4,20 @@ let fetch = require('node-fetch')
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
   if (!text) throw 'Ingrese un texto justo al comando'
+  conn.reply(m.chat, global.wait, m)
   let mc = await tod.mcpedl(text)
   let msg = mc.map(({ name, category, date, desc, link }) => {
     return `*• Nombre:* ${name}\n*• Categoria:* ${category}\n*• Fecha:* ${date}\n*• Descripción:* ${desc}\n*• Link:* ${link}`
   }).join`\n\n─────────────────\n\n`
-  await m.reply(msg)
-  //await m.reply(`${JSON.stringify(rr, null, 1)}`)
+  let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).buffer()
+  if (ss.includes('html')) throw ''
+  let res = await conn.prepareMessage(m.chat, ss, MessageType.image, { quoted: m, caption: '\t\t\t\t*‧ 🔎 Mcpedl Busqueda 🔎 ‧*\n\n─────────────────\n\n' + msg})
+  let typenya = res.message.imageMessage
+
+  typenya["height"] = 55
+  typenya["width"] = 100
+
+  await conn.relayWAMessage(res)
 }
 
 handler.help = ['mcpdl']
