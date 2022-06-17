bd24da9f678c6e8fbedb382a79eb465540150a9d
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-16 20:35:29 -0500

    Update cmd-lock.js

--- plugins/cmd-lock.js
+++ plugins/cmd-lock.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!m.quoted) throw '✳️ Responde a un mensaje'
   if (!m.quoted.fileSha256) throw 'SHA256 Hash Missing'
-  let sticker = db.data.sticker
+  let sticker = global.DATABASE.data.sticker
   let hash = m.quoted.fileSha256.toString('hex')
   if (!(hash in sticker)) throw 'No está registrado en la base de datos'
   sticker[hash].locked = !/^un/i.test(command)
